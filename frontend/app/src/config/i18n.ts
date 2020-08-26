31f3e025329e11a1cbd5a2733cb14c38a0509a2f
Koen Brouwer <contact@kbrouwer.nl>
2020-08-26 15:34:47 +0200

    Setup i18n with react-i18next.

---
+++ frontend/app/src/config/i18n.ts
@@ -0,0 +1,28 @@
+import i18Next from "i18next";
+import { initReactI18next, Trans, useTranslation } from "react-i18next";
+
+/* Add languages here */
+const languages = {
+	nl: require("./nl.json"),
+};
+
+/* Don't change anything below this line */
+const resources = {};
+Object.keys(languages).forEach((lang) => {
+	resources[lang] = { translation: languages[lang] };
+});
+
+console.log(resources);
+
+i18Next.use(initReactI18next).init({
+	resources,
+	lng: "nl",
+	fallbackLng: "nl",
+	interpolation: {
+		escapeValue: false,
+	},
+	defaultNS: "translation",
+});
+
+export { Trans };
+export const useTranslate = useTranslation;
