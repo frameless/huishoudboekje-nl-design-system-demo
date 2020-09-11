b1d6c2ac404c7cc74fcf07aeaa68ab00aca12498
Koen Brouwer <contact@kbrouwer.nl>
2020-09-11 15:50:32 +0200

    Login screen is now more responsive.

--- frontend/app/src/components/Logo.tsx
+++ frontend/app/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Image } from "@chakra-ui/core";
 
 const Logo = (props) => (
-	<Box h={"auto"} bg={"white"} p={5} {...props}>
+	<Box h={"auto"} p={5} {...props}>
 		<Image src={process.env.PUBLIC_URL + "/theme/logo.svg"} alt="logo"/>
 	</Box>
 );
