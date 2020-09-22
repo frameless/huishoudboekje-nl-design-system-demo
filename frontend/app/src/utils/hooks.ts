5978c490238d7f74c9d08f1b63dbae615fe3d587
Koen Brouwer <contact@kbrouwer.nl>
2020-09-22 21:37:48 +0200

    Sample data can now be used with the useSampleData hook, which makes typing and auto importing easier.

--- frontend/app/src/utils/hooks.ts
+++ frontend/app/src/utils/hooks.ts
@@ -8,3 +8,10 @@ export const useSession = () => {
 	const store = useStore();
 	return store.session;
 }
+
+export const useSampleData = () => {
+	return {
+		users: require("../config/sampleData/users.json"),
+		citizens: require("../config/sampleData/citizens.json"),
+	}
+}
