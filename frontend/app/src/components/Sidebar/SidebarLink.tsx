a1c3bd585b38c62bd063cbe5a44966bb867b32fa
Koen Brouwer <contact@kbrouwer.nl>
2020-09-22 22:51:25 +0200

    Fixed the styling of icons

--- frontend/app/src/components/Sidebar/SidebarLink.tsx
+++ frontend/app/src/components/Sidebar/SidebarLink.tsx
@@ -1,17 +1,37 @@
 import React from "react";
-import {Button, Flex, Icon, Text} from "@chakra-ui/core";
-import {useHistory} from "react-router-dom";
+import {Box, BoxProps, Button, Flex, Text} from "@chakra-ui/core";
+import {useHistory, useLocation} from "react-router-dom";
+import {IconContext} from "react-icons";
+import theme from "../../config/theme";
 
-const SidebarLink = ({icon, href, children, ...props}) => {
+const SidebarLink = ({icon, href, children, exactMatch = false, ...props}) => {
 	const {push} = useHistory();
+	const location = useLocation();
+	const isActive = exactMatch ? location.pathname === href : location.pathname.includes(href);
+
+	// We use the mapped color from the Chakra theme because IconContext requires "regular" colors and thus can't handle "primary.700".
+	// Todo: maybe create a hook that handles translation of colors.
+	const color = isActive ? theme.colors["primary"][700] : "black";
+
+	const LinkIcon: React.FC<BoxProps> = (props) => {
+		return (
+			<IconContext.Provider value={{style: {color}}}>
+				<Box {...props}>
+					{icon()}
+				</Box>
+			</IconContext.Provider>
+		)
+	}
 
 	return (
-		<Button justifyContent={"flex-start"} onClick={() => push(href)} {...props}>
-			<Flex alignItems={"center"}>
-				<Icon as={icon} size={"24px"} mr={6} />
-				<Text>{children}</Text>
-			</Flex>
-		</Button>
+		<IconContext.Provider value={{color: "blue"}}>
+			<Button justifyContent={"flex-start"} onClick={() => push(href)} variant={"link"} {...props}>
+				<Flex alignItems={"center"}>
+					<LinkIcon mr={5} fontSize={"24px"} />
+					<Text color={color}>{children}</Text>
+				</Flex>
+			</Button>
+		</IconContext.Provider>
 	);
 };
 
