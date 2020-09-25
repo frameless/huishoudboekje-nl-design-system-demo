8818516339f22b125c9e6296a4e18847171dafd6
Koen Brouwer <contact@kbrouwer.nl>
2020-09-25 16:26:42 +0200

    CitizenCard is now a row again on mobile.

--- frontend/app/src/components/Citizens/CitizenCard.tsx
+++ frontend/app/src/components/Citizens/CitizenCard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import {Avatar, BoxProps, Flex, Text, Stack} from "@chakra-ui/core";
+import {Avatar, BoxProps, Flex, Stack, Text} from "@chakra-ui/core";
 import Routes from "../../config/routes";
 import {useHistory} from "react-router-dom";
+import {useIsMobile} from "react-grapple";
 
 const NotificationBadge = ({children}) => {
 	const size = "1.4em";
@@ -16,17 +17,18 @@ const NotificationBadge = ({children}) => {
 const CitizenCard: React.FC<BoxProps & { citizen, showBadge?: boolean }> = ({citizen, showBadge = false, ...props}) => {
 	const name = `${citizen.firstName} ${citizen.lastName}`;
 	const {push} = useHistory();
+	const isMobile = useIsMobile(450);
 
 	return (
-		<Stack direction={"row"} width={"100%"} justifyContent={"center"} alignItems={"center"} bg={"white"} borderRadius={10} p={5}
+		<Stack direction={"row"} width={"100%"} justifyContent={isMobile ? "flex-start" : "center"} alignItems={"center"} bg={"white"} borderRadius={10} p={5}
 		       onClick={() => push(Routes.Citizen(citizen.id))} {...props}>
-			<Stack direction={"column"} spacing={5} alignItems={"center"}>
+			<Stack direction={isMobile ? "row" : "column"} spacing={5} alignItems={"center"}>
 				<Avatar name={name}>
 					{showBadge && citizen.notifications && (
 						<NotificationBadge>{citizen.notifications}</NotificationBadge>
 					)}
 				</Avatar>
-				<Text fontSize={"md"} textAlign={"center"}><strong>{name}</strong></Text>
+				<Text fontSize={"md"} {...!isMobile && {textAlign: "center"}}><strong>{name}</strong></Text>
 			</Stack>
 		</Stack>
 	);
