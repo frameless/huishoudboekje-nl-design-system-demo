c7d65f3d80109dd53aebb09be5a45db5b7dd1e06
Koen Brouwer <contact@kbrouwer.nl>
2020-09-28 15:00:54 +0200

    Disabled click on CitizenCard.

--- frontend/app/src/components/Citizens/CitizenCard.tsx
+++ frontend/app/src/components/Citizens/CitizenCard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Avatar, BoxProps, Flex, Stack, Text} from "@chakra-ui/core";
-import Routes from "../../config/routes";
-import {useHistory} from "react-router-dom";
 import {useIsMobile} from "react-grapple";
 
 const NotificationBadge = ({children}) => {
@@ -16,12 +14,16 @@ const NotificationBadge = ({children}) => {
 
 const CitizenCard: React.FC<BoxProps & { citizen, showBadge?: boolean }> = ({citizen, showBadge = false, ...props}) => {
 	const name = `${citizen.firstName} ${citizen.lastName}`;
-	const {push} = useHistory();
+	// const {push} = useHistory();
 	const isMobile = useIsMobile(450);
+
+	const onClick = () => {
+		// push(Routes.Citizen(citizen.id))
+	};
 
 	return (
 		<Stack direction={"row"} width={"100%"} justifyContent={isMobile ? "flex-start" : "center"} alignItems={"center"} bg={"white"} borderRadius={10} p={5}
-		       onClick={() => push(Routes.Citizen(citizen.id))} {...props}>
+		       onClick={onClick} {...props}>
 			<Stack direction={isMobile ? "row" : "column"} spacing={5} alignItems={"center"}>
 				<Avatar name={name}>
 					{showBadge && citizen.notifications && (
