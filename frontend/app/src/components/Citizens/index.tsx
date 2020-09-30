24c24ee38a25438030511155eff1a274241df721
Koen Brouwer <contact@kbrouwer.nl>
2020-09-30 13:26:02 +0200

    Enabled Route to CitizenDetail

--- frontend/app/src/components/Citizens/index.tsx
+++ frontend/app/src/components/Citizens/index.tsx
@@ -3,10 +3,11 @@ import {Route} from "react-router-dom";
 import Routes from "../../config/routes";
 import CitizenList from "./CitizenList";
 import CreateCitizen from "./CreateCitizen";
+import CitizenDetail from "./CitizenDetail";
 
 const Citizens = () => (<>
 	<Route exact path={Routes.Citizens} component={CitizenList} />
-	{/*<Route path={Routes.Citizens + "/:id(\\d+)"} component={CitizenDetail} />*/}
+	<Route path={Routes.Citizens + "/:id(\\d+)"} component={CitizenDetail} />
 	<Route path={Routes.CitizenNew} exact component={CreateCitizen} />
 </>);
 
