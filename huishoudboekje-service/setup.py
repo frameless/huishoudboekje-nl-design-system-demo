45e7735ce3c4a41f6fc5a822baeffec0ef231235
Lennart Backus <l.backus@alteza.nl>
2020-10-09 14:26:26 +0200

    added vscode to gitignore and removed databases from setup.py

--- huishoudboekje-service/setup.py
+++ huishoudboekje-service/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='huishoudboekje-service',
     version='0.1.0',
-    packages=['models', 'database', 'core'],
+    packages=['models', 'core'],
     scripts=[],
     url='',
     license='',
