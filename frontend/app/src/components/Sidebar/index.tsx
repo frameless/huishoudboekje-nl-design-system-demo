48384509f56d49b7091291832f322e55be6fe1c9
Koen Brouwer <contact@kbrouwer.nl>
2020-10-15 12:09:51 +0200

    Changed icons to outline styled ones.

--- frontend/app/src/components/Sidebar/index.tsx
+++ frontend/app/src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {BoxProps, Divider, Heading, Stack} from "@chakra-ui/core";
 import SidebarLink from "./SidebarLink";
-import {GrGraphQl, MdAccountBalance, MdCreditCard, MdDashboard, MdPerson, MdShoppingCart} from "react-icons/all";
+import {FaRegBuilding, GrGraphQl, MdCreditCard, RiDashboardLine, RiShoppingCart2Line, RiUserLine} from "react-icons/all";
 import {useTranslate} from "../../config/i18n";
 import Routes from "../../config/routes";
 import {isDev} from "../../utils/things";
@@ -11,17 +11,17 @@ const Sidebar: React.FC<BoxProps> = (props) => {
 
 	return (
 		<Stack spacing={5} p={5} alignSelf={"center"} {...props} width="100%">
-			<SidebarLink exactMatch href={Routes.Dashboard} icon={MdDashboard}>{t("dashboard")}</SidebarLink>
-			<SidebarLink href={Routes.Citizens} icon={MdPerson}>{t("citizens")}</SidebarLink>
-			<SidebarLink href={Routes.Organizations} icon={MdAccountBalance}>{t("organizations")}</SidebarLink>
-			<SidebarLink href={Routes.Balances} icon={MdShoppingCart}>{t("balances")}</SidebarLink>
+			<SidebarLink exactMatch href={Routes.Dashboard} icon={RiDashboardLine}>{t("dashboard")}</SidebarLink>
+			<SidebarLink href={Routes.Citizens} icon={RiUserLine}>{t("citizens")}</SidebarLink>
+			<SidebarLink href={Routes.Organizations} icon={FaRegBuilding}>{t("organizations")}</SidebarLink>
+			<SidebarLink href={Routes.Balances} icon={RiShoppingCart2Line}>{t("balances")}</SidebarLink>
 			<SidebarLink href={Routes.Banking} icon={MdCreditCard}>{t("banking")}</SidebarLink>
 
 			{isDev && (<>
 				<Divider />
 				<Stack spacing={5}>
 					<Heading size={"xs"}>Developer menu</Heading>
-					<SidebarLink as="a" cursor="pointer" href={Routes.GraphiQL} onClick={() => window.location.href = "/api/graphql"} icon={GrGraphQl}>GraphiQL</SidebarLink>
+					<SidebarLink as="a" cursor="pointer" href={Routes.GraphiQL} onClick={() => window.open("/api/graphql")} icon={GrGraphQl}>GraphiQL</SidebarLink>
 				</Stack>
 			</>)}
 		</Stack>
