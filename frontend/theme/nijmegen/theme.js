d985a24d555b6f3c993e26daad24a5568c2c8bc1
Koen Brouwer <contact@kbrouwer.nl>
2020-10-17 01:03:40 +0200

    Added themes for Rotterdam and Nijmegen.

---
+++ frontend/theme/nijmegen/theme.js
@@ -0,0 +1,34 @@
+const primary = {
+	50: "#ffe2f1",
+	100: "#ffb1cf",
+	200: "#ff7faf",
+	300: "#ff4d8f",
+	400: "#fe1e6e",
+	500: "#e50655",
+	600: "#b30042",
+	700: "#81002f",
+	800: "#4f001c",
+	900: "#20000a",
+};
+
+const secondary = {
+	50: "#e1fdf7",
+	100: "#c0f0e6",
+	200: "#9ae4d6",
+	300: "#74d9c6",
+	400: "#51ceb5",
+	500: "#38b49c",
+	600: "#2a8d79",
+	700: "#1b6557",
+	800: "#0a3c34",
+	900: "#001611",
+};
+
+window.branding = {
+	colors: {
+		primary,
+		secondary,
+		red: primary,
+		green: secondary,
+	},
+};
---
+++ frontend/theme/rotterdam/theme.js
@@ -0,0 +1,34 @@
+const primary = {
+	50: "#e2fdec",
+	100: "#bdf2cf",
+	200: "#96e7b2",
+	300: "#6ede94",
+	400: "#46d477",
+	500: "#2ebb5d",
+	600: "#219148",
+	700: "#166832",
+	800: "#083e1d",
+	900: "#001704",
+};
+
+const secondary = {
+	50: "#ebefff",
+	100: "#c9cfeb",
+	200: "#a6aed9",
+	300: "#838ec8",
+	400: "#606eb8",
+	500: "#46549e",
+	600: "#37427c",
+	700: "#262f59",
+	800: "#161c37",
+	900: "#060818",
+};
+
+window.branding = {
+	colors: {
+		primary,
+		secondary,
+		green: primary,
+		blue: secondary,
+	},
+};
