bb0a14ff5fb486ffdce7052b3ef57615f551af6c
Christian Hulleman <c.hulleman@anchormen.nl>
2020-10-28 16:43:32 +0100

    Queries added

--- backend/hhb_backend/graphql/queries/__init__.py
+++ backend/hhb_backend/graphql/queries/__init__.py
@@ -1,8 +1,11 @@
 """ GraphQL schema queries module """
 import graphene
+
 from .gebruikers import GebruikersQuery, GebruikerQuery
 from .organisaties import OrganisatieQuery, OrganisatiesQuery
 from .afspraken import AfspraakQuery, AfsprakenQuery
+from .rekeningen import RekeningenGebruikerQuery, RekeningenOrganisatieQuery
+
 
 class RootQuery(graphene.ObjectType):
     """ The root of all queries """
@@ -12,6 +15,8 @@ class RootQuery(graphene.ObjectType):
     organisaties = OrganisatiesQuery.return_type
     afspraak = AfspraakQuery.return_type
     afspraken = AfsprakenQuery.return_type
+    rekeningengebruiker = RekeningenGebruikerQuery.return_type
+    rekeningenorganisatie = RekeningenOrganisatieQuery.return_type
 
     def resolve_gebruiker(root, info, **kwargs):
         return GebruikerQuery.resolver(root, info, **kwargs)
@@ -25,8 +30,16 @@ class RootQuery(graphene.ObjectType):
     def resolve_organisaties(root, info, **kwargs):
         return OrganisatiesQuery.resolver(root, info, **kwargs)
 
+<<<<<<< HEAD
     def resolve_afspraak(root, info, **kwargs):
         return AfspraakQuery.resolver(root, info, **kwargs)
 
     def resolve_afspraken(root, info, **kwargs):
         return AfsprakenQuery.resolver(root, info, **kwargs)
+=======
+    def resolve_rekeningengebruiker(root, info, **kwargs):
+        return RekeningenGebruikerQuery.resolver(root, info, **kwargs)
+
+    def resolve_rekeningenorganisatie(root, info, **kwargs):
+        return RekeningenOrganisatieQuery.resolver(root, info, **kwargs)
+>>>>>>> 17d6e7d... Queries added
