a0f92c81ad78e0aef14afecae4fb78adf08ef7d9
Christian Hulleman <c.hulleman@anchormen.nl>
2020-11-04 16:39:03 +0100

    Added a customer statement message view

---
+++ transactie-service/core/views/customer_statement_message.py
@@ -0,0 +1,45 @@
+""" MethodView for /customerstatementmessages/ path """
+from models.customer_statement_message import CustomerStatementMessage
+from core.views.hhb_view import HHBView
+
+
+class CustomerStatementMessageView(HHBView):
+    """ Methods for /customerstatementmessages/(<customerstatementmessage_id>) path """
+    hhb_model = CustomerStatementMessage
+    validation_data = {
+        "type": "object",
+        "properties": {
+            "upload_date": {
+                "type": "string",
+                "pattern": "(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})"
+            },
+            "raw_data": {
+                "type": "string",
+            },
+            "transaction_reference_number": {
+                "type": "string",
+            },
+            "related_reference": {
+                "type": "string",
+            },
+            "account_identification": {
+                "type": "string",
+            },
+            "sequence_number": {
+                "type": "string",
+            },
+            "opening_balance": {
+                "type": "integer",
+            },
+            "closing_balance": {
+                "type": "integer",
+            },
+            "closing_available_funds": {
+                "type": "integer",
+            },
+            "forward_available_balance": {
+                "type": "integer",
+            }
+        },
+        "required": []
+    }
