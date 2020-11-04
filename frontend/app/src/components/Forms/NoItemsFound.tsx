54847878b47ae13ba58636c56650930b2c00880d
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-11-04 11:39:52 +0100

    simplified NoItemsFound using DeadEndPage

--- frontend/app/src/components/Forms/NoItemsFound.tsx
+++ frontend/app/src/components/Forms/NoItemsFound.tsx
@@ -1,24 +1,16 @@
 import React, { MouseEventHandler } from "react";
-import { useHistory } from "react-router-dom";
-import { Box, Button, Stack, Text } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
+import DeadEndPage from "../DeadEndPage";
 
-import EmptyIllustration from "../Illustrations/EmptyIllustration";
-
-type NoItemsFoundProps = {
+export const NoItemsFound = ({ onClick, buttonLabel, hint}: {
 	onClick: MouseEventHandler<HTMLButtonElement>
 	buttonLabel: string
 	hint: string
-}
-export const NoItemsFound = ({ onClick, buttonLabel, hint}: NoItemsFoundProps) => {
-	const { push } = useHistory();
-
+}) => {
 	return (
-		<Stack justifyContent={"center"} alignItems={"center"} bg={"white"} borderRadius={5} p={20} spacing={10}>
-			<Box as={EmptyIllustration} maxWidth={[200, 300, 400]} height={"auto"} />
-			<Text fontSize={"sm"}>{hint}</Text>
-			<Button onClick={onClick} size={"sm"} variantColor={"primary"} variant={"solid"}
-					leftIcon={"add"}>{buttonLabel}</Button>
-		</Stack>
+		<DeadEndPage message={hint}>
+			<Button onClick={onClick} size={"sm"} variantColor={"primary"} variant={"solid"} leftIcon={"add"}>{buttonLabel}</Button>
+		</DeadEndPage>
 	);
 };
 
