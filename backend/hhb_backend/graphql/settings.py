a63d16c88bfb1f90b8c6f2dd51243d0087f660dd
Lennart Backus <l.backus@alteza.nl>
2020-11-09 13:53:34 +0000

    Update settings.py

--- backend/hhb_backend/graphql/settings.py
+++ backend/hhb_backend/graphql/settings.py
@@ -3,4 +3,3 @@ import os
 HHB_SERVICES_URL = os.getenv('HHB_SERVICE_URL', "http://localhost:8000")
 ORGANISATIE_SERVICES_URL = os.getenv('ORGANISATIE_SERVICE_URL', "http://localhost:8001")
 TRANSACTIE_SERVICES_URL = os.getenv('TRANSACTIE_SERVICE_URL', "http://localhost:8002")
-g
