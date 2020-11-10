70d137a7728252529c531e39c3d446277ce7279d
Christian Hulleman <c.hulleman@anchormen.nl>
2020-11-10 16:30:00 +0100

    CSM processing

--- backend/hhb_backend/graphql/mutations/customer_statement_messages/create_customer_statement_message.py
+++ backend/hhb_backend/graphql/mutations/customer_statement_messages/create_customer_statement_message.py
@@ -11,27 +11,6 @@ from graphql import GraphQLError
 from hhb_backend.graphql import settings
 
 
-class CustomerStatementMessageServiceModel():
-    upload_date = str
-    raw_data = str
-    # Tag 20
-    transaction_reference_number = str
-    # Tag 21
-    related_reference = str
-    # Tag 25
-    account_identification = str
-    # Tag 28C
-    sequence_number = str
-    # Tag 60f
-    opening_balance = int
-    # Tag 62f
-    closing_balance = int
-    # Tag 64
-    closing_available_funds = int
-    # Tag 65
-    forward_available_balance = int
-
-
 class CreateCustomerStatementMessage(graphene.Mutation):
     class Arguments:
         file = Upload(required=True)
@@ -48,25 +27,47 @@ class CreateCustomerStatementMessage(graphene.Mutation):
         csm_file = mt940.parse(content)
 
         # Fill the csm model
-        csmServiceModel = CustomerStatementMessageServiceModel()
-        csmServiceModel.upload_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        csmServiceModel.raw_data = content
-        csmServiceModel.transaction_reference_number = csm_file.data['transaction_reference']
+        csmServiceModel = {
+            "upload_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "raw_data": content.decode("utf-8")}
+
+        if 'transaction_reference' in csm_file.data and csm_file.data["transaction_reference"] is not None:
+            csmServiceModel["transaction_reference_number"] = csm_file.data['transaction_reference']
+        else:
+            raise GraphQLError(f"Incorrect file, missing tag 20 transaction reference")
+
         # csmServiceModel.related_reference = csm_file.data['??']
-        csmServiceModel.account_identification = csm_file.data['account_identification']
-        csmServiceModel.sequence_number = csm_file.data['sequence_number']
-        csmServiceModel.opening_balance = int(csm_file.data['final_opening_balance'].amount.amount * 100)
-        csmServiceModel.closing_balance = int(csm_file.data['final_closing_balance'].amount.amount * 100)
-        csmServiceModel.closing_available_funds = int(csm_file.data['available_balance'].amount.amount * 100)
-        csmServiceModel.forward_available_balance = int(csm_file.data['forward_available_balance'].amount.amount * 100)
+        if 'account_identification' in csm_file.data and csm_file.data["account_identification"] is not None:
+            csmServiceModel["account_identification"] = csm_file.data['account_identification']
+        else:
+            raise GraphQLError(f"Incorrect file, missing tag 25 account identification")
+
+        if 'sequence_number' in csm_file.data and csm_file.data["sequence_number"] is not None:
+            csmServiceModel["sequence_number"] = csm_file.data['sequence_number']
+
+        if 'final_opening_balance' in csm_file.data and csm_file.data["final_opening_balance"] is not None:
+            csmServiceModel["opening_balance"] = int(csm_file.data['final_opening_balance'].amount.amount * 100)
+
+        if 'final_closing_balance' in csm_file.data and csm_file.data["final_closing_balance"] is not None:
+            csmServiceModel["closing_balance"] = int(csm_file.data['final_closing_balance'].amount.amount * 100)
+
+        if 'available_balance' in csm_file.data and csm_file.data["available_balance"] is not None:
+            csmServiceModel["closing_available_funds"] = int(csm_file.data['available_balance'].amount.amount * 100)
+
+        if 'forward_available_balance' in csm_file.data and csm_file.data["forward_available_balance"] is not None:
+            csmServiceModel["forward_available_balance"] = int(
+                csm_file.data['forward_available_balance'].amount.amount * 100)
 
         # Send the model
         org_service_response = requests.post(
-            f"{settings.TRANSACTIE_SERVICES_URL}/transacties/",
-            data=json.dumps(csmServiceModel.__dict__),
+            f"{settings.TRANSACTIE_SERVICES_URL}/customerstatementmessages/",
+            data=json.dumps(csmServiceModel),
             headers={'Content-type': 'application/json'}
         )
         if org_service_response.status_code != 201:
             raise GraphQLError(f"Upstream API responded: {org_service_response.json()}")
+
+        # Add banktransactions
+        # Post banktransactions
 
         return CreateCustomerStatementMessage(ok=True)
