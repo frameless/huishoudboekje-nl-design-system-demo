161307235f2046784586248158fb1a90f3668aae
Lennart Backus <l.backus@alteza.nl>
2020-11-11 15:23:55 +0100

    Fixed interval issue

--- backend/hhb_backend/utils.py
+++ backend/hhb_backend/utils.py
@@ -3,21 +3,22 @@ import re
 def convert_hhb_interval_to_iso(graphql_format: dict):
     if any (graphql_format.get(k,None) for k in ('jaren', 'maanden','weken','dagen')):
         isoformat = "P"
-        if graphql_format.get('jaren', None):
-            isoformat += f"{graphql_format.get('jaren')}Y"
-        if graphql_format.get('maanden', None):
-            isoformat += f"{graphql_format.get('maanden')}M"
-        if graphql_format.get('weken', None):
-            isoformat += f"{graphql_format.get('weken')}W"
-        if graphql_format.get('dagen', None):
-            isoformat += f"{graphql_format.get('dagen')}D"
+        isoformat += f"{graphql_format.get('jaren', 0)}Y"
+        isoformat += f"{graphql_format.get('maanden', 0)}M"
+        isoformat += f"{graphql_format.get('weken', 0)}W"
+        isoformat += f"{graphql_format.get('dagen', 0)}D"
         return isoformat
     return None
 
 def convert_hhb_interval_to_dict(iso_format: str):
+    print(type(iso_format))
+    print(iso_format)
     if iso_format:
+        print("if")
         p = re.compile(r'P(\d+)Y(\d+)M(\d+)W(\d+)D').findall(iso_format)
         if p:
+            print("=====")
+            print(p)
             return {
                 "jaren": int(p[0][0]),
                 "maanden": int(p[0][1]),
