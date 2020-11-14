93bba1ecbd3050e3b19eb3964f1c3226d76e4716
Koen Brouwer <contact@kbrouwer.nl>
2020-11-14 17:12:00 +0100

    Fixed mobile view.

--- frontend/app/src/components/Agreements/AfpraakItem.tsx
+++ frontend/app/src/components/Agreements/AfpraakItem.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
-import {Badge, Box, BoxProps, Button, IconButton, Stack} from "@chakra-ui/core";
+import {Badge, Box, BoxProps, Button, IconButton, Stack, Text} from "@chakra-ui/core";
 import {useIsMobile} from "react-grapple";
 import {IAfspraak} from "../../models";
 import {currencyFormat, Interval} from "../../utils/things";
 import {useTranslation} from "react-i18next";
 import Routes from "../../config/routes";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import GridCard from "../GridCard";
 
 const AfspraakItem: React.FC<BoxProps & { afspraak: IAfspraak, onToggleActive?: (id: number) => void, onDelete?: (id: number) => void }> = ({afspraak: a, onToggleActive, onDelete, ...props}) => {
 	const isMobile = useIsMobile();
@@ -48,12 +49,30 @@ const AfspraakItem: React.FC<BoxProps & { afspraak: IAfspraak, onToggleActive?: 
 
 	const onClickEditButton = () => push(Routes.EditAgreement(a.id));
 
-	return (
-		<Stack direction={isMobile ? "column" : "row"} alignItems={"center"} {...props}>
+	return isMobile ? (
+		<GridCard cursor={"default"} {...props}>
+			<Stack direction={"row"} width={"100%"} alignItems={"center"} justifyContent={"center"}>
+				<Stack spacing={1} flex={1}>
+					<Text>{a.beschrijving}</Text>
+					<Text fontSize={"14px"} color={"gray.500"}>{a.organisatie?.weergaveNaam || a.tegenRekening?.rekeninghouder || t("unknown")}</Text>
+				</Stack>
+				<Stack spacing={1} flex={1} alignItems={"flex-end"}>
+					<Box flex={1}>{currencyFormat.format(a.bedrag)}</Box>
+					<Box flex={1}>
+						<Badge fontSize={"10px"}>{intervalString()}</Badge>
+					</Box>
+				</Stack>
+			</Stack>
+			{/*	Todo: edit and delete actions on mobile? (14-11-2020) */}
+		</GridCard>
+	) : (
+		<Stack direction={"row"} alignItems={"center"} justifyContent={"center"} {...props}>
 			<Box flex={1}>{a.organisatie?.weergaveNaam || a.tegenRekening?.rekeninghouder || t("unknown")}</Box>
 			<Box flex={1}>{a.beschrijving}</Box>
-			<Box flex={1} textAlign={"right"}>{currencyFormat.format(a.bedrag)}</Box>
-			<Box flex={1}>{intervalString()}</Box>
+			<Stack spacing={1} flex={1} alignItems={"flex-end"}>
+				<Box textAlign={"right"}>{currencyFormat.format(a.bedrag)}</Box>
+				<Badge fontSize={"10px"}>{intervalString()}</Badge>
+			</Stack>
 			<Stack width={100} spacing={2} isInline justifyContent={"center"}>
 				{onToggleActive ? (
 					<Button size={"xs"} variantColor={a.actief ? "green" : "gray"} {...onToggleActive && {cursor: "pointer"}}
