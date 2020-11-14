d06e2ad30af84853e806178d56b9a2ea15c17ce8
Koen Brouwer <contact@kbrouwer.nl>
2020-11-14 21:19:24 +0100

    Whatever. Skip git hash for now.

--- frontend/app/version.js
+++ frontend/app/version.js
@@ -1,11 +1,7 @@
 const pkg = require("./package.json");
 const {resolve, relative} = require("path");
 const {writeFileSync, existsSync, mkdirSync} = require("fs");
-const {gitDescribeSync} = require("git-describe");
-const {hash} = gitDescribeSync();
-
 const {version} = pkg;
-const rawVersion = `${version} ${hash ? `(${hash})` : ""}`;
 
 if (!existsSync(resolve(__dirname, "src"))) {
 	mkdirSync(resolve(__dirname, "src"))
@@ -13,9 +9,9 @@ if (!existsSync(resolve(__dirname, "src"))) {
 const file = resolve(__dirname, "src", "version.ts");
 const data = `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECK IN!
 /* eslint-disable */
-const VERSION = "${rawVersion}";
+const VERSION = "${version}";
 export default VERSION;
 /* eslint-enable */`;
 writeFileSync(file, data, {encoding: "utf-8"});
 
-console.log(`Version info ${rawVersion} saved in ${relative(resolve(__dirname, ".."), file)}`);
+console.info(`Version info ${version} saved in ${relative(resolve(__dirname, ".."), file)}`);
