723f237e8b314f6fc3802e7a19befc8c07da5831
Koen Brouwer <contact@kbrouwer.nl>
2020-11-16 10:52:14 +0100

    Removed Sidebar item and Route to Dashboard.

--- frontend/app/src/components/Sidebar/index.tsx
+++ frontend/app/src/components/Sidebar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {BoxProps, Divider, Heading, Stack} from "@chakra-ui/core";
+import {BoxProps, Divider, Stack} from "@chakra-ui/core";
 import SidebarLink from "./SidebarLink";
-import {FaRegBuilding, GrGraphQl, MdCreditCard, RiDashboardLine, RiShoppingCart2Line, RiUserLine} from "react-icons/all";
+import {FaRegBuilding, GrGraphQl, MdCreditCard, RiShoppingCart2Line, RiUserLine} from "react-icons/all";
 import {useTranslate} from "../../config/i18n";
 import Routes from "../../config/routes";
 import {isDev} from "../../utils/things";
@@ -11,7 +11,6 @@ const Sidebar: React.FC<BoxProps> = (props) => {
 
 	return (
 		<Stack spacing={5} p={5} alignSelf={"center"} {...props} width="100%">
-			<SidebarLink exactMatch href={Routes.Dashboard} icon={RiDashboardLine}>{t("sidebar.dashboard")}</SidebarLink>
 			<SidebarLink href={Routes.Burgers} icon={RiUserLine}>{t("burgers.burgers")}</SidebarLink>
 			<SidebarLink href={Routes.Organizations} icon={FaRegBuilding}>{t("organizations.organizations")}</SidebarLink>
 			<SidebarLink href={Routes.Balances} icon={RiShoppingCart2Line}>{t("balances.balances")}</SidebarLink>
@@ -20,10 +19,7 @@ const Sidebar: React.FC<BoxProps> = (props) => {
 			{isDev && (
 				<Stack spacing={5}>
 					<Divider />
-					<Stack spacing={5}>
-						<Heading size={"xs"}>Developer menu</Heading>
-						<SidebarLink as="a" cursor="pointer" href={Routes.GraphiQL} onClick={() => window.open(Routes.GraphiQL)} icon={GrGraphQl}>GraphiQL</SidebarLink>
-					</Stack>
+					<SidebarLink as="a" cursor="pointer" href={Routes.GraphiQL} onClick={() => window.open(Routes.GraphiQL)} icon={GrGraphQl}>GraphiQL</SidebarLink>
 				</Stack>
 			)}
 		</Stack>
