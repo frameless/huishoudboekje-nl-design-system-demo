ae9819ce49cc44172995943c859103a1e1538c06
Koen Brouwer <contact@kbrouwer.nl>
2020-11-23 12:36:49 +0100

    Removed tab navigation buttons, and shoved them into the sidebar.

--- frontend/app/src/components/Banking/index.tsx
+++ frontend/app/src/components/Banking/index.tsx
@@ -1,30 +1,13 @@
-import {Heading, PseudoBoxProps, Stack, Tab, TabList, Tabs} from "@chakra-ui/core";
+import {Heading, Stack} from "@chakra-ui/core";
 import React from "react";
 import {useTranslation} from "react-i18next";
-import {NavLink, NavLinkProps, Redirect, Route, Switch, useLocation} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Routes from "../../config/routes";
 import CustomerStatementMessages from "./CustomerStatementMessages";
 import Transactions from "./Transactions";
 
-const tabRoutes = [
-	Routes.Transactions,
-	Routes.CSMs
-];
-
-type TabProps = PseudoBoxProps & React.ButtonHTMLAttributes<any>;
-type NavLinkTabProps = TabProps & NavLinkProps;
-const NavLinkTab: React.FC<NavLinkTabProps> = React.forwardRef(
-	(props: NavLinkTabProps, ref: React.Ref<any>) => {
-		return <Tab ref={ref} as={NavLink} {...props} />;
-	}
-);
-
 const Banking = () => {
 	const {t} = useTranslation();
-	const location = useLocation();
-
-	const defaultTabRouteIdx = tabRoutes.findIndex(l => location.pathname.includes(l));
-	const defaultTabRoute = defaultTabRouteIdx > -1 ? defaultTabRouteIdx : 0;
 
 	return (
 		<Stack spacing={5}>
@@ -32,13 +15,6 @@ const Banking = () => {
 				<Stack direction={"row"} spacing={5} alignItems={"center"}>
 					<Heading size={"lg"}>{t("banking.banking")}</Heading>
 				</Stack>
-
-				<Tabs defaultIndex={defaultTabRoute}>
-					<TabList>
-						<NavLinkTab to={tabRoutes[0]}>{t("banking.transactions")}</NavLinkTab>
-						<NavLinkTab to={tabRoutes[1]}>{t("banking.customerStatementMessages")}</NavLinkTab>
-					</TabList>
-				</Tabs>
 			</Stack>
 
 			<Switch>
@@ -47,9 +23,6 @@ const Banking = () => {
 				</Route>
 				<Route path={Routes.CSMs}>
 					<CustomerStatementMessages />
-				</Route>
-				<Route>
-					<Redirect to={tabRoutes[defaultTabRoute]} />
 				</Route>
 			</Switch>
 		</Stack>
