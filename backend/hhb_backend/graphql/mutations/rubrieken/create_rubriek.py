7226a96bcacf1bac99f8e144ef246209b5e5851c
Lennart Backus <l.backus@alteza.nl>
2020-12-02 13:54:20 +0100

    Made the same code more compact

--- backend/hhb_backend/graphql/mutations/rubrieken/create_rubriek.py
+++ backend/hhb_backend/graphql/mutations/rubrieken/create_rubriek.py
@@ -17,10 +17,8 @@ class CreateRubriek(graphene.Mutation):
 
     def mutate(root, info, **kwargs):
         """ Create the new Rubriek """
-        if kwargs["grootboekrekening_id"]:
-            grootboek_check = requests.get(f"{settings.GROOTBOEK_SERVICE_URL}/grootboekrekeningen/{kwargs['grootboekrekening_id']}")
-            if grootboek_check.status_code != 200:
-                raise GraphQLError(f"Grootboekrekening id [{kwargs['grootboekrekening_id']}] not found.")
+        if kwargs["grootboekrekening_id"] and requests.get(f"{settings.GROOTBOEK_SERVICE_URL}/grootboekrekeningen/{kwargs['grootboekrekening_id']}").status_code != 200:
+            raise GraphQLError(f"Grootboekrekening id [{kwargs['grootboekrekening_id']}] not found.")
         post_response = requests.post(
             f"{settings.HHB_SERVICES_URL}/rubrieken/",
             data=json.dumps(kwargs, default=str),
--- backend/hhb_backend/graphql/mutations/rubrieken/update_rubriek.py
+++ backend/hhb_backend/graphql/mutations/rubrieken/update_rubriek.py
@@ -19,10 +19,8 @@ class UpdateRubriek(graphene.Mutation):
     def mutate(root, info, **kwargs):
         """ Update a Rubriek """
         rubriek_id = kwargs.pop("id")
-        if kwargs["grootboekrekening_id"]:
-            grootboek_check = requests.get(f"{settings.GROOTBOEK_SERVICE_URL}/grootboekrekeningen/{kwargs['grootboekrekening_id']}")
-            if grootboek_check.status_code != 200:
-                raise GraphQLError(f"Grootboekrekening id [{kwargs['grootboekrekening_id']}] not found.")
+        if kwargs["grootboekrekening_id"] and requests.get(f"{settings.GROOTBOEK_SERVICE_URL}/grootboekrekeningen/{kwargs['grootboekrekening_id']}").status_code != 200:
+            raise GraphQLError(f"Grootboekrekening id [{kwargs['grootboekrekening_id']}] not found.")
         post_response = requests.post(
             f"{settings.HHB_SERVICES_URL}/rubrieken/{rubriek_id}",
             data=json.dumps(kwargs),
