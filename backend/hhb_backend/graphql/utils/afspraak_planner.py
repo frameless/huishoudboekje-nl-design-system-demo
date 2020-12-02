a9a275e7f2665e7d41dfd982a63db8302e5baf15
Lennart Backus <l.backus@alteza.nl>
2020-12-02 15:18:30 +0100

    First version overschijving plannen, now with unittests

--- backend/hhb_backend/graphql/utils/afspraak_planner.py
+++ backend/hhb_backend/graphql/utils/afspraak_planner.py
@@ -6,35 +6,44 @@ from hhb_backend.graphql.utils import convert_hhb_interval_to_relativetime
 def planned_overschrijvingen(afspraak, start_datum=None, eind_datum=None):
     if not eind_datum:
         eind_datum = datetime.now().date()
-
     payments = []
     payment_date = isoparse(afspraak["start_datum"]).date()
     time_delta = convert_hhb_interval_to_relativetime(afspraak["interval"])
     if afspraak["aantal_betalingen"] > 0:
-        payment_per_period = floor(afspraak["bedrag"] / afspraak["aantal_betalingen"])
-        rest_payment = afspraak["bedrag"] % afspraak["aantal_betalingen"]
-        for payment_id in range(afspraak["aantal_betalingen"]):
-            payment_amount = payment_per_period
-            if payment_id == afspraak["aantal_betalingen"] - 1:
-                payment_amount += rest_payment
-            if not start_datum or payment_date >= start_datum:
-                payments.append({
-                    "id": payment_id,
-                    "bedrag": payment_amount,
-                    "datum": payment_date,
-                })
-            payment_date = payment_date + time_delta
-            if eind_datum and payment_date > eind_datum:
-                break
+        payments += get_normal_afspraak_overschijvingen(afspraak, payment_date, time_delta, start_datum, eind_datum)
     else:
-        payment_id = 0
-        while payment_date <= eind_datum:
-            if not start_datum or payment_date >= start_datum:
-                payments.append({
-                    "id": payment_id,
-                    "bedrag": afspraak["bedrag"],
-                    "datum": payment_date,
-                })
-            payment_date = payment_date + time_delta
-            payment_id += 1
+        payments += get_doorlopende_afspraak_overschijvingen(afspraak, payment_date, time_delta, start_datum, eind_datum)
+
     return payments
+
+def get_normal_afspraak_overschijvingen(afspraak, payment_date, time_delta, start_datum=None, eind_datum=None):
+    payments = []
+    payment_per_period = floor(afspraak["bedrag"] / afspraak["aantal_betalingen"])
+    rest_payment = afspraak["bedrag"] % afspraak["aantal_betalingen"]
+    for payment_id in range(afspraak["aantal_betalingen"]):
+        payment_amount = payment_per_period
+        if payment_id == afspraak["aantal_betalingen"] - 1:
+            payment_amount += rest_payment
+        if not start_datum or payment_date >= start_datum:
+            payments.append(make_overschijving_dict(payment_amount, payment_date))
+        payment_date = payment_date + time_delta
+        if eind_datum and payment_date > eind_datum:
+            break
+    return payments
+
+def get_doorlopende_afspraak_overschijvingen(afspraak, payment_date, time_delta, start_datum=None, eind_datum=None):
+    payments = []
+    payment_id = 0
+    while payment_date <= eind_datum:
+        if not start_datum or payment_date >= start_datum:
+            payments.append(make_overschijving_dict(afspraak["bedrag"], payment_date))
+        payment_date = payment_date + time_delta
+        payment_id += 1
+    return payments
+
+def make_overschijving_dict(bedrag, date):
+    return {
+        "id": None,
+        "bedrag": bedrag,
+        "datum": date,
+    }
--- backend/tests/graphql/queries/test_afspraak.py
+++ backend/tests/graphql/queries/test_afspraak.py
@@ -1,5 +1,6 @@
 
 import requests_mock
+from freezegun import freeze_time
 from hhb_backend.graphql import settings
 
 def test_afspraken(client):
@@ -72,3 +73,83 @@ def test_afspraak_empty_interval(client):
             }]
         }}
 
+@freeze_time("2020-01-01")
+def test_afspraak_overschijvingen_planner_normal(client):
+    with requests_mock.Mocker() as rm:
+        rm.get(f"{settings.HHB_SERVICES_URL}/afspraken/", json={'data': [{
+            'id': 1,
+            'interval': "P0Y1M0W0D",
+            'start_datum': "2020-01-01",
+            'aantal_betalingen': 10,
+            'bedrag': 1011
+        }]})
+        response = client.post(
+            "/graphql",
+            json={"query": '''{ afspraken(ids:[1]) { overschrijvingen(startDatum: "2020-01-01", eindDatum: "2021-01-01") { datum bedrag status } } }'''},
+            content_type='application/json'
+        )
+        print(response.json)
+        assert response.json['data']['afspraken'][0]['overschrijvingen'] == [
+            {'datum': '2020-01-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-02-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-03-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-04-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-05-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-06-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-07-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-08-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-09-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-10-01', 'bedrag': '1.02', 'status': 'VERWACHTING'}
+        ]
+
+@freeze_time("2020-01-01")
+def test_afspraak_overschijvingen_planner_no_einddatum(client):
+    with requests_mock.Mocker() as rm:
+        rm.get(f"{settings.HHB_SERVICES_URL}/afspraken/", json={'data': [{
+            'id': 1,
+            'interval': "P0Y1M0W0D",
+            'start_datum': "2020-01-01",
+            'aantal_betalingen': 10,
+            'bedrag': 1011
+        }]})
+        response = client.post(
+            "/graphql",
+            json={"query": '''{ afspraken(ids:[1]) { overschrijvingen(startDatum: "2020-01-01") { datum bedrag status } } }'''},
+            content_type='application/json'
+        )
+        print(response.json)
+        assert response.json['data']['afspraken'][0]['overschrijvingen'] == [
+            {'datum': '2020-01-01', 'bedrag': '1.01', 'status': 'VERWACHTING'}
+        ]
+
+
+@freeze_time("2020-01-01")
+def test_afspraak_overschijvingen_planner_doorlopened(client):
+    with requests_mock.Mocker() as rm:
+        rm.get(f"{settings.HHB_SERVICES_URL}/afspraken/", json={'data': [{
+            'id': 1,
+            'interval': "P0Y1M0W0D",
+            'start_datum': "2020-01-01",
+            'aantal_betalingen': 0,
+            'bedrag': 101
+        }]})
+        response = client.post(
+            "/graphql",
+            json={"query": '''{ afspraken(ids:[1]) { overschrijvingen(startDatum: "2020-02-01", eindDatum: "2021-01-01") { datum bedrag status } } }'''},
+            content_type='application/json'
+        )
+        print(response.json)
+        assert response.json['data']['afspraken'][0]['overschrijvingen'] == [
+            {'datum': '2020-02-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-03-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-04-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-05-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-06-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-07-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-08-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-09-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-10-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-11-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2020-12-01', 'bedrag': '1.01', 'status': 'VERWACHTING'},
+            {'datum': '2021-01-01', 'bedrag': '1.01', 'status': 'VERWACHTING'}
+        ]
