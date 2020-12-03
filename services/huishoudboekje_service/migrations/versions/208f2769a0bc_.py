3eb962c61bf00e938079fc50e52b55236525ffd4
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-12-03 16:12:01 +0100

    merge alembic revisions

---
+++ services/huishoudboekje_service/migrations/versions/208f2769a0bc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 208f2769a0bc
+Revises: dbbfa0d519b6, 2db81b1fda15
+Create Date: 2020-12-03 16:11:53.198347
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '208f2769a0bc'
+down_revision = ('dbbfa0d519b6', '2db81b1fda15')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
