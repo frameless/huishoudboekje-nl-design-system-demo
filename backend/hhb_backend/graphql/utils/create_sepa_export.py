ae2d20e0b54eb9d57bf7ea92be6fc763b4e80610
Christian Hulleman <c.hulleman@anchormen.nl>
2020-12-09 18:57:39 +0100

    Sepa export gets the objects correctly

--- backend/hhb_backend/graphql/utils/create_sepa_export.py
+++ backend/hhb_backend/graphql/utils/create_sepa_export.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import requests
+from flask import request
 from graphql import GraphQLError
 from sepaxml import SepaTransfer
 
@@ -8,6 +9,14 @@ from hhb_backend.graphql import settings
 
 
 def create_export_string(overschrijvingen, export) -> str:
+    # TODO get overschrijvingen by export
+
+    afspraken_ids = list(set([overschrijving['afspraak_id'] for overschrijving in overschrijvingen]))
+    afspraken = await request.dataloader.afspraken_by_id.load_many(afspraken_ids)
+    # Get all tegen_rekeningen
+    tegen_rekeningen_ids = [afspraak_result['tegen_rekening_id'] for afspraak_result in afspraken]
+    tegen_rekeningen = await request.dataloader.rekeningen_by_id.load_many(tegen_rekeningen_ids)
+
     config = {
         "name": "Huishoudboekje " + get_config_value("gemeente_naam"),
         "IBAN": get_config_value("gemeente_iban"),
@@ -18,13 +27,15 @@ def create_export_string(overschrijvingen, export) -> str:
     sepa = SepaTransfer(config, clean=True)
 
     for overschrijving in overschrijvingen:
+        afspraak = next(filter(lambda x: x['id'] == overschrijving['id'], afspraken), None)
+        tegen_rekening = next(filter(lambda x: x['id'] == afspraak['tegen_rekening_id'], tegen_rekeningen), None)
         payment = {
-            "name": overschrijving["rekenhouder"],
-            "IBAN": overschrijving["iban"],
+            "name": tegen_rekening["rekenhouder"],
+            "IBAN": tegen_rekening["iban"],
             "BIC": "BANKNL2A", # TODO nodig??
             "amount": overschrijving['bedrag'],
-            "execution_date": overschrijving['datum'],
-            "description": overschrijving['beschrijving'],
+            "execution_date": datetime.strptime(overschrijving['datum'], '%Y-%m-%d'),
+            "description": afspraak['beschrijving'],
             # "endtoend_id": str(uuid.uuid1())  # optional
         }
         sepa.add_payment(payment)
