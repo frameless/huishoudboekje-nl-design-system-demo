a60e92703d783ad69bb916ed069ed9e50f3d2a4f
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-12-18 15:34:51 +0100

    wip with scenarios

--- services/huishoudboekje_service/sample_data/generators/gen_gebruikers.py
+++ services/huishoudboekje_service/sample_data/generators/gen_gebruikers.py
@@ -1,117 +1,166 @@
+import csv
+import dataclasses
+import os
 import string
-import sys 
+import sys
+from _csv import QUOTE_MINIMAL
 from os import path
 from random import choice, randrange
 import requests
+from schwifty import IBAN
+from datetime import date, timedelta
+
+from sample_data.generators.scenarios import Scenario, GebruikerScenario
 
 if not path.exists("../context/straatnamen.txt"):
     sys.exit("Missing required data: straatnamen")
 with open("../context/straatnamen.txt", "r") as fh:
     straten = fh.readlines()
 
-class GebruikerGenerator:
 
+def faker_datum(start_date: date = date(1950, 1, 1), end_date: date = date(1999, 12, 31)):
+    return start_date + timedelta(days=randrange((end_date - start_date).days))
+
+
+def faker_postcode():
+    return f"13{randrange(1, 9)}{randrange(1, 9)}{choice(string.ascii_letters).upper()}{choice(string.ascii_letters).upper()}"
+
+
+def create_rekening(naam: str, id: int):
+    return {
+        "id": id,
+        "iban": IBAN.generate('NL', bank_code='BANK', account_code=str(id)),
+        "rekeninghouder": naam
+    }
+
+
+def create_gebruiker(id: int):
+    response = requests.get("http://faker.hook.io/?property=helpers.createCard&locale=nl").json()
+    achternaam = response["name"].split(" ")[-1]
+    voornamen = " ".join(response["name"].split(" ")[:-1])
+    voorletters = " ".join([n[0] for n in response["name"].split(" ")[:-1]])
+    return {
+        "id": id,
+        "telefoonnummer": response["phone"],
+        "email": response["email"],
+        "geboortedatum": faker_datum(),
+        "voorletters": voorletters,
+        "voornamen": voornamen,
+        "achternaam": achternaam,
+        "straatnaam": choice(straten).strip(),
+        "huisnummer": randrange(1, 9999),
+        "postcode": faker_postcode(),
+        "plaatsnaam": "Sloothuizen"
+    }
+
+
+def create_gebruiker_rekening(gebruiker, rekening):
+    return {
+        "rekening_id": rekening["id"],
+        "gebruiker_id": gebruiker["id"],
+    }
+
+
+def dict_keys_subset_builder(match_keys: list):
+    """only include items with a matching key"""
+    return lambda actual_dict: dict((k, actual_dict[k] if k in actual_dict else None) for k in match_keys)
+
+class HHBCsvDialect(csv.Dialect):
+    delimiter = '\t'
+    quoting = QUOTE_MINIMAL
+    quotechar = '"'
+    lineterminator = '\n'
+
+class Generator:
+    fieldnames = {
+        "gebruikers": ["id", "telefoonnummer", "email", "geboortedatum", "voorletters", "voornamen", "achternaam",
+                       "straatnaam", "huisnummer", "postcode", "plaatsnaam"],
+        "rekeningen": ["id", "iban", "rekeninghouder"],
+        "gebruiker_rekeningen": ["rekening_id", "gebruiker_id"],
+        "kvk_details": ["kvk_nummer", "naam", "straatnaam", "huisnummer", "postcode", "plaatsnaam"],
+        "organisaties": ["id", "kvk_nummer", "naam"],
+        "organisatie_rekenignen": ["organisatie_id", "gebruiker_id"],
+        "configuratie": ["id", "waarde"],
+        "rubrieken": ["naam", "grootboekrekening_id"],
+    }
+    rubrieken = []
+    configuratie = []
     gebruikers = []
     rekeningen = []
+    afspraken = []
     gebruiker_rekeningen = []
 
-    rekening_counter = 40001
-    gebruiker_counter = 1
+    rekening_counter = 40200
+    gebruiker_counter = 200
+    kvk_number_counter = 462345
 
-    @property
-    def __faker_datum(self):
-        return f"19{str(randrange(1, 99)).zfill(2)}-{str(randrange(1, 12)).zfill(2)}-{str(randrange(1, 30)).zfill(2)}"
+    def __init__(self, scenario: Scenario):
+        self.scenario = scenario
 
-    @property
-    def __faker_postcode(self):
-        return f"13{randrange(1, 9)}{randrange(1, 9)}{choice(string.ascii_letters).upper()}{choice(string.ascii_letters).upper()}"
+    def generate(self):
+        self.rubrieken = map(dataclasses.asdict, self.scenario.configuratie.rubrieken)
+        self.configuratie = map(dataclasses.asdict,self.scenario.configuratie.configuratie)
+        for organisatie_scenario in self.scenario.organisatie.scenarios:
+            # self.generate_organisaties(organisatie_scenario)
+            pass
 
-
-    def create_gebruiker(self):
-        response = requests.get("http://faker.hook.io/?property=helpers.createCard&locale=nl").json()
-        achternaam = response["name"].split(" ")[-1]
-        voornamen = " ".join(response["name"].split(" ")[:-1])
-        voorletters = " ".join([n[0] for n in response["name"].split(" ")[:-1]])
-        gebruiker = {
-            "id": self.gebruiker_counter,
-            "telefoonnummer": response["phone"],
-            "email": response["email"],
-            "geboortedatum": self.__faker_datum,
-            "voorletters": voorletters,
-            "voornamen": voornamen,
-            "achternaam": achternaam,
-            "straatnaam": choice(straten).strip(),
-            "huisnummer": randrange(1, 9999),
-            "postcode": self.__faker_postcode,
-            "plaatsnaam": "Sloothuizen"
-        }
-        self.gebruiker_counter += 1
-        return gebruiker
+        for gebruiker_scenario in self.scenario.gebruikers.scenarios:
+            self.generate_gebruikers(gebruiker_scenario)
 
     def generate_gebruiker_rekeningen(self, gebruiker_id, naam):
-        self.rekeningen.append({
-            "id":  self.rekening_counter,
-            "iban": "NL00BANK00" + str(self.rekening_counter),
-            "rekeninghouder": naam
-        })
-        
+
+        rekening = self.generate_rekening(naam)
+        self.rekening_counter += 1
+
         self.gebruiker_rekeningen.append({
-            "rekening_id": self.rekening_counter,
+            "rekening_id": rekening["id"],
             "gebruiker_id": gebruiker_id
         })
-        self.rekening_counter += 1
-        
-    def generate_gebruikers(self, amount: int = 100):
-        for i in range(amount):
-            gebruiker = self.create_gebruiker()
+
+    def generate_gebruikers(self, scenario: GebruikerScenario):
+        for _ in range(scenario.gebruikers_scenario.aantal):
+            self.gebruiker_counter += 1
+            gebruiker = create_gebruiker(id=self.gebruiker_counter)
             self.gebruikers.append(gebruiker)
-            self.generate_gebruiker_rekeningen(gebruiker["id"], gebruiker["achternaam"])
 
-    def get_csv_rekeningen(self):
-        csv_data = ""
-        for rekening in self.rekeningen:
-            csv_data += f'{rekening["id"]}\t{rekening["iban"]}\t{rekening["rekeninghouder"]}\n'
-        return csv_data
+            for _ in range(scenario.aantal_rekeningen):
+                self.rekening_counter += 1
+                rekening = create_rekening(id=self.rekening_counter, naam=gebruiker["achternaam"])
+                self.rekeningen.append(rekening)
 
-    def get_csv_gebruiker_rekenignen(self):
-        csv_data = ""
-        for rekening_rel in self.gebruiker_rekeningen:
-            csv_data += f'{rekening_rel["rekening_id"]}\t{rekening_rel["gebruiker_id"]}\n'
-        return csv_data
-    
-    def get_csv_huishoudboekje_db(self):
-        csv_data = ""
-        for gb in self.gebruikers:
-            csv_data += f'{gb["id"]}\t{gb["telefoonnummer"]}\t{gb["email"]}\t{gb["geboortedatum"]}\t{gb["voorletters"]}\t{gb["voornamen"]}\t{gb["achternaam"]}\t{gb["straatnaam"]}\t{gb["huisnummer"]}\t{gb["postcode"]}\t{gb["plaatsnaam"]}\t\n'
-        return csv_data
-    
-    def save_csvs(self):
-        self.save_huishoudboekje_db()
-        self.save_rekeningen()
-        self.save_rekening_relations()
+                gebruiker_rekening = create_gebruiker_rekening(gebruiker=gebruiker, rekening=rekening)
+                self.gebruiker_rekeningen.append(gebruiker_rekening)
 
-    def save_huishoudboekje_db(self):
-        if path.exists("../huishoudboekje_db/gebruikers.txt"):
-            print("data for huishoudboekje_db  gebruikers already exists.")
+            # for afspraak_scenario in scenario.afspraken:
+            #     gebruiker.afspraken.append(self.generate_afspraak)
+
+            self.gebruikers.append(gebruiker)
+
+    def save(self):
+        self.save_csv(db='huishoudboekje_db', name='configuratie')
+        self.save_csv(db='huishoudboekje_db', name='rubrieken')
+        self.save_csv(db='huishoudboekje_db', name='gebruikers')
+        self.save_csv(db='huishoudboekje_db', name='rekeningen')
+        self.save_csv(db='huishoudboekje_db', name='gebruiker_rekeningen')
+        self.save_csv(db='huishoudboekje_db', name='organisaties')
+
+    def save_csv(self, db='huishoudboekje_db', name='gebruikers', fieldnames=None, data=None):
+        if path.exists(f"../{db}/{name}.txt"):
+            print(f"data for {db}:{name} already exists.")
         else:
-            with open("../huishoudboekje_db/gebruikers.txt", "w") as out_file:
-                out_file.write(self.get_csv_huishoudboekje_db())
+            os.makedirs(f"../{db}", exist_ok=True)
+            with open(f"../{db}/{name}.txt", "w") as out_file:
+                fieldnames = fieldnames or self.fieldnames[name]
+                writer = csv.DictWriter(
+                    out_file,
+                    fieldnames=fieldnames,
+                    dialect=HHBCsvDialect
+                )
+                writer.writeheader()
+                writer.writerows(map(dict_keys_subset_builder(fieldnames), data or self.__dict__[name]))
 
-    def save_rekeningen(self):
-        if path.exists("../huishoudboekje_db/rekeningen_gebruikers.txt"):
-            print("data for huishoudboekje_db  rekenignen_gebruikers already exists.")
-        else:
-            with open("../huishoudboekje_db/rekeningen_gebruikers.txt", "w") as out_file:
-                out_file.write(self.get_csv_rekeningen())
 
-    def save_rekening_relations(self):
-        if path.exists("../huishoudboekje_db/rekeningen_gebruikers_rel.txt"):
-            print("data for huishoudboekje_db  rekenignen_gebruikers_rel already exists.")
-        else:
-            with open("../huishoudboekje_db/rekeningen_gebruikers_rel.txt", "w") as out_file:
-                out_file.write(self.get_csv_gebruiker_rekenignen())
-
-gb = GebruikerGenerator()
-gb.generate_gebruikers()
-gb.save_csvs()
-
+scenario = Scenario()
+generator = Generator(scenario)
+generator.generate()
+generator.save()
---
+++ services/huishoudboekje_service/sample_data/generators/scenarios.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass, field
+from pprint import pprint
+from typing import List
+
+from yaml import load, SafeLoader
+from marshmallow_dataclass import class_schema
+
+
+@dataclass
+class ConfiguratieItem:
+    id: str
+    waarde: str
+
+
+@dataclass
+class Rubriek:
+    naam: str
+    grootboekrekening_id: str
+
+
+@dataclass
+class Systeem:
+    configuratie: List[ConfiguratieItem] = field(default_factory=list)
+    rubrieken: List[Rubriek] = field(default_factory=list)
+
+
+@dataclass
+class Rekening:
+    iban: str
+    rekeninghouder: str
+
+
+@dataclass
+class Organisatie:
+    huisnummer: str
+    kvkNummer: str
+    naam: str
+    plaatsnaam: str
+    postcode: str
+    straatnaam: str
+    weergaveNaam: str
+    rekeningen: List[Rekening] = field(default_factory=list)
+
+
+@dataclass
+class OrganisatieScenario:
+    aantal: int
+
+@dataclass
+class Organisaties:
+    organisaties: List[Organisatie] = field(default_factory=list)
+    straatnamen: List[str] = field(default_factory=list)
+    scenarios: List[OrganisatieScenario] = field(default_factory=list)
+
+
+@dataclass
+class AfspraakScenario:
+    aantal: int
+    rubriek: str
+    organisatie: str
+    bedrag: int
+
+
+@dataclass
+class GebruikerScenario:
+    aantal: int
+    aantal_rekeningen: int
+    afspraken: List[AfspraakScenario] = field(default_factory=list)
+
+
+@dataclass
+class Gebruikers:
+    scenarios: List[GebruikerScenario] = field(default_factory=list)
+
+def load_yaml_dataclass(filename: str, clazz):
+    with open(filename, 'r') as reader:
+        loaded = load(reader, Loader=SafeLoader)
+
+        schema = class_schema(clazz)
+
+        return schema().load(loaded)
+
+class Scenario:
+    configuratie: Systeem = load_yaml_dataclass('../scenarios/configuratie.yaml', Systeem)
+    organisatie: Organisaties = load_yaml_dataclass('../scenarios/organisaties.yaml', Organisaties)
+    gebruikers: Gebruikers = load_yaml_dataclass('../scenarios/gebruikers.yaml', Gebruikers)
