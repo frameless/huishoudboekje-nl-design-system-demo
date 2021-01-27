ada120903874cfbc49cf4bc170f67f32ca255ab2
Gilgamesh Nootebos <gilgamez@gmail.com>
2021-01-27 16:45:10 +0100

    unit tests moet je wel goed herschrijven

--- backend/tests/graphql/mutations/gebruikers/test_create_gebruiker.py
+++ backend/tests/graphql/mutations/gebruikers/test_create_gebruiker.py
@@ -20,10 +20,14 @@ class MockResponse():
 
 def test_create_gebruiker_success(client):
     with requests_mock.Mocker() as mock:
-        rekeningen_request = mock.get(f"{settings.HHB_SERVICES_URL}/rekeningen/?filter_ibans=gb33bukb20201555555555", json={'data': [{'id': 1}]}, status_code=200)
-        gebruikers_request = mock.post(f"{settings.HHB_SERVICES_URL}/gebruikers/", json={'data': [{'id': 1}]}, status_code=201)
-        gebruikers_rekeningen_request = mock.post(f"{settings.HHB_SERVICES_URL}/gebruikers/1/rekeningen/", json={'data': [{'id': 1}]}, status_code=201)
-        log_request = mock.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/", json={'data': [{'id': 1}]}, status_code=201)
+        rekeningen_request = mock.get(f"{settings.HHB_SERVICES_URL}/rekeningen/?filter_ibans=gb33bukb20201555555555",
+                                      json={'data': [{'id': 1}]}, status_code=200)
+        gebruikers_request = mock.post(f"{settings.HHB_SERVICES_URL}/gebruikers/", json={'data': {'id': 1}},
+                                       status_code=201)
+        gebruikers_rekeningen_request = mock.post(f"{settings.HHB_SERVICES_URL}/gebruikers/1/rekeningen/",
+                                                  json={'data': {'id': 1}}, status_code=201)
+        log_request = mock.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/", json={'data': {'id': 1}},
+                                status_code=201)
         response = client.post(
             "/graphql",
             json={
@@ -51,8 +55,8 @@ def test_create_gebruiker_success(client):
                                     "rekeninghouder": "C. Lown"}]}}},
             content_type='application/json'
         )
+        assert response.json == {"data": {"createGebruiker": {"ok": True, "gebruiker": {"id": 1}}}}
         assert rekeningen_request.called_once
         assert gebruikers_request.called_once
         assert gebruikers_rekeningen_request.called_once
         assert log_request.called_once
-        assert response.json["data"]["createGebruiker"]["ok"] is True
