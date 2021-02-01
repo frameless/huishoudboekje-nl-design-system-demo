d0ed9a5db849bccfb2db04bbecfe8f705b247dfd
Koen Brouwer <contact@kbrouwer.nl>
2021-02-01 10:14:21 +0100

    Explicitly set align-items to flex-start on FormLeft

--- frontend/app/src/components/Forms/FormLeftRight.tsx
+++ frontend/app/src/components/Forms/FormLeftRight.tsx
@@ -8,7 +8,7 @@ type FormLeftProps = {
 }
 
 export const FormLeft: React.FC<StackProps & FormLeftProps> = ({title, helperText, children, ...props}) => (
-	<Stack flex={1} spacing={1} {...props}>
+	<Stack flex={1} spacing={1} alignItems={"flex-start"} {...props}>
 		{title && <Heading size={"md"}>{title}</Heading>}
 		{helperText && <Text fontSize={"md"} color={"gray.500"}>{helperText}</Text>}
 		{children}
