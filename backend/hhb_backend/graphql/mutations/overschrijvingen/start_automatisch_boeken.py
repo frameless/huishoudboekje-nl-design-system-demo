78eab243a3cc723df63c7febb8c79eb62c904f9f
Christian Hulleman <c.hulleman@anchormen.nl>
2021-02-17 15:22:51 +0100

    rebase fix

--- backend/hhb_backend/graphql/mutations/overschrijvingen/start_automatisch_boeken.py
+++ backend/hhb_backend/graphql/mutations/overschrijvingen/start_automatisch_boeken.py
@@ -1,17 +1,24 @@
 import graphene
 
 import hhb_backend.graphql.models.journaalpost as journaalpost
+from hhb_backend.graphql.utils.gebruikersactiviteiten import log_gebruikers_activiteit
 from hhb_backend.processen import automatisch_boeken
 
 
 
 class StartAutomatischBoeken(graphene.Mutation):
-
     ok = graphene.Boolean()
     journaalposten = graphene.List(lambda: journaalpost.Journaalpost)
 
+    def gebruikers_activiteit(self, _root, info, *_args, **_kwargs):
+        return dict(
+            action=info.field_name,
+            entities=[]
+        )
+
+    @staticmethod
+    @log_gebruikers_activiteit
     async def mutate(_root, _info, **kwargs):
         journaalposten = await automatisch_boeken.automatisch_boeken()
 
         return StartAutomatischBoeken(ok=True, journaalposten=journaalposten)
-
