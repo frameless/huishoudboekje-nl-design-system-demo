dcdc07c21d4fdb9425a775498091512fd0dcad72
Koen Brouwer <contact@kbrouwer.nl>
2021-02-23 14:28:58 +0100

    Now force breaking too long organisatie names.

--- frontend/app/src/components/Organisaties/Views/OrganisatieListView.tsx
+++ frontend/app/src/components/Organisaties/Views/OrganisatieListView.tsx
@@ -7,25 +7,25 @@ import Routes from "../../../config/routes";
 import {Organisatie} from "../../../generated/graphql";
 import GridCard from "../../GridCard";
 
-const OrganisatieListView: React.FC<BoxProps & { organisaties: Organisatie[], showAddButton?: boolean }> = ({organisaties, showAddButton = false, ...props}) => {
+const OrganisatieListView: React.FC<BoxProps & {organisaties: Organisatie[], showAddButton?: boolean}> = ({organisaties, showAddButton = false, ...props}) => {
 	const {t} = useTranslation();
 	const {push} = useHistory();
 
 	return (
-		<Grid maxWidth={"100%"} gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)", "repeat(6, 1fr)"]} gap={5}>
+		<Grid maxWidth={"100%"} gridTemplateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]} overflowX={"scroll"} gap={5}>
 			{showAddButton && (
 				<Box>
 					<Button colorScheme={"blue"} borderStyle={"dashed"} variant={"outline"} leftIcon={<AddIcon />}
-					        w="100%" h="100%" onClick={() => push(Routes.CreateOrganisatie)} borderRadius={5}
-					        p={5}>{t("actions.add")}</Button>
+						w="100%" h="100%" onClick={() => push(Routes.CreateOrganisatie)} borderRadius={5}
+						p={5}>{t("actions.add")}</Button>
 				</Box>
 			)}
 			{organisaties.map((o, i) => (
 				<GridCard key={i} justifyContent={"center"} onClick={() => {
-					push(Routes.Organisatie(o.id))
+					push(Routes.Organisatie(o.id));
 				}}>
 					<Stack spacing={1}>
-						<Text fontSize={"md"}><strong>{o.weergaveNaam}</strong></Text>
+						<Text fontSize={"md"} overflowX={"hidden"} textOverflow={"ellipsis"} width={"100%"} maxW={["300px", "200px", "250px", "200px"]} title={o.weergaveNaam}><strong>{o.weergaveNaam}</strong></Text>
 						<Text fontSize={"md"} color={"gray.400"}>{o.kvkDetails?.plaatsnaam}</Text>
 					</Stack>
 				</GridCard>
