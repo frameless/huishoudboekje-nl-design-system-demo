237fe3dfb5ec9e465d44cb19e292a3ea1f4698a0
Koen Brouwer <contact@kbrouwer.nl>
2021-03-02 21:35:11 +0100

    Fix #388

--- frontend/app/src/components/Bankzaken/Transacties/TransactieItem.tsx
+++ frontend/app/src/components/Bankzaken/Transacties/TransactieItem.tsx
@@ -25,7 +25,7 @@ import {
 	Tooltip,
 	useBreakpointValue,
 	useDisclosure,
-	useToast
+	useToast,
 } from "@chakra-ui/react";
 import React, {useContext, useState} from "react";
 import {useTranslation} from "react-i18next";
@@ -37,7 +37,7 @@ import {
 	useCreateJournaalpostAfspraakMutation,
 	useCreateJournaalpostGrootboekrekeningMutation,
 	useDeleteJournaalpostMutation,
-	useGetTransactionItemFormDataQuery
+	useGetTransactionItemFormDataQuery,
 } from "../../../generated/graphql";
 import Queryable from "../../../utils/Queryable";
 import {formatIBAN} from "../../../utils/things";
@@ -49,7 +49,7 @@ import SelectAfspraakOption from "../../Layouts/SelectAfspraak/SelectAfspraakOpt
 import {TransactionsContext} from "./index";
 import TransactieDetailsView from "./TransactieDetailsView";
 
-const TransactieItem: React.FC<BoxProps & { bankTransaction: BankTransaction }> = ({bankTransaction: bt, ...props}) => {
+const TransactieItem: React.FC<BoxProps & {bankTransaction: BankTransaction}> = ({bankTransaction: bt, ...props}) => {
 	const {t} = useTranslation();
 	const isMobile = useBreakpointValue([true, null, null, false]);
 	const toast = useToast();
@@ -89,24 +89,135 @@ const TransactieItem: React.FC<BoxProps & { bankTransaction: BankTransaction }> 
 					isClosable: true,
 				});
 			});
-	}
+	};
 
 	const onClickDeleteJournaalpost = () => {
 		const id = bt.journaalpost?.id;
 
 		if (id) {
 			handleMutation(deleteJournaalpost({
-				variables: {id}
+				variables: {id},
 			}), () => {
 				setSelectedRubriek(undefined);
 				setSelectedAfspraak(undefined);
 			});
 		}
-	}
+	};
 
 	const onClick = () => {
 		if (!isMobile) {
 			onOpen();
+		}
+	};
+
+	const booking = () => {
+		if (selectedAfspraak) {
+			return (
+				<Stack>
+					<Divider />
+
+					<Heading size={"sm"}>{t("booking")}</Heading>
+					<Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} spacing={2}>
+						<SelectAfspraakOption afspraak={selectedAfspraak} enableHover={false} />
+						<Box>
+							<IconButton icon={<DeleteIcon />} variant={"ghost"} aria-label={t("actions.disconnect")} onClick={onClickDeleteJournaalpost}
+								isLoading={$deleteJournaalpost.loading} />
+						</Box>
+					</Stack>
+				</Stack>
+			);
+		}
+		else if (selectedRubriek) {
+			return (
+				<Stack direction={"row"} justifyContent={"flex-start"} alignItems={"center"} spacing={3}>
+					<Box>
+						<Label>{t("forms.agreements.fields.rubriek")}</Label>
+						<Box>
+							<Text>{selectedRubriek.naam}</Text>
+							{selectedRubriek.grootboekrekening && <Text fontSize={"sm"}>{selectedRubriek.grootboekrekening.omschrijving}</Text>}
+						</Box>
+					</Box>
+					<Box>
+						<IconButton icon={<DeleteIcon />} variant={"ghost"} aria-label={t("actions.disconnect")} onClick={onClickDeleteJournaalpost}
+							isLoading={$deleteJournaalpost.loading} />
+					</Box>
+				</Stack>
+			);
+		}
+		else {
+			return (
+				<Queryable query={$transactionItemFormData}>{(data: {rubrieken: Rubriek[], afspraken: Afspraak[]}) => {
+					const {rubrieken, afspraken} = data;
+
+					const options = {
+						afspraken: afspraken.filter(a => {
+							// Show all afspraken if there is no tegenRekening
+							if (!bt.tegenRekening && !bt.tegenRekeningIban) {
+								return true;
+							}
+							return (
+								a.tegenRekening?.iban?.replaceAll(" ", "") === bt.tegenRekening?.iban?.replaceAll(" ", "") ||
+								a.tegenRekening?.iban?.replaceAll(" ", "") === bt.tegenRekeningIban?.replaceAll(" ", "")
+							);
+						}),
+						rubrieken: rubrieken.filter(r => r.grootboekrekening && r.grootboekrekening.id).map((r: Rubriek) => ({
+							key: r.id,
+							label: r.naam,
+							value: r.grootboekrekening!.id,
+						})),
+					};
+
+					const onSelectRubriek = (val) => {
+						const foundRubriek = rubrieken.find(r => r.grootboekrekening?.id === val.value);
+
+						const transactionId = bt?.id;
+						const grootboekrekeningId = foundRubriek?.grootboekrekening?.id;
+
+						if (transactionId && grootboekrekeningId) {
+							handleMutation(createJournaalpostGrootboekrekening({
+								variables: {transactionId, grootboekrekeningId},
+							}), () => {
+								setSelectedRubriek(foundRubriek);
+							});
+						}
+					};
+
+					const onSelectAfspraak = (afspraak: Afspraak) => {
+						const transactionId = bt?.id;
+						const afspraakId = afspraak.id;
+
+						if (transactionId && afspraakId) {
+							handleMutation(createJournaalpostAfspraak({
+								variables: {transactionId, afspraakId},
+							}), () => {
+								setSelectedAfspraak(afspraak);
+							});
+						}
+					};
+
+					return (
+						<Stack>
+							<Heading size={"sm"}>Koppelen met</Heading>
+
+							<Tabs isFitted>
+								<TabList>
+									<Tab>Afspraak</Tab>
+									<Tab>Rubriek</Tab>
+								</TabList>
+								<TabPanels>
+									<TabPanel px={0}>
+										<SelectAfspraak value={selectedAfspraak} options={options.afspraken} onChange={onSelectAfspraak} />
+									</TabPanel>
+									<TabPanel px={0}>
+										<Select onChange={onSelectRubriek} options={options.rubrieken} isClearable={true} noOptionsMessage={() => t("select.noOptions")}
+											maxMenuHeight={200} />
+									</TabPanel>
+								</TabPanels>
+							</Tabs>
+						</Stack>
+					);
+				}}</Queryable>
+			);
 		}
 	};
 
@@ -119,110 +230,7 @@ const TransactieItem: React.FC<BoxProps & { bankTransaction: BankTransaction }> 
 				<ModalBody>
 					<Stack spacing={5}>
 						<TransactieDetailsView transaction={bt} />
-
-						{selectedAfspraak && (
-							<Stack>
-								<Divider />
-
-								<Heading size={"sm"}>{t("booking")}</Heading>
-								<Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} spacing={2}>
-									<SelectAfspraakOption afspraak={selectedAfspraak} enableHover={false} />
-									<Box>
-										<IconButton icon={<DeleteIcon />} variant={"ghost"} aria-label={t("actions.disconnect")} onClick={onClickDeleteJournaalpost}
-										            isLoading={$deleteJournaalpost.loading} />
-									</Box>
-								</Stack>
-							</Stack>
-						)}
-						{selectedRubriek && (
-							<Stack direction={"row"} justifyContent={"flex-start"} alignItems={"center"} spacing={3}>
-								<Box>
-									<Label>{t("forms.agreements.fields.rubriek")}</Label>
-									<Box>
-										<Text>{selectedRubriek.naam}</Text>
-										{selectedRubriek.grootboekrekening && <Text fontSize={"sm"}>{selectedRubriek.grootboekrekening.omschrijving}</Text>}
-									</Box>
-								</Box>
-								<Box>
-									<IconButton icon={<DeleteIcon />} variant={"ghost"} aria-label={t("actions.disconnect")} onClick={onClickDeleteJournaalpost}
-									            isLoading={$deleteJournaalpost.loading} />
-								</Box>
-							</Stack>
-						)}
-
-						{(!selectedRubriek && !selectedAfspraak) && (
-							<Queryable query={$transactionItemFormData}>{(data: { rubrieken: Rubriek[], afspraken: Afspraak[] }) => {
-								const {rubrieken, afspraken} = data;
-
-								const options = {
-									afspraken: afspraken.filter(a => {
-										// Show all afspraken if there is no tegenRekening
-										if (!bt.tegenRekening && !bt.tegenRekeningIban) {
-											return true;
-										}
-										return (
-											a.tegenRekening?.iban?.replaceAll(" ", "") === bt.tegenRekening?.iban?.replaceAll(" ", "") ||
-											a.tegenRekening?.iban?.replaceAll(" ", "") === bt.tegenRekeningIban?.replaceAll(" ", "")
-										);
-									}),
-									rubrieken: rubrieken.filter(r => r.grootboekrekening && r.grootboekrekening.id).map((r: Rubriek) => ({
-										key: r.id,
-										label: r.naam,
-										value: r.grootboekrekening!.id
-									}))
-								};
-
-								const onSelectRubriek = (val) => {
-									const foundRubriek = rubrieken.find(r => r.grootboekrekening?.id === val.value);
-
-									const transactionId = bt?.id;
-									const grootboekrekeningId = foundRubriek?.grootboekrekening?.id;
-
-									if (transactionId && grootboekrekeningId) {
-										handleMutation(createJournaalpostGrootboekrekening({
-											variables: {transactionId, grootboekrekeningId}
-										}), () => {
-											setSelectedRubriek(foundRubriek);
-										});
-									}
-								}
-
-								const onSelectAfspraak = (afspraak: Afspraak) => {
-									const transactionId = bt?.id;
-									const afspraakId = afspraak.id;
-
-									if (transactionId && afspraakId) {
-										handleMutation(createJournaalpostAfspraak({
-											variables: {transactionId, afspraakId}
-										}), () => {
-											setSelectedAfspraak(afspraak);
-										});
-									}
-								}
-
-								return (
-									<Stack>
-										<Heading size={"sm"}>Koppelen met</Heading>
-
-										<Tabs isFitted>
-											<TabList>
-												<Tab>Afspraak</Tab>
-												<Tab>Rubriek</Tab>
-											</TabList>
-											<TabPanels>
-												<TabPanel px={0}>
-													<SelectAfspraak value={selectedAfspraak} options={options.afspraken} onChange={onSelectAfspraak} />
-												</TabPanel>
-												<TabPanel px={0}>
-													<Select onChange={onSelectRubriek} options={options.rubrieken} isClearable={true} noOptionsMessage={() => t("select.noOptions")}
-													        maxMenuHeight={200} />
-												</TabPanel>
-											</TabPanels>
-										</Tabs>
-									</Stack>
-								);
-							}}</Queryable>
-						)}
+						{booking()}
 					</Stack>
 				</ModalBody>
 			</ModalContent>
@@ -231,7 +239,7 @@ const TransactieItem: React.FC<BoxProps & { bankTransaction: BankTransaction }> 
 		<Box px={2} mx={-2} {...!bt.journaalpost && {
 			// bg: "red.50"
 		}} _hover={{
-			bg: "gray.100"
+			bg: "gray.100",
 		}}>
 			<Stack direction={"row"} alignItems={"center"} justifyContent={"center"} {...props} onClick={onClick} cursor={"pointer"}>
 				<Box flex={2}>{bt.tegenRekening ? (
