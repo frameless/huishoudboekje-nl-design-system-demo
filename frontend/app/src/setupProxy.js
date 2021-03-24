b286af88e8309a9e540a5b4130f3f3db817a63c5
Koen Brouwer <contact@kbrouwer.nl>
2021-03-24 15:36:39 +0100

    Inlined headers.

--- frontend/app/src/setupProxy.js
+++ frontend/app/src/setupProxy.js
@@ -1,18 +1,18 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function(app) {
 	const target = process.env.PROXY || "https://test.huishoudboekje.demoground.nl";
-	const headers = {};
-	if (process.env.PROXY_AUTHORIZATION) {
-		headers["Authorization"] = `Bearer ${process.env.PROXY_AUTHORIZATION}`;
-	}
 	app.use(
 		"/api",
 		createProxyMiddleware({
 			target,
 			changeOrigin: true,
 			xfwd: true,
-			headers,
+			headers: {
+				...process.env.PROXY_AUTHORIZATION && {
+					Authorization: `Bearer ${process.env.PROXY_AUTHORIZATION}`,
+				},
+			},
 		}),
 	);
 };
