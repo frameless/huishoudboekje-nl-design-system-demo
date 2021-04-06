e81f4c9ff369ba3c01afde76fc46610bebaef9f9
Koen Brouwer <contact@kbrouwer.nl>
2021-04-06 12:58:33 +0200

    Fix date (#465)

--- frontend/app/src/components/Bankzaken/Transacties/TransactiesList.tsx
+++ frontend/app/src/components/Bankzaken/Transacties/TransactiesList.tsx
@@ -1,8 +1,8 @@
 import {Box, Stack, useBreakpointValue} from "@chakra-ui/react";
-import moment from "moment";
 import React from "react";
 import {useTranslation} from "react-i18next";
 import {BankTransaction} from "../../../generated/graphql";
+import d from "../../../utils/dayjs";
 import {sortBankTransactions} from "../../../utils/things";
 import DeadEndPage from "../../DeadEndPage";
 import Label from "../../Layouts/Label";
@@ -21,7 +21,7 @@ const TransactiesList: React.FC<TransactiesListProps> = ({transacties}) => {
 
 	/* Group transacties by date */
 	const bt = transacties.sort((a, b) => a.transactieDatum > b.transactieDatum ? -1 : 1).reduce((result, t) => {
-		const trDateAsString = moment(new Date(t.transactieDatum)).format("L");
+		const trDateAsString = d(new Date(t.transactieDatum)).format("L");
 		return {
 			...result,
 			[trDateAsString]: [
