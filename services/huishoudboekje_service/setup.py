aad5548c99189ccd7ad82e523d1f24df4aeebbc2
Christian Hulleman <c.hulleman@anchormen.nl>
2021-04-20 15:30:59 +0200

    Flask < 2

--- services/huishoudboekje_service/setup.py
+++ services/huishoudboekje_service/setup.py
@@ -9,7 +9,7 @@ setup(
     license='',
     description='Huishoudboekje Service',
     install_requires=[
-        "Flask>=1.1.2",
+        "Flask>=1.1.2,<2",
         "SQLAlchemy>=1.4",
         "psycopg2-binary>=2.8.6",
         "Flask-SQLAlchemy>=2.4.4",
