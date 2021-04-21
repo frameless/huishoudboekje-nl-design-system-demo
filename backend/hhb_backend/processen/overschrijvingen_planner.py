115387bfec2aef2ade3de6d861ae9a0dc1d5b2d0
Christian Hulleman <c.hulleman@anchormen.nl>
2021-04-21 16:34:00 +0200

    by_month and by_month_day working

--- backend/hhb_backend/processen/overschrijvingen_planner.py
+++ backend/hhb_backend/processen/overschrijvingen_planner.py
@@ -28,6 +28,8 @@ def get_planned_overschrijvingen(input: PlannedOverschijvingenInput, start_datum
 def get_doorlopende_afspraak_overschrijvingen(input: PlannedOverschijvingenInput, start_datum: date,
                                               eind_datum: date = None):
     payments = {}
+    except_dates = input.betaalinstructie["except_dates"] or []
+
     if input.betaalinstructie["by_day"]:
         for weekday in input.betaalinstructie["by_day"]:
             weekday_int = time.strptime(weekday, "%A").tm_wday
@@ -37,14 +39,18 @@ def get_doorlopende_afspraak_overschrijvingen(input: PlannedOverschijvingenInput
                     payments[payment_date.isoformat()] = make_overschrijving_dict(input.bedrag, payment_date,
                                                                                   input.afspraak_id)
                 payment_date = next_weekday(payment_date, weekday_int)
-        x = 1
-    elif input.betaalinstructie["by_month_day"] and input.betaalinstructie["by_month"]:
-        x = 2
+    elif input.betaalinstructie["by_month_day"]:
+        date_list = [start_datum + datetime.timedelta(days=x) for x in range(0, (eind_datum - start_datum).days)]
+        pay_days = input.betaalinstructie["by_month_day"]
+        pay_months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        if input.betaalinstructie["by_month"]:
+            pay_months = input.betaalinstructie["by_month"]
+        for possible_date in date_list:
+            if possible_date.day in pay_days and possible_date.month in pay_months:
+                payments[possible_date.isoformat()] = make_overschrijving_dict(input.bedrag, possible_date,
+                                                                               input.afspraak_id)
 
-    sorted_tuples = sorted(payments, key=lambda p: p[3])
-    sorted_dict = {k: v for k, v in sorted_tuples}
-
-    return sorted_dict
+    return dict(sorted(payments.items()))
 
     # determine first day, loop until enddate
     # Feitelijk 2 opties, of per week met de mogelijkheid tot meerdere dagen. Of per maand met de opties tot meerdere dagen per maand. Except dates in de gaten houden.
@@ -60,9 +66,10 @@ def get_doorlopende_afspraak_overschrijvingen(input: PlannedOverschijvingenInput
 
 def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
-    if days_ahead <= 0: # Target day already happened this week
+    if days_ahead <= 0:  # Target day already happened this week
         days_ahead += 7
     return d + datetime.timedelta(days_ahead)
+
 
 def make_overschrijving_dict(bedrag, date, afspraak_id):
     return {
