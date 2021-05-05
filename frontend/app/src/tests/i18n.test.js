cc6bcfa80bf30badb5275c5a771a85a78ed74676
Koen Brouwer <contact@kbrouwer.nl>
2021-05-05 16:28:09 +0200

    Test now shows better where there is a missing translation.

--- frontend/app/src/tests/i18n.test.js
+++ frontend/app/src/tests/i18n.test.js
@@ -1,22 +1,58 @@
 const fs = require("fs");
 const path = require("path");
+const readline = require("readline");
 
 const LANG_DIR = "/../lang";
 const requiredTranslationFiles = ["nl.translation.json"];
 
+const getFileLines = async (file) => {
+	const fileName = path.join(__dirname, LANG_DIR, file);
+	const lines = [];
+
+	const fileStream = fs.createReadStream(fileName);
+	const rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity,
+	});
+
+	for await (const line of rl){
+		lines.push(line);
+	}
+
+	return lines;
+};
+
+const loadFiles = async () => {
+	const files = fs.readdirSync(__dirname + LANG_DIR).map(async file => {
+		const lines = await getFileLines(file);
+		return {
+			fileName: file,
+			lines,
+		};
+	});
+
+	return Promise.all(files);
+};
+
+let files = [];
+
+beforeAll(async () => {
+	files = await loadFiles();
+	return Promise.resolve(files);
+});
+
 describe("Testing language files for missing translations", () => {
 
 	it("All required language files are there", () => {
-		expect(files).toEqual(expect.arrayContaining(requiredTranslationFiles));
+		const allFoundFiles = files.map(f => f.fileName);
+		expect(allFoundFiles).toEqual(expect.arrayContaining(requiredTranslationFiles));
 	});
 
-	const files = fs.readdirSync(__dirname + LANG_DIR);
-	files.forEach(file => {
-		it(`(${file}) contains no missing translations.`, () => {
-			const fileName = path.join(__dirname, LANG_DIR, file);
-			const fileContent = require(fileName);
-			const json = JSON.stringify(fileContent, null, 2);
-			expect(json).not.toContain("__MISSING-TRANSLATION__");
+	it("Tests all files files for missing translations", () => {
+		files.forEach(file => {
+			file.lines.forEach((line, i) => {
+				expect(line).not.toContain("__MISSING-TRANSLATION__");
+			});
 		});
 	});
 
