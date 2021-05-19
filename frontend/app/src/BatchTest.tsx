e95e1e28c95f3ab73b61b32caf95a6b7c7f02207
Koen Brouwer <contact@kbrouwer.nl>
2021-05-19 16:08:08 +0200

    Replaced default link with BatchLink.

---
+++ frontend/app/src/BatchTest.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "./components/Layouts/Page";
+
+const BatchTest = () => {
+	return (
+		<Page title={"Test"}>
+			Whatever
+		</Page>
+	);
+};
+
+export default BatchTest;
--- frontend/app/src/services/graphql-client.ts
+++ frontend/app/src/services/graphql-client.ts
@@ -1,12 +1,17 @@
-import {ApolloClient, ApolloLink, createHttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloClient, ApolloLink, InMemoryCache} from "@apollo/client";
+import {BatchHttpLink} from "@apollo/client/link/batch-http";
 import {createUploadLink} from "apollo-upload-client";
 
 const GraphqlApiUrl = "/api/graphql";
 const GraphqlApiUrlUpload = "/api/graphql";
 
-const defaultLink = createHttpLink({
+const defaultLink = new BatchHttpLink({
 	uri: GraphqlApiUrl,
+	batchMax: 20,
+	batchInterval: 250,
+	batchDebounce: true,
 });
+
 const uploadLink = createUploadLink({
 	uri: GraphqlApiUrlUpload,
 });
