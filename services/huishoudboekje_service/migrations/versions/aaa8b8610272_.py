0ec5c7efd6f29e3b8e5cd273dddbfae0ad23b7cf
Chieling Yueh <chielingyueh@gmail.com>
2021-08-04 15:31:49 +0200

    fixed bug migration files

--- services/huishoudboekje_service/migrations/versions/aaa8b8610272_.py
+++ services/huishoudboekje_service/migrations/versions/aaa8b8610272_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'aaa8b8610272'
-down_revision = '3051f9101b6d'
+down_revision = 'd4258041047d'
 branch_labels = None
 depends_on = None
 
