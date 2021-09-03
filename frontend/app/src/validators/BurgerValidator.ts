0fb83319730b8dea1aa87f482efb97d31810ac9b
Koen Brouwer <contact@kbrouwer.nl>
2021-09-03 22:19:29 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- frontend/app/src/validators/BurgerValidator.ts
+++ frontend/app/src/validators/BurgerValidator.ts
@@ -4,7 +4,7 @@ import zod from "../utils/zod";
 const BurgerValidator = zod.object({
 	bsn: zod.string().regex(/^([0-9]{8,9})$/),
 	voorletters: zod.string().regex(/^([A-Z]\.)+$/),
-	voornamen: zod.string().nonempty("Oops"),
+	voornamen: zod.string().nonempty(),
 	achternaam: zod.string().nonempty(),
 	geboortedatum: zod.string().regex(Regex.Date),
 	email: zod.string().nonempty().email(),
