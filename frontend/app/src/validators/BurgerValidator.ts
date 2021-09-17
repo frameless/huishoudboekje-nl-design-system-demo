1273640c08877d18c4bf60797af5ae82207a6911
Koen Brouwer <contact@kbrouwer.nl>
2021-09-17 15:12:59 +0200

    Not accepting date in the future.

--- frontend/app/src/validators/BurgerValidator.ts
+++ frontend/app/src/validators/BurgerValidator.ts
@@ -1,12 +1,14 @@
+import dayjs from "../utils/dayjs";
 import {Regex} from "../utils/things";
 import zod from "../utils/zod";
 
+// t("messages.burgers.invalidGeboortedatum")
 const BurgerValidator = zod.object({
 	bsn: zod.string().regex(/^([0-9]{8,9})$/),
 	voorletters: zod.string().regex(/^([A-Z]\.)+$/),
 	voornamen: zod.string().nonempty(),
 	achternaam: zod.string().nonempty(),
-	geboortedatum: zod.string().regex(Regex.Date),
+	geboortedatum: zod.string().regex(Regex.Date).refine(strval => dayjs(strval, "L").isSameOrBefore(dayjs()), {message: "messages.burgers.invalidGeboortedatum"}),
 	email: zod.string().nonempty().email(),
 	straatnaam: zod.string().nonempty(),
 	huisnummer: zod.string().nonempty(),
