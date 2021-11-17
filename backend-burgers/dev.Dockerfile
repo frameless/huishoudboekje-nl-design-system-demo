96579cf68276cd4975390c68f138f7dca8bd42ba
Koen Brouwer <contact@kbrouwer.nl>
2021-11-17 15:33:16 +0100

    Added deployment for backend-burgers.

--- backend-burgers/dev.Dockerfile
+++ backend-burgers/dev.Dockerfile
@@ -1,17 +1,11 @@
 ARG DOCKER_PROXY=''
-
-# Set up production environment using nginx
 FROM ${DOCKER_PROXY}bitnami/node:14 as webserver
-
 ENV HUISHOUDBOEKJESERVICE_URL="http://huishoudboekjeservice:8000"
-
+ENV ORGANISATIESERVICE_URL="http://organisatieservice:8002"
+ENV BANKTRANSACTIESERVICE_URL="http://banktransactieservice:8003"
 WORKDIR /app
-
 COPY ./package*.json /app/
 RUN npm ci
-
 COPY . /app
-
 EXPOSE 8080
-
 CMD ["npm", "run", "dev"]
--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -11,5 +11,10 @@ helm repo add bitnami "https://charts.bitnami.com/bitnami"
 helm repo update
 helm dependency build helm/contactcatalogus
 helm upgrade hhb-contactcatalogus helm/contactcatalogus --install --namespace=$NAMESPACE --set settings.env=prod --set settings.debug=0 --set settings.cache=1
-sleep 30 # Todo: We use this to wait for the contactcatalogus to be running, 30 seconds can be too long or too short. We should actually run some check that waits until it is up.
+
+# Todo: We use this to wait for the contactcatalogus to be running, 30 seconds can be too long or too short. We should actually run some check that waits until it is up.
+echo "Sleeping 30 seconds to wait for the database of the contactcatalogus to be up..."
+sleep 30
+
+# Update database schema of the Contactcatalogus. They currently (30-10-2021) have no better way to do this... :-(
 kubectl exec $(kubectl get pods -l app.kubernetes.io/instance=hhb-contactcatalogus -n $NAMESPACE | grep php | cut -d' ' -f1 | head -n1) --namespace=$NAMESPACE -- bin/console doctrine:schema:update -f
