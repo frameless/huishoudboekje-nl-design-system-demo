0dedef85e647bc978e2f6dbe18e38faa2f3c1b21
Koen Brouwer <contact@kbrouwer.nl>
2021-11-24 14:03:09 +0100

    Added deployment for postadressenservice

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -7,6 +7,7 @@ export NAMESPACE=${NAMESPACE:-huishoudboekje}
 export DEPLOYMENT_DIST_DIR="dist"
 
 kubectl apply -f k8s/$DEPLOYMENT_DIST_DIR/single_deploy_file.yaml --namespace=$NAMESPACE
+
 helm repo add bitnami "https://charts.bitnami.com/bitnami"
 helm repo update
 helm dependency build helm/contactcatalogus
