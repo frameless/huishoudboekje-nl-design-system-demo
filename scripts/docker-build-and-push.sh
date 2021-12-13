5296d09d752cec8486ec4e0b9ea68851ef8869d4
Koen Brouwer <contact@kbrouwer.nl>
2021-12-13 13:45:46 +0100

    Added deployment for signalenservice

--- scripts/docker-build-and-push.sh
+++ scripts/docker-build-and-push.sh
@@ -12,6 +12,7 @@ docker build -t $CI_REGISTRY_IMAGE/python-postgres:$IMAGE_TAG        --build-arg
 docker build -t $CI_REGISTRY_IMAGE/unleashservice:$IMAGE_TAG         --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   ./services/unleashservice
 docker build -t $CI_REGISTRY_IMAGE/postadressenservice:$IMAGE_TAG    --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   ./services/postadressenservice
 docker build -t $CI_REGISTRY_IMAGE/alarmenservice:$IMAGE_TAG         --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   ./services/alarmenservice
+docker build -t $CI_REGISTRY_IMAGE/signalenservice:$IMAGE_TAG        --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   ./services/signalenservice
 docker build -t $CI_REGISTRY_IMAGE/banktransactieservice:$IMAGE_TAG  --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   --file=./services/bank_transactie_service/Dockerfile  services
 docker build -t $CI_REGISTRY_IMAGE/grootboekservice:$IMAGE_TAG       --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   --file=./services/grootboek_service/Dockerfile        services
 docker build -t $CI_REGISTRY_IMAGE/huishoudboekjeservice:$IMAGE_TAG  --build-arg "DOCKER_PROXY=$DOCKER_PROXY"   --file=./services/huishoudboekje_service/Dockerfile   services
@@ -26,6 +27,7 @@ docker push $CI_REGISTRY_IMAGE/python-postgres:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/unleashservice:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/postadressenservice:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/alarmenservice:$IMAGE_TAG
+docker push $CI_REGISTRY_IMAGE/signalenservice:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/banktransactieservice:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/grootboekservice:$IMAGE_TAG
 docker push $CI_REGISTRY_IMAGE/huishoudboekjeservice:$IMAGE_TAG
