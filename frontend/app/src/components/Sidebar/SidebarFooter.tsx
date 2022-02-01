4a85a47fb1873a44ae997507e3986b565229fbde
Koen Brouwer <contact@kbrouwer.nl>
2022-02-01 16:25:09 +0100

    Bump version

--- frontend/app/src/components/Sidebar/SidebarFooter.tsx
+++ frontend/app/src/components/Sidebar/SidebarFooter.tsx
@@ -5,7 +5,7 @@ import VersionTag from "../Layouts/VersionTag";
 const SidebarFooter = () => (
 	<Box textAlign={"center"}>
 		<Heading size={"sm"} color={"gray.400"}>Huishoudboekje</Heading>
-		<VersionTag color={"gray.400"} />
+		<VersionTag />
 	</Box>
 );
 
