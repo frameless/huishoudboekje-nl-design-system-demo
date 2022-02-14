dc26b2d46ae03efb2cf8a820334f9fa367ca22de
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-02-14 16:31:08 +0100

    WIP, Signal shows context with data (schrijving)

--- frontend/app/src/components/Signalen/SignalenList.tsx
+++ frontend/app/src/components/Signalen/SignalenList.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import {Signal} from "../../generated/graphql";
-import {Box} from "@chakra-ui/react";
+import {Table, Tbody, Td, Text, Th, Thead, Tr} from "@chakra-ui/react";
+import {useTranslation} from "react-i18next";
+import {GoPrimitiveDot} from "react-icons/all";
 
 const SignalenList: React.FC<{ signalen: Signal[] }> = ({signalen}) => {
+	const {t} = useTranslation();
+
 	return (
-		<Box>
-			<pre>{JSON.stringify(signalen, null, 2)}</pre>
-		</Box>
-	);
+		<Table size={"sm"} variant={"noLeftPadding"}>
+			<Thead>
+				<Tr>
+					<Th>{t("signalen.status")}</Th>
+					<Th>{t("signalen.context")}</Th>
+				</Tr>
+			</Thead>
+			<Tbody>
+				{signalen.map((s, i) => (
+					<Tr>
+						<Td textAlign={"right"}>
+							<GoPrimitiveDot color="green" size="25" />
+						</Td>
+						<Td>
+							<Text>{t("signalen.contextMessage", {
+								omschrijving: (s.alarm?.afspraak?.omschrijving),
+								bedrag: "100.00",
+								datum: "20-09-2022"
+							})}</Text>
+						</Td>
+					</Tr>
+				))}
+			</Tbody>
+		</Table>
+	)
 };
 
 export default SignalenList;
