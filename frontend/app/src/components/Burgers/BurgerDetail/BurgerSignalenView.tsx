1fe96d204d1068942726fa7b01927a390a63d9b4
Koen Brouwer <contact@kbrouwer.nl>
2022-02-22 11:15:59 +0100

    Made the UI look more like Gebeurtenissen.

--- frontend/app/src/components/Burgers/BurgerDetail/BurgerSignalenView.tsx
+++ frontend/app/src/components/Burgers/BurgerDetail/BurgerSignalenView.tsx
@@ -1,43 +1,26 @@
-import React from "react";
 import {Stack, StackProps} from "@chakra-ui/react";
-import {Burger} from "../../../generated/graphql";
-import {FormLeft, FormRight} from "../../shared/Forms";
+import React from "react";
 import {useTranslation} from "react-i18next";
+import exampleSignalen from "../../../exampleSignalen.json"; // Todo: remove this once we can grab the signalen from the API
+import {Burger, Signal} from "../../../generated/graphql";
+import {FormLeft, FormRight} from "../../shared/Forms";
 import SignalenList from "../../Signalen/SignalenList";
 
-const BurgerSignalenView: React.FC<StackProps & { burger: Burger }> = ({burger, ...props}) => {
+// Todo: remove this temporary type once "Signal" has been renamed to "Signaal" and context was changed.
+export type Signaal = Omit<Signal, "context"> & {
+	context: any,
+	timeUpdated: string,
+};
+
+const BurgerSignalenView: React.FC<StackProps & {burger: Burger}> = ({burger, ...props}) => {
 	const {t} = useTranslation();
-	// const afspraken: Afspraak[] = burger.afspraken || [];
-	// const signalen: Signal[] = afspraken.map(afspraak => afspraak.alarm?.signaal as Signal) || []
-	const signalen = [
-		{
-			"id": "10039f40-efe9-45af-ac56-e1ba5b50e6c7",
-			"alarm": {
-				"id": "f5283f61-c770-453c-b609-89e3416ad5b0",
-				"isActive": true,
-				"gebruikerEmail": "developer@sloothuizen.nl",
-				"afspraak": {
-					"id": 9,
-					"bedrag": "939.94",
-					"credit": false,
-					"omschrijving": "Leefgeld"
-				}
-			},
-			"isActive": true,
-			"type": "default",
-			"actions": [],
-			"context": {
-				"bedrag": "100.80",
-				"datum": "2022-07-07"
-			},
-			"timeCreated": "2022-02-14T09:54:24.005Z"
-		}
-	]
+	// const signalen: Signal[] = (burger.afspraken || []).map(afspraak => afspraak.alarm?.signaal as Signal) || [];
+
 	return (
 		<Stack spacing={2} mb={1} direction={["column", "row"]} {...props}>
 			<FormLeft title={t("forms.burgers.sections.signalen.title")} helperText={t("forms.burgers.sections.signalen.detailText")} />
 			<FormRight justifyContent={"center"}>
-				<SignalenList signalen={signalen as any} />
+				<SignalenList signalen={exampleSignalen} />
 			</FormRight>
 		</Stack>
 	);
--- frontend/app/src/components/Signalen/SignalenList.tsx
+++ frontend/app/src/components/Signalen/SignalenList.tsx
@@ -1,38 +1,55 @@
+import {BellIcon, CheckIcon} from "@chakra-ui/icons";
+import {Badge, IconButton, Stack, Table, Tbody, Td, Text, Tr} from "@chakra-ui/react";
 import React from "react";
-import {Signal} from "../../generated/graphql";
-import {Table, Tbody, Td, Text, Th, Thead, Tr} from "@chakra-ui/react";
-import {useTranslation} from "react-i18next";
-import {GoPrimitiveDot} from "react-icons/all";
+import {Trans, useTranslation} from "react-i18next";
+import {AppRoutes} from "../../config/routes";
+import d from "../../utils/dayjs";
+import {currencyFormat2} from "../../utils/things";
+import {useToaster} from "../../utils/useToaster";
+import {Signaal} from "../Burgers/BurgerDetail/BurgerSignalenView";
+import AuditLogLink from "../Gebeurtenissen/AuditLogLink";
 
-const SignalenList: React.FC<{ signalen: Signal[] }> = ({signalen}) => {
+const SignalenList: React.FC<{signalen: Signaal[]}> = ({signalen}) => {
 	const {t} = useTranslation();
+	const toast = useToaster();
 
 	return (
 		<Table size={"sm"} variant={"noLeftPadding"}>
-			<Thead>
-				<Tr>
-					<Th>{t("signalen.status")}</Th>
-					<Th>{t("signalen.context")}</Th>
-				</Tr>
-			</Thead>
 			<Tbody>
 				{signalen.map((s, i) => (
-					<Tr>
-						<Td textAlign={"right"}>
-							<GoPrimitiveDot color={s.isActive ? "green" : "red"} size="25" />
-						</Td>
+					<Tr key={i}>
 						<Td>
-							<Text>{t("signalen.contextMessage", {
-								omschrijving: (s.alarm?.afspraak?.omschrijving),
-								bedrag: "100.00",
-								datum: "20-09-2022"
-							})}</Text>
+							<Stack spacing={1} width={"100%"}>
+								<Text>
+									<Trans i18nKey={"signalen.contextMessage"} values={{
+										afspraak: (s.alarm?.afspraak?.omschrijving),
+										bedrag: currencyFormat2(true).format(parseFloat(s.context?.bedrag)),
+									}} components={{
+										strong: <strong />,
+										linkAfspraak: <AuditLogLink to={AppRoutes.ViewAfspraak(s.alarm?.afspraak?.id)}>{s.alarm?.afspraak?.omschrijving}</AuditLogLink>,
+									}} />
+								</Text>
+								<Text fontSize={"sm"} color={"gray.500"}>
+									{d(s.timeUpdated).format("LL LT")}
+								</Text>
+							</Stack>
+						</Td>
+						<Td textAlign={"center"}>
+							<Badge colorScheme={s.isActive ? "green" : "gray"}>{s.isActive ? t("enabled") : t("disabled")}</Badge>
+						</Td>
+						<Td textAlign={"center"}>
+							<IconButton icon={s.isActive ? <CheckIcon /> : <BellIcon />} size={"sm"} aria-label={s.isActive ? t("actions.disable") : t("actions.enable")} onClick={() => {
+								toast({
+									description: "Deze functionaliteit wordt nog ontwikkeld.",
+									status: "info",
+								});
+							}} />
 						</Td>
 					</Tr>
 				))}
 			</Tbody>
 		</Table>
-	)
+	);
 };
 
 export default SignalenList;
