5262e7667b63bba14ba711f053246cfbe8243bdc
Koen Brouwer <contact@kbrouwer.nl>
2022-03-03 15:27:25 +0100

    Changed label and added list of formats

--- frontend/app/src/components/Bankzaken/Bankafschriften/index.tsx
+++ frontend/app/src/components/Bankzaken/Bankafschriften/index.tsx
@@ -1,11 +1,11 @@
-import {Box, FormLabel, Stack, Table, Tbody, Th, Thead, Tr, useDisclosure} from "@chakra-ui/react";
+import {Box, FormLabel, List, ListIcon, ListItem, Stack, Table, Tbody, Text, Th, Thead, Tr, useDisclosure} from "@chakra-ui/react";
 import React from "react";
 import {useTranslation} from "react-i18next";
+import {MdCheckCircle} from "react-icons/md";
 import {CustomerStatementMessage, GetCsmsDocument, useDeleteCustomerStatementMessageMutation, useGetCsmsQuery} from "../../../generated/graphql";
 import Queryable from "../../../utils/Queryable";
 import useToaster from "../../../utils/useToaster";
 import AddButton from "../../shared/AddButton";
-import DeadEndPage from "../../shared/DeadEndPage";
 import {FormLeft, FormRight} from "../../shared/Forms";
 import Page from "../../shared/Page";
 import Section from "../../shared/Section";
@@ -14,7 +14,7 @@ import CsmUploadModal from "./CsmUploadModal";
 
 const CustomerStatementMessages = () => {
 	const {t} = useTranslation();
-	const {isOpen, onClose, onOpen} = useDisclosure();
+	const addCsmModal = useDisclosure();
 	const toast = useToaster();
 
 	const $customerStatementMessages = useGetCsmsQuery();
@@ -41,33 +41,31 @@ const CustomerStatementMessages = () => {
 
 	return (
 		<Page title={t("bankzaken.customerStatementMessages.title")}>
-			{isOpen && (
-				<CsmUploadModal onClose={() => onClose()} />
+			{addCsmModal.isOpen && (
+				<CsmUploadModal onClose={() => addCsmModal.onClose()} />
 			)}
 			<Queryable query={$customerStatementMessages}>{(data) => {
 				/* Sort CSMs so that the newest appears first */
 				const csms: CustomerStatementMessage[] = [...data.customerStatementMessages || []].sort((a, b) => a.uploadDate <= b.uploadDate ? 1 : -1);
 
-				if (csms.length === 0) {
-					return (
-						<DeadEndPage message={t("messages.csms.addHint", {buttonLabel: t("global.actions.add")})}>
-							<AddButton onClick={() => onOpen()} />
-						</DeadEndPage>
-					);
-				}
-
 				return (
 					<Stack spacing={5}>
 						<Section>
 							<Stack direction={["column", "row"]} spacing={5}>
-								<FormLeft title={t("forms.bankzaken.sections.customerStatementMessages.title")} helperText={t("forms.bankzaken.sections.customerStatementMessages.detailText")} />
+								<FormLeft title={t("forms.bankzaken.sections.customerStatementMessages.title")} helperText={t("forms.bankzaken.sections.customerStatementMessages.helperText")}>
+									<Text>{t("customerStatementMessages.formats.title")}</Text>
+									<List>
+										<ListItem> <ListIcon as={MdCheckCircle} color={"green.500"} /> {t("customerStatementMessages.formats.mt940")}</ListItem>
+										<ListItem> <ListIcon as={MdCheckCircle} color={"green.500"} /> {t("customerStatementMessages.formats.camt")}</ListItem>
+									</List>
+								</FormLeft>
 								<FormRight>
 									<Box>
-										<AddButton onClick={() => onOpen()} />
+										<AddButton onClick={() => addCsmModal.onOpen()} />
 									</Box>
 
 									{csms.length > 0 && (
-										<Table variant={"noLeftPadding"}>
+										<Table variant={"noLeftPadding"} size={"sm"}>
 											<Thead>
 												<Tr>
 													<Th>
@@ -76,9 +74,7 @@ const CustomerStatementMessages = () => {
 													<Th>
 														<FormLabel>{t("global.time")}</FormLabel>
 													</Th>
-													<Th isNumeric>
-														<FormLabel>{t("global.actions.actions")}</FormLabel>
-													</Th>
+													<Th isNumeric />
 												</Tr>
 											</Thead>
 											<Tbody>
