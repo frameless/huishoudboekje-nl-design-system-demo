34f96162973b8790b3efc4cf001add683ad346b4
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-03-08 14:18:48 +0100

    When date is today, yesterday show word. When the year is the same don't show the year

--- mijn-omgeving/src/lib/BanktransactiesList.tsx
+++ mijn-omgeving/src/lib/BanktransactiesList.tsx
@@ -3,19 +3,38 @@ import "@utrecht/components/dist/table/bem.css";
 import React from "react";
 import {Banktransactie} from "./generated/graphql";
 import d from "../utils/dayjs";
+import {useTranslation} from "react-i18next";
 import {Box, FormLabel, Stack, Text} from "@chakra-ui/react";
 import BanktransactieListItem from "./BanktransactieListItem";
 
 const BanktransactiesList: React.FC<{ transacties: Banktransactie[] }> = ({transacties}) => {
+	const {t} = useTranslation();
+
+	const current = new Date();
+	const today = current.getFullYear() + "-" + ("0" + (current.getMonth() + 1)).slice(-2) + "-" + ("0" + current.getDate()).slice(-2);
+	const yesterday = current.getFullYear() + "-" + ("0" + (current.getMonth() + 1)).slice(-2) + "-" + ("0" + (current.getDate() - 1)).slice(-2);
+
 	const bt = transacties.reduce((result, t) => {
-		const trDateAsString = d(t.transactiedatum).format("dddd LL");
-		return {
-			...result,
-			[trDateAsString]: [
-				...(result[trDateAsString] || []),
-				t,
-			],
-		};
+		if (d(t.transactiedatum).year() === d().year()) {
+			const trDateAsString = d(t.transactiedatum).format("dddd DD MMMM");
+			return {
+				...result,
+				[trDateAsString]: [
+					...(result[trDateAsString] || []),
+					t,
+				],
+			};
+		}
+		else {
+			const trDateAsString = d(t.transactiedatum).format("dddd LL");
+			return {
+				...result,
+				[trDateAsString]: [
+					...(result[trDateAsString] || []),
+					t,
+				],
+			};
+		}
 	}, {});
 
 	return (<>
@@ -27,7 +46,11 @@ const BanktransactiesList: React.FC<{ transacties: Banktransactie[] }> = ({trans
 					return (
 						<Stack key={i}>
 							<Box>
-								<FormLabel>{transactionDate}</FormLabel>
+								<FormLabel>{
+									transactionDate === today ? <Text>{t("date.today")}</Text>
+										: transactionDate === yesterday ? <Text>{t("date.yesterday")}</Text>
+											: transactionDate}
+								</FormLabel>
 							</Box>
 							<Stack>
 								{bt[transactionDate].map((transactie, i) => {
