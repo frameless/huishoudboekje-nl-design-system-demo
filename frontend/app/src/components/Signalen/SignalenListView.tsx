811a3ee6d0f938c3759a67a7c276c8cd0706a83e
Koen Brouwer <contact@kbrouwer.nl>
2022-03-10 16:22:27 +0100

    Inactive signalen are now visually different

--- frontend/app/src/components/Signalen/SignalenListView.tsx
+++ frontend/app/src/components/Signalen/SignalenListView.tsx
@@ -1,22 +1,11 @@
-import {Badge, Flex, HStack, Stack, Switch, Text} from "@chakra-ui/react";
+import {Stack, Text} from "@chakra-ui/react";
 import React from "react";
-import {Trans, useTranslation} from "react-i18next";
-import {AppRoutes} from "../../config/routes";
-import {GetSignalenDocument, Signaal, useUpdateSignaalMutation} from "../../generated/graphql";
-import d from "../../utils/dayjs";
-import {currencyFormat2} from "../../utils/things";
-import {useToaster} from "../../utils/useToaster";
-import AuditLogLink from "../Gebeurtenissen/AuditLogLink";
+import {useTranslation} from "react-i18next";
+import {Signaal} from "../../generated/graphql";
+import SignalenListItem from "./SignalenListItem";
 
 const SignalenListView: React.FC<{signalen: Signaal[]}> = ({signalen = []}) => {
 	const {t} = useTranslation();
-	const toast = useToaster();
-
-	const [updateSignaal] = useUpdateSignaalMutation({
-		refetchQueries: [
-			{query: GetSignalenDocument},
-		],
-	});
 
 	if (signalen.length === 0) {
 		return (
@@ -24,53 +13,10 @@ const SignalenListView: React.FC<{signalen: Signaal[]}> = ({signalen = []}) => {
 		);
 	}
 
-	const toggleSignaalActive = (id: string, newActive: boolean) => {
-		updateSignaal({
-			variables: {
-				id,
-				input: {
-					isActive: newActive,
-				},
-			},
-		}).then((result) => {
-			const isActive = result.data?.updateSignaal?.signaal?.isActive;
-			if (isActive) {
-				toast({success: t("messages.enableSignaalSuccess")});
-			}
-			else {
-				toast({success: t("messages.disableSignaalSuccess")});
-			}
-		}).catch(err => {
-			toast.closeAll();
-			toast({error: err.message});
-		});
-	};
-
 	return (
 		<Stack>
 			{signalen.map((s) => (
-				<HStack justify={"center"} key={s.id}>
-					<Stack spacing={1} width={"100%"}>
-						<Text>
-							<Trans i18nKey={"signalen.contextMessage"} values={{
-								afspraak: (s.alarm?.afspraak?.omschrijving),
-								bedrag: currencyFormat2(true).format(parseFloat(s.alarm?.afspraak?.bedrag)),
-							}} components={{
-								strong: <strong />,
-								linkAfspraak: <AuditLogLink to={AppRoutes.ViewAfspraak(s.alarm?.afspraak?.id)}>{s.alarm?.afspraak?.omschrijving}</AuditLogLink>,
-							}} />
-						</Text>
-						<Text fontSize={"sm"} color={"gray.500"}>
-							{d(s.timeUpdated).format("LL LT")}
-						</Text>
-					</Stack>
-					<Flex justify={"center"}>
-						<Badge colorScheme={s.isActive ? "green" : "gray"}>{s.isActive ? t("enabled") : t("disabled")}</Badge>
-					</Flex>
-					<Flex justify={"center"}>
-						<Switch size={"sm"} isChecked={s.isActive} onChange={() => toggleSignaalActive(s.id!, !s.isActive)} />
-					</Flex>
-				</HStack>
+				<SignalenListItem signaal={s} />
 			))}
 		</Stack>
 	);
