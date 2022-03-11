8b4853d6ec8e30c834265936c3ba0b5f0da78311
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-03-11 11:49:59 +0100

    ToekomstList.tsx gesorteerd op betalingen in de toekomst van die persoon

--- mijn-omgeving/src/lib/Toekomst/ToekomstList.tsx
+++ mijn-omgeving/src/lib/Toekomst/ToekomstList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Afspraak} from "../../generated/graphql";
 import {Stack} from "@chakra-ui/react";
 import ToekomstListItem from "./ToekomstListItem";
+import d from "dayjs";
 
 const ToekomstList: React.FC<{ afspraken: Afspraak [] }> = ({afspraken}) => {
 	const getNextDate = (afspraak) => {
