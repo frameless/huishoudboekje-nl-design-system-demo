fc9c026d2b1e5a6b66d0d0d3333ad8600177e481
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-03-17 15:57:22 +0100

    Overzicht met afspraken zichtbaar. Opgeslits in bij en afschrijvingen

--- mijn-omgeving/src/lib/components/Afspraken/Afspraken.tsx
+++ mijn-omgeving/src/lib/components/Afspraken/Afspraken.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {useGetBurgerQuery} from "../../../generated/graphql";
 import Queryable from "../../utils/Queryable";
-import {Link} from "@gemeente-denhaag/link";
-import {ArrowLeftIcon} from "@gemeente-denhaag/icons";
+import {Table, Tbody, Td, Text, Th, Thead, Tr} from "@chakra-ui/react";
 import {Heading2} from "@gemeente-denhaag/typography";
+import {ArrowLeftIcon} from "@gemeente-denhaag/icons";
+import {currencyFormat2} from "../../utils/numberFormat";
+import {Link} from "@gemeente-denhaag/link";
 
 
 const Afspraken: React.FC<{ bsn: number }> = ({bsn}) => {
@@ -19,9 +21,50 @@ const Afspraken: React.FC<{ bsn: number }> = ({bsn}) => {
 				<div>
 					<Link href={"/"} icon={<ArrowLeftIcon />} iconAlign={"start"}>Huishoudboekje</Link>
 					<Heading2>Afspraken</Heading2>
-					{afspraken.map(afspraak => {
-						return afspraak.id
-					})}
+
+					<Text fontSize={"xl"} mt={10}>Bijschrijvingen</Text>
+					<Table>
+						<Thead>
+							<Tr>
+								<Th color={"gray.400"}>Tegenpartij</Th>
+								<Th color={"gray.400"}>Omschrijving</Th>
+								<Th color={"gray.400"} isNumeric>Bedrag</Th>
+							</Tr>
+						</Thead>
+						{afspraken.filter(afspraken => afspraken.credit).map((afspraak, i) => {
+							return (
+								<Tbody key={i}>
+									<Tr key={i}>
+										<Td>{afspraak.tegenrekening.rekeninghouder}</Td>
+										<Td>{afspraak.omschrijving}</Td>
+										<Td isNumeric>{currencyFormat2(true).format((parseInt(afspraak.bedrag) / 100) * (afspraak.credit ? 1 : -1))}</Td>
+									</Tr>
+								</Tbody>
+							)
+						})}
+					</Table>
+
+					<Text fontSize={"xl"} mt={10}>Afschrijvingen</Text>
+					<Table variant={"simple"}>
+						<Thead>
+							<Tr>
+								<Th color={"gray.400"}>Tegenpartij</Th>
+								<Th color={"gray.400"}>Omschrijving</Th>
+								<Th color={"gray.400"} isNumeric>Bedrag</Th>
+							</Tr>
+						</Thead>
+						{afspraken.filter(afspraken => afspraken.credit === false).map((afspraak, i) => {
+							return (
+								<Tbody key={i}>
+									<Tr key={i}>
+										<Td>{afspraak.tegenrekening.rekeninghouder}</Td>
+										<Td>{afspraak.omschrijving}</Td>
+										<Td isNumeric>{currencyFormat2(true).format((parseInt(afspraak.bedrag) / 100) * (afspraak.credit ? 1 : -1))}</Td>
+									</Tr>
+								</Tbody>
+							)
+						})}
+					</Table>
 				</div>
 			)
 		}} />
