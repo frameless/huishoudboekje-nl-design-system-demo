5f9042de68046f558316e96f60a19578600b7cd6
Koen Brouwer <contact@kbrouwer.nl>
2022-04-26 13:12:02 +0200

    Fixed ESLint issues

--- frontend/app/src/components/Configuratie/ParameterItem.tsx
+++ frontend/app/src/components/Configuratie/ParameterItem.tsx
@@ -4,49 +4,48 @@ import React from "react";
 import {useTranslation} from "react-i18next";
 import {Configuratie as IConfiguratie, GetConfiguratieDocument, useDeleteConfiguratieMutation} from "../../generated/graphql";
 import useToaster from "../../utils/useToaster";
-import UpdateParameterModal from "./UpdateParameterModal";
 import DeleteConfirmButton from "../shared/DeleteConfirmButton";
+import UpdateParameterModal from "./UpdateParameterModal";
 
-const ParameterItem: React.FC<FormControlProps & { c: IConfiguratie }> = ({c, ...props}) => {
-    const toast = useToaster();
-    const {t} = useTranslation();
-    const updateParameterModal = useDisclosure();
+const ParameterItem: React.FC<FormControlProps & {c: IConfiguratie}> = ({c, ...props}) => {
+	const toast = useToaster();
+	const {t} = useTranslation();
+	const updateParameterModal = useDisclosure();
 
-    const [deleteConfig] = useDeleteConfiguratieMutation({
-        variables: {
-            key: String(c.id),
-        },
-        refetchQueries: [
-            {query: GetConfiguratieDocument},
-        ],
-    });
+	const [deleteConfig] = useDeleteConfiguratieMutation({
+		variables: {
+			key: String(c.id),
+		},
+		refetchQueries: [
+			{query: GetConfiguratieDocument},
+		],
+	});
 
-    const onDelete = () => {
-        deleteConfig().then(result => {
-            if (result.data?.deleteConfiguratie?.ok) {
-                toast.closeAll();
-                toast({
-                    success: t("messages.configuratie.deleteSuccess"),
-                });
-            }
-        });
-    };
+	const onDelete = () => {
+		deleteConfig().then(result => {
+			if (result.data?.deleteConfiguratie?.ok) {
+				toast.closeAll();
+				toast({
+					success: t("messages.configuratie.deleteSuccess"),
+				});
+			}
+		});
+	};
 
-    return (<>
-            {updateParameterModal.isOpen && <UpdateParameterModal onClose={updateParameterModal.onClose} configuratie={c} />}
+	return (<>
+		{updateParameterModal.isOpen && <UpdateParameterModal onClose={updateParameterModal.onClose} configuratie={c} />}
 
-            <Tr>
-                <Td>{c.id}</Td>
-                <Td>{c.waarde}</Td>
-                <Td>
-                    <HStack>
-                        <IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updateParameterModal.onOpen()} />
-                        <DeleteConfirmButton onConfirm={() => onDelete()} />
-                    </HStack>
-                </Td>
-            </Tr>
-        </>
-    );
+		<Tr>
+			<Td>{c.id}</Td>
+			<Td>{c.waarde}</Td>
+			<Td>
+				<HStack>
+					<IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updateParameterModal.onOpen()} />
+					<DeleteConfirmButton onConfirm={() => onDelete()} />
+				</HStack>
+			</Td>
+		</Tr>
+	</>);
 };
 
 export default ParameterItem;
