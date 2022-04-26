d7c4a1f1d9a08e3def45472a5f6a16f564ebde31
Koen Brouwer <contact@kbrouwer.nl>
2022-04-26 13:12:10 +0200

    Fixed alignment issue

--- frontend/app/src/components/Postadressen/PostadresListItem.tsx
+++ frontend/app/src/components/Postadressen/PostadresListItem.tsx
@@ -1,5 +1,5 @@
 import {DeleteIcon, EditIcon} from "@chakra-ui/icons";
-import {Button, IconButton, Td, Text, Tr, useDisclosure} from "@chakra-ui/react";
+import {Button, HStack, IconButton, Td, Text, Tr, useDisclosure} from "@chakra-ui/react";
 import React, {useEffect, useRef} from "react";
 import {useTranslation} from "react-i18next";
 import {Afdeling, Postadres} from "../../generated/graphql";
@@ -8,69 +8,71 @@ import Alert from "../shared/Alert";
 import UpdatePostadresModal from "./UpdatePostadresModal";
 
 type PostadresListItemProps = {
-    postadres: Postadres,
-    onDelete?: VoidFunction,
-    afdeling: Afdeling,
+	postadres: Postadres,
+	onDelete?: VoidFunction,
+	afdeling: Afdeling,
 };
 
 const PostadresListItem: React.FC<PostadresListItemProps> = ({postadres, afdeling, onDelete}) => {
-    const {t} = useTranslation();
-    const deleteAlert = useDisclosure();
-    const updatePostadresModal = useDisclosure();
-    const editablePreviewRef = useRef<HTMLSpanElement>(null);
+	const {t} = useTranslation();
+	const deleteAlert = useDisclosure();
+	const updatePostadresModal = useDisclosure();
+	const editablePreviewRef = useRef<HTMLSpanElement>(null);
 
-    const onConfirmDelete = () => {
-        if (onDelete) {
-            onDelete();
-        }
-    };
+	const onConfirmDelete = () => {
+		if (onDelete) {
+			onDelete();
+		}
+	};
 
-    /* Truncate the length of the text if EditablePreview's value gets too long. */
-    useEffect(() => {
-        if (editablePreviewRef.current) {
-            editablePreviewRef.current.innerText = truncateText(editablePreviewRef.current.innerText, 50);
-        }
-    });
+	/* Truncate the length of the text if EditablePreview's value gets too long. */
+	useEffect(() => {
+		if (editablePreviewRef.current) {
+			editablePreviewRef.current.innerText = truncateText(editablePreviewRef.current.innerText, 50);
+		}
+	});
 
-    return (<>
-        {updatePostadresModal.isOpen && <UpdatePostadresModal postadres={postadres} onClose={updatePostadresModal.onClose} afdeling={afdeling} />}
-        {deleteAlert.isOpen && (
-            <Alert
-                title={t("messages.postadressen.deleteTitle")}
-                cancelButton={true}
-                confirmButton={
-                    <Button colorScheme={"red"} onClick={onConfirmDelete} ml={3}>
-                        {t("global.actions.delete")}
-                    </Button>
-                }
-                onClose={() => deleteAlert.onClose()}
-            >
-                {t("messages.postadressen.deleteQuestion")}
-            </Alert>
-        )}
+	return (<>
+		{updatePostadresModal.isOpen && <UpdatePostadresModal postadres={postadres} onClose={updatePostadresModal.onClose} afdeling={afdeling} />}
+		{deleteAlert.isOpen && (
+			<Alert
+				title={t("messages.postadressen.deleteTitle")}
+				cancelButton={true}
+				confirmButton={
+					<Button colorScheme={"red"} onClick={onConfirmDelete} ml={3}>
+						{t("global.actions.delete")}
+					</Button>
+				}
+				onClose={() => deleteAlert.onClose()}
+			>
+				{t("messages.postadressen.deleteQuestion")}
+			</Alert>
+		)}
 
-        <Tr>
-            <Td>
-                <Text>{(postadres.straatnaam || "").length > 0 ? postadres.straatnaam : t("unknown")}</Text>
-            </Td>
-            <Td>
-                <Text>{(postadres.huisnummer || "").length > 0 ? postadres.huisnummer : t("unknown")}</Text>
-            </Td>
-            <Td>
-                <Text>{(postadres.postcode || "").length > 0 ? postadres.postcode : t("unknown")}</Text>
-            </Td>
-            <Td>
-                <Text>{(postadres.plaatsnaam || "").length > 0 ? postadres.plaatsnaam : t("unknown")}</Text>
-            </Td>
-            <Td isNumeric>
-                <IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updatePostadresModal.onOpen()} />
-                {onDelete && (
-                    <IconButton icon={<DeleteIcon />} size={"xs"} variant={"ghost"} onClick={() => deleteAlert.onOpen()}
-                                aria-label={t("global.actions.delete")} />
-                )}
-            </Td>
-        </Tr>
-    </>);
+		<Tr>
+			<Td>
+				<Text>{(postadres.straatnaam || "").length > 0 ? postadres.straatnaam : t("unknown")}</Text>
+			</Td>
+			<Td>
+				<Text>{(postadres.huisnummer || "").length > 0 ? postadres.huisnummer : t("unknown")}</Text>
+			</Td>
+			<Td>
+				<Text>{(postadres.postcode || "").length > 0 ? postadres.postcode : t("unknown")}</Text>
+			</Td>
+			<Td>
+				<Text>{(postadres.plaatsnaam || "").length > 0 ? postadres.plaatsnaam : t("unknown")}</Text>
+			</Td>
+			<Td isNumeric>
+				<HStack justify={"flex-end"}>
+					<IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updatePostadresModal.onOpen()} />
+					{onDelete && (
+						<IconButton icon={<DeleteIcon />} size={"xs"} variant={"ghost"} onClick={() => deleteAlert.onOpen()}
+							aria-label={t("global.actions.delete")} />
+					)}
+				</HStack>
+			</Td>
+		</Tr>
+	</>);
 };
 
 export default PostadresListItem;
--- frontend/app/src/components/Rekeningen/RekeningListItem.tsx
+++ frontend/app/src/components/Rekeningen/RekeningListItem.tsx
@@ -1,5 +1,5 @@
 import {DeleteIcon, EditIcon} from "@chakra-ui/icons";
-import {Button, IconButton, Td, Text, Tr, useDisclosure} from "@chakra-ui/react";
+import {Button, HStack, IconButton, Td, Text, Tr, useDisclosure} from "@chakra-ui/react";
 import React, {useEffect, useRef} from "react";
 import {Trans, useTranslation} from "react-i18next";
 import {Rekening} from "../../generated/graphql";
@@ -9,71 +9,74 @@ import PrettyIban from "../shared/PrettyIban";
 import UpdateAfdelingRekeningModal from "./UpdateAfdelingRekeningModal";
 
 type RekeningListItemProps = {
-    rekening: Rekening,
-    onDelete?: VoidFunction
+	rekening: Rekening,
+	onDelete?: VoidFunction
 };
 
 const RekeningListItem: React.FC<RekeningListItemProps> = ({rekening, onDelete}) => {
-    const {t} = useTranslation();
-    const deleteAlert = useDisclosure();
-    const updateAfdelingRekeningModal = useDisclosure();
-    const editablePreviewRef = useRef<HTMLSpanElement>(null);
+	const {t} = useTranslation();
+	const deleteAlert = useDisclosure();
+	const updateAfdelingRekeningModal = useDisclosure();
+	const editablePreviewRef = useRef<HTMLSpanElement>(null);
 
-    const onConfirmDeleteDialog = () => {
-        if (onDelete) {
-            onDelete();
-        }
-    };
-    const onCloseDeleteDialog = () => deleteAlert.onClose();
+	const onConfirmDeleteDialog = () => {
+		if (onDelete) {
+			onDelete();
+		}
+	};
+	const onCloseDeleteDialog = () => deleteAlert.onClose();
 
-    /* Truncate the length of the text if EditablePreview's value gets too long. */
-    useEffect(() => {
-        if (editablePreviewRef.current) {
-            editablePreviewRef.current.innerText = truncateText(editablePreviewRef.current.innerText, 50);
-        }
-    });
+	/* Truncate the length of the text if EditablePreview's value gets too long. */
+	useEffect(() => {
+		if (editablePreviewRef.current) {
+			editablePreviewRef.current.innerText = truncateText(editablePreviewRef.current.innerText, 50);
+		}
+	});
 
 
-    if (!rekening) {
-        return null;
-    }
+	if (!rekening) {
+		return null;
+	}
 
-    return (<>
-        {updateAfdelingRekeningModal.isOpen && <UpdateAfdelingRekeningModal rekening={rekening} onClose={updateAfdelingRekeningModal.onClose} />}
-        {deleteAlert.isOpen && (
-            <Alert
-                title={t("messages.rekeningen.deleteTitle")}
-                cancelButton={true}
-                confirmButton={
-                    <Button colorScheme={"red"} onClick={onConfirmDeleteDialog} ml={3}>
-                        {t("global.actions.delete")}
-                    </Button>
-                }
-                onClose={onCloseDeleteDialog}
-            >
-                <Trans i18nKey={"messages.rekeningen.deleteQuestion"} values={{
-                    iban: formatIBAN(rekening.iban),
-                    rekeninghouder: rekening.rekeninghouder,
-                }} components={{
-                    strong: <strong />,
-                }} />
-            </Alert>)
-        }
+	return (<>
+		{updateAfdelingRekeningModal.isOpen && <UpdateAfdelingRekeningModal rekening={rekening} onClose={updateAfdelingRekeningModal.onClose} />}
+		{deleteAlert.isOpen && (
+			<Alert
+				title={t("messages.rekeningen.deleteTitle")}
+				cancelButton={true}
+				confirmButton={
+					<Button colorScheme={"red"} onClick={onConfirmDeleteDialog} ml={3}>
+						{t("global.actions.delete")}
+					</Button>
+				}
+				onClose={onCloseDeleteDialog}
+			>
+				<Trans i18nKey={"messages.rekeningen.deleteQuestion"} values={{
+					iban: formatIBAN(rekening.iban),
+					rekeninghouder: rekening.rekeninghouder,
+				}} components={{
+					strong: <strong />,
+				}} />
+			</Alert>)
+		}
 
-        <Tr>
-            <Td>
-                <Text>{(rekening.rekeninghouder || "").length > 0 ? rekening.rekeninghouder : t("unknown")}</Text>
-            </Td>
-            <Td>
-                <PrettyIban iban={rekening.iban} />
-            </Td>
-            <Td isNumeric>
-                <IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updateAfdelingRekeningModal.onOpen()} />
-                {onDelete && (
-                    <IconButton icon={<DeleteIcon />} size={"xs"} variant={"ghost"} onClick={() => deleteAlert.onOpen()} aria-label={t("global.actions.delete")} />
-                )}</Td>
-        </Tr>
-    </>);
+		<Tr>
+			<Td>
+				<Text>{(rekening.rekeninghouder || "").length > 0 ? rekening.rekeninghouder : t("unknown")}</Text>
+			</Td>
+			<Td>
+				<PrettyIban iban={rekening.iban} />
+			</Td>
+			<Td isNumeric>
+				<HStack justify={"flex-end"}>
+					<IconButton size={"sm"} variant={"ghost"} colorScheme={"gray"} icon={<EditIcon />} aria-label={t("global.actions.edit")} onClick={() => updateAfdelingRekeningModal.onOpen()} />
+					{onDelete && (
+						<IconButton icon={<DeleteIcon />} size={"xs"} variant={"ghost"} onClick={() => deleteAlert.onOpen()} aria-label={t("global.actions.delete")} />
+					)}
+				</HStack>
+			</Td>
+		</Tr>
+	</>);
 };
 
 export default RekeningListItem;
