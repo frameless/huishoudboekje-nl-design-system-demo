9dd5ee6710dbf7bf051d96d9e427693cac7f2198
Koen Brouwer <contact@kbrouwer.nl>
2022-05-02 14:14:45 +0200

    Updated test for Modal

--- frontend/app/src/tests/Modal.test.tsx
+++ frontend/app/src/tests/Modal.test.tsx
@@ -2,7 +2,6 @@ import {act, fireEvent, screen} from "@testing-library/react";
 import React from "react";
 import {render, unmountComponentAtNode} from "react-dom";
 import Modal from "../components/shared/Modal";
-import {Button} from "@chakra-ui/react";
 
 jest.mock("react-i18next", () => require("./utils/mock-hooks").reactI18NextMock());
 
@@ -15,36 +14,18 @@ beforeEach(() => {
 
 afterEach(() => {
 	unmountComponentAtNode(container!);
-    container!.remove();
-    container = null;
+	container!.remove();
+	container = null;
 });
 
 describe("Modal", () => {
 	it("Show the Modal", async () => {
-		act(() => {
-			render((
-				<Modal title={"Burger toevoegen"} isOpen={true} onClose={() => void (0)}>
-                    Hier kan een formulier staan
-				</Modal>
-			), container);
-		});
-
-		const [modal] = screen.queryAllByRole("ChakraModal");
-		expect(modal).toMatchSnapshot();
-	});
-
-	it("Show the Modal with confirmbutton", () => {
 		const onClose = jest.fn();
-		const onConfirm = jest.fn();
 
 		act(() => {
 			render((
-				<Modal
-					title={"Burger wijzigen"}
-					isOpen={true}
-					onClose={onClose}
-					confirmButton={<Button colorScheme={"primary"} onClick={onConfirm}>Opslaan</Button>}>
-                    Hier kan een formulier staan.
+				<Modal title={"Burger toevoegen"} onClose={onClose}>
+					Hier kan een formulier staan
 				</Modal>
 			), container);
 		});
@@ -55,19 +36,12 @@ describe("Modal", () => {
 		const clickEvent = new Event("click", {
 			bubbles: true,
 			cancelable: true,
-		})
-
-		const confirmButton = screen.getByText("Opslaan");
-		expect(confirmButton).toBeInTheDocument();
-
-		fireEvent(confirmButton, clickEvent);
-		expect(onConfirm).toHaveBeenCalledTimes(1);
+		});
 
 		const closeButton = screen.getByLabelText("Close");
 		expect(closeButton).toBeInTheDocument();
 
 		fireEvent(closeButton, clickEvent);
 		expect(onClose).toHaveBeenCalledTimes(1);
-
 	});
-})
+});
