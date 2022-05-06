061abe23d56dc1efe0d565ee1518355935187afd
Koen Brouwer <contact@kbrouwer.nl>
2022-05-06 14:23:22 +0200

    Added support for bearer token instead of cookie

--- services/authservice/src/SessionHelper.ts
+++ services/authservice/src/SessionHelper.ts
@@ -1,17 +1,54 @@
 import jwt from "jsonwebtoken";
 
+type SessionHelperConfig = {
+	secret: string,
+	expiresIn: string,
+	audience: string,
+	issuer: string,
+}
+
 class SessionHelper {
 	private readonly secret: string;
+	private readonly expiresIn: string;
+	private readonly audience: string;
+	private readonly issuer: string;
 
-	constructor() {
-		this.secret = process.env.SECRET || "test";
+	constructor(config: SessionHelperConfig = {
+		secret: "testtest",
+		expiresIn: "30d",
+		audience: "huishoudboekje",
+		issuer: "huishoudboekje",
+	}) {
+		this.secret = config.secret;
+		this.expiresIn = config.expiresIn;
+		this.audience = config.audience;
+		this.issuer = config.issuer;
+	}
+
+	private _getTokenFromRequest(req: any): string {
+		const cookieToken = req.cookies["app-token"];
+		const bearerToken = req.headers["authorization"]?.replace("Bearer ", "");
+		const token: string = cookieToken || bearerToken;
+		return token;
+	}
+
+	private _getUserFromToken(token: string): any {
+		return jwt.decode(token);
+	}
+
+	getUserFromRequest(req: any): any{
+		const token = this._getTokenFromRequest(req);
+		return this._getUserFromToken(token);
 	}
 
 	isAuthenticated(req: any): boolean {
-		const token = req.cookies["app-token"];
-		const verifiedToken = this.verifyToken(token);
-		console.log({verifiedToken});
-		return verifiedToken;
+		const token = this._getTokenFromRequest(req);
+
+		if (!token) {
+			return false;
+		}
+
+		return this.verifyToken(token);
 	}
 
 	createSession(res, user) {
@@ -24,13 +61,19 @@ class SessionHelper {
 	}
 
 	generateToken(user: any): string {
-		return jwt.sign({user}, this.secret, {expiresIn: "30d"});
+		return jwt.sign({user}, this.secret, {
+			expiresIn: this.expiresIn,
+			audience: this.audience,
+			issuer: this.issuer,
+		});
 	}
 
 	verifyToken(token: string): boolean {
 		try {
-			const verifiedToken = jwt.verify(token, this.secret);
-			console.log({verifiedToken});
+			jwt.verify(token, this.secret, {
+				audience: this.audience,
+				issuer: this.issuer,
+			});
 			return true;
 		}
 		catch (err) {
--- services/authservice/src/server.ts
+++ services/authservice/src/server.ts
@@ -8,7 +8,7 @@ import SessionHelper from "./SessionHelper";
 const sessionHelper = new SessionHelper();
 
 const config = Object.freeze({
-	secret: process.env.SECRET,
+	secret: process.env.SECRET || "testtest",
 	port: process.env.APP_PORT ?? 8080,
 	debug: process.env.NODE_ENV !== "production",
 });
@@ -28,53 +28,84 @@ const server = (prefix: string = "/auth") => {
 		authorizationParams: {
 			response_type: "code",
 		},
+		secret: config.secret,
 		idpLogout: true,
 		authRequired: false,
 		routes: {
-			callback: prefix + "/callback",
-			login: prefix + "/login",
+			// callback: prefix + "/callback",
+			// login: prefix + "/login",
+			login: false,
 			logout: false,
 			postLogoutRedirect: prefix + "/logout",
-		},
-		afterCallback: (req, res, session) => {
-			sessionHelper.createSession(res, session);
-			return {
-				...session,
-			};
 		},
 	}));
 
 	const authRouter = express.Router();
 
-	if (!config.debug) {
+	if (config.debug) {
 		authRouter.get("/", (req, res) => {
 			if (sessionHelper.isAuthenticated(req)) {
 				return res.send(`
 					<h1>Hello ${req.oidc.user?.name}</h1>
-					<a href="/logout">Logout</a>
+					<a href="${prefix}/logout">Logout</a>
 					<a href="${prefix}/me">Check auth</a>
 				`);
 			}
 
 			return res.send(`
 				<h1>Hello!</h1>
-				<a href="/login">Login</a>
+				<a href="${prefix}/login">Login</a>
 				<a href="${prefix}/me">Check auth</a>
 			`);
 		});
 	}
 
 	authRouter.get("/me", async (req, res) => {
+		let user;
+
+		// Check with the OIDC provider if the user is authenticated.
 		if (req.oidc.isAuthenticated()) {
-			sessionHelper.createSession(res, req.oidc.user);
+			console.log("OIDC provider says it's ok.");
+			user = req.oidc.user;
+		}
+		else {
+			console.log("OIDC provider didn't recognize user.");
+
+			// If not, see if the client has provided a valid token.
+			if (sessionHelper.isAuthenticated(req)) {
+				// If so, use the user's data from the token.
+				console.log("Token is valid.");
+
+				user = sessionHelper.getUserFromRequest(req);
+			}
+		}
+
+		// If a user was found, create a session and allow the user in.
+		if (user) {
+			console.log("User found:", user);
+			sessionHelper.createSession(res, user);
 			return res.json({
 				ok: true,
-				user: req.oidc.user,
+				user,
 			});
 		}
 
+		// If no user was found, deny access.
+		console.log("No user found.", user);
 		sessionHelper.destroySession(res);
 		return res.status(401).json({ok: false, message: "Unauthorized"});
+	});
+
+	authRouter.get("/login", (req, res) => {
+		return res.oidc.login({
+			returnTo: prefix + "/login_callback",
+		});
+	});
+
+	authRouter.get("/login_callback", (req, res) => {
+		console.log(req.oidc.user);
+		console.log(req.cookies["return-to"]);
+		res.status(418).json({user: req.oidc.user, returnTo: req.cookies["return-to"]});
 	});
 
 	authRouter.get("/logout", (req, res) => {
@@ -91,6 +122,7 @@ const server = (prefix: string = "/auth") => {
 	return {
 		start: () => app.listen(config.port, () => {
 			console.log(`Server is running.`);
+			console.log({config, prefix});
 		}),
 	};
 };
