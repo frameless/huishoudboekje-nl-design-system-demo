e9592a2ff261394cbaec93c3bf3d7fd81d4cca0b
Koen Brouwer <contact@kbrouwer.nl>
2022-05-09 17:32:41 +0200

    Added data migration for Alarms: datum is now copied over to startDate.

--- services/alarmenservice/prisma/migrations/20220329114625_removed_date_and_added_startdate_and_enddate/migration.sql
+++ services/alarmenservice/prisma/migrations/20220329114625_removed_date_and_added_startdate_and_enddate/migration.sql
@@ -6,7 +6,22 @@
   - Added the required column `startDate` to the `Alarm` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
+-- Create temporary table
+SELECT * FROM "Alarm" into "Alarm_tmp";
+
+-- Alter Alarm table
 ALTER TABLE "Alarm" DROP COLUMN "datum",
-ADD COLUMN     "endDate" TEXT NOT NULL,
-ADD COLUMN     "startDate" TEXT NOT NULL;
+ADD COLUMN "endDate" TEXT,
+ADD COLUMN "startDate" TEXT;
+
+-- Migrate datum to startDate
+UPDATE "Alarms"
+ SET "startDate" = tmp.datum
+ FROM "Alarms" AS a
+ JOIN "Alarm_tmp" AS tmp ON tmp.id = a.id;
+
+-- Add not null constraint to startDate
+ALTER TABLE "Alarm" ALTER COLUMN "startDate" SET NOT NULL;
+
+-- Delete temporary table
+DROP TABLE "Alarm_tmp";
--- services/alarmenservice/prisma/migrations/20220329114901_enddate_optional/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Alarm" ALTER COLUMN "endDate" DROP NOT NULL;
