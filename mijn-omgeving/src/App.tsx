379083ab433633e89bd08e398dd5e609a9afa7d4
Koen Brouwer <contact@kbrouwer.nl>
2022-05-09 17:02:45 +0200

    API_URL is now configurable in envvars in dev mode

--- mijn-omgeving/src/App.tsx
+++ mijn-omgeving/src/App.tsx
@@ -1,21 +1,22 @@
 import "@gemeente-denhaag/design-tokens-components";
 import React from "react";
+import {API_URL} from "./config";
 import Huishoudboekje from "./lib/App";
 import {HuishoudboekjeUser} from "./lib/models";
 
 const App = () => {
-    const user: HuishoudboekjeUser = {
-        bsn: 999999990,
-    };
+	const user: HuishoudboekjeUser = {
+		bsn: 999999990,
+	};
 
-    return (
-        <div style={{
-            width: 800,
-            margin: "0 auto",
-        }}>
-            <Huishoudboekje user={user} config={{apiUrl: "https://test.huishoudboekje.demoground.nl/api/burgers"}} />
-        </div>
-    );
+	return (
+		<div style={{
+			width: 800,
+			margin: "0 auto",
+		}}>
+			<Huishoudboekje user={user} config={{apiUrl: API_URL}} />
+		</div>
+	);
 
 };
 
