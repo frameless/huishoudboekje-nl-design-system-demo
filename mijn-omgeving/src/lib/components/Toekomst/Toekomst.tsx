31145c2fea9c7b60b3f583c9db9cb937f179e98f
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-05-09 09:31:45 +0200

    WIP, wanneer er geen afspraken geplanned staan dan laat die tekst zien

--- mijn-omgeving/src/lib/components/Toekomst/Toekomst.tsx
+++ mijn-omgeving/src/lib/components/Toekomst/Toekomst.tsx
@@ -4,30 +4,35 @@ import Queryable from "../../utils/Queryable";
 import ToekomstList from "./ToekomstList";
 import {Heading2} from "@gemeente-denhaag/typography";
 import BackButton from "../BackButton";
+import {Text} from "@chakra-ui/react";
 
 const Toekomst: React.FC<{ bsn: number }> = ({bsn}) => {
-	const $burger = useGetBurgerQuery({
-		variables: {bsn},
-	});
+    const $burger = useGetBurgerQuery({
+        variables: {bsn},
+    });
 
-	return (
-		<div>
-			<BackButton label={"Huishoudboekje"} />
-			<Heading2>Toekomst</Heading2>
-			<Queryable query={$burger} render={data => {
-				const {rekeningen = [], afspraken = []} = data.burger || {};
+    return (
+        <div>
+            <BackButton label={"Huishoudboekje"} />
+            <Heading2>Toekomst</Heading2>
+            <Queryable query={$burger} render={data => {
+                const {rekeningen = [], afspraken = []} = data.burger || {};
 
-				const burgerRekeningenIds: number[] = rekeningen.map(r => r.id);
-				const filteredAfspraken = afspraken.filter(a => burgerRekeningenIds.includes(a.tegenrekening?.id));
+                const burgerRekeningenIds: number[] = rekeningen.map(r => r.id);
+                const filteredAfspraken = afspraken.filter(a => burgerRekeningenIds.includes(a.tegenrekening?.id)).sort((a, b) => b.by);
 
-				return (
-					<div>
-						<ToekomstList afspraken={filteredAfspraken} />
-					</div>
-				)
-			}} />
-		</div>
-	);
+                return (
+                    <>
+                        {filteredAfspraken.length > 0 ? (
+                            <ToekomstList afspraken={filteredAfspraken} />
+                        ) : (
+                            <Text>Er zijn geen verwachte transacties gevonden.</Text>
+                        )}
+                    </>
+                )
+            }} />
+        </div>
+    );
 };
 
 export default Toekomst;
--- mijn-omgeving/src/lib/components/Toekomst/ToekomstList.tsx
+++ mijn-omgeving/src/lib/components/Toekomst/ToekomstList.tsx
@@ -6,55 +6,54 @@ import ToekomstListItem from "./ToekomstListItem";
 
 const ToekomstList: React.FC<{ afspraken: Afspraak [] }> = ({afspraken}) => {
 
-	const getNextDate = (afspraak) => {
-		let recur;
-		const b = afspraak.betaalinstructie;
+    const getNextDate = (afspraak) => {
+        let recur;
+        const b = afspraak.betaalinstructie;
 
-		if (b?.byMonthDay && b.byMonth) { // Maandelijks of eenmalig
-			if (b.startDate && b.endDate) {
-				const dStartDate = d(b.startDate, "YYYY-MM-DD");
-				const dEndDate = d(b.endDate, "YYYY-MM-DD");
-				const periodLength = Math.abs(dStartDate.diff(dEndDate, "seconds"));
-				const onceDate = d().year(dStartDate.year()).month(b.byMonth[0] - 1).date(b.byMonthDay[0]);
+        if (b?.byMonthDay && b.byMonth) { // Maandelijks of eenmalig
+            if (b.startDate && b.endDate) {
+                const dStartDate = d(b.startDate, "YYYY-MM-DD");
+                const dEndDate = d(b.endDate, "YYYY-MM-DD");
+                const periodLength = Math.abs(dStartDate.diff(dEndDate, "seconds"));
+                const onceDate = d().year(dStartDate.year()).month(b.byMonth[0] - 1).date(b.byMonthDay[0]);
 
-				if (periodLength <= (3600 * 24 * 365)) {
-					if (onceDate.isAfter(d())) { // Eenmalig
-						return onceDate;
-					}
-					return undefined;
-				}
-			}
+                if (periodLength <= (3600 * 24 * 365)) {
+                    if (onceDate.isAfter(d())) { // Eenmalig
+                        return onceDate;
+                    }
+                    return undefined;
+                }
+            }
 
-			recur = d().recur().every(b.byMonthDay).daysOfMonth().every((b?.byMonth || []).map(m => m - 1)).monthsOfYear();
-		}
-		if (b?.byDay) { // Wekelijks
-			recur = d().recur().every(b?.byDay).daysOfWeek();
-		}
+            recur = d().recur().every(b.byMonthDay).daysOfMonth().every((b?.byMonth || []).map(m => m - 1)).monthsOfYear();
+        }
+        if (b?.byDay) { // Wekelijks
+            recur = d().recur().every(b?.byDay).daysOfWeek();
+        }
 
-		return recur ? recur.next(1) : undefined;
-	}
+        return recur ? recur.next(1) : undefined;
+    }
 
 
-	return (
-		<Stack>
-			{afspraken.map((afspraak, i) => {
-				const nextDate = getNextDate(afspraak);
+    return (
+        <Stack>
+            <Table variant={"simple"}>
+                <Tbody>
+                    {afspraken.map((afspraak, i) => {
+                        const nextDate = getNextDate(afspraak);
 
-				if (nextDate && d(nextDate).isAfter(d())) {
-					const format = d(nextDate).year() !== d().year() ? "dddd D MMMM YYYY" : "dddd D MMMM";
-					return (
-						<Table variant={"simple"} key={i}>
-							<Tbody>
-								<ToekomstListItem datum={d(nextDate).format(format)} bedrag={afspraak.bedrag} omschrijving={afspraak.omschrijving} rekening={afspraak.tegenrekening?.iban} />
-							</Tbody>
-						</Table>
-					)
-				}
-
-				return null;
-			})}
-		</Stack>
-	)
+                        if (nextDate && d(nextDate).isAfter(d())) {
+                            const format = d(nextDate).year() !== d().year() ? "dddd D MMMM YYYY" : "dddd D MMMM";
+                            return (
+                                <ToekomstListItem key={i} datum={d(nextDate).format(format)} bedrag={afspraak.bedrag} omschrijving={afspraak.omschrijving} rekening={afspraak.tegenrekening?.iban} />
+                            )
+                        }
+                        return null;
+                    })}
+                </Tbody>
+            </Table>
+        </Stack>
+    )
 
 };
 
