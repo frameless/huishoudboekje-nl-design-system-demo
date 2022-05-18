78b05d0677830405a16f2d1b35de87307280c191
AnitavanHengel <anita.van.hengel@topicus.nl>
2022-05-18 11:51:52 +0200

    Refactor signalen.

--- backend/hhb_backend/graphql/mutations/signalen/create_signaal.py
+++ backend/hhb_backend/graphql/mutations/signalen/create_signaal.py
@@ -1,19 +1,8 @@
 """ GraphQL mutatie voor het aanmaken van een Signaal """
-from hhb_backend.graphql.models import signaal
 import graphene
 from hhb_backend.graphql.models.signaal import Signaal
-from hhb_backend.graphql.utils.gebruikersactiviteiten import (log_gebruikers_activiteit, gebruikers_activiteit_entities)
-import requests
-from graphql import GraphQLError
-from hhb_backend.graphql import settings
+from hhb_backend.graphql.mutations.signalen.signalen import SignaalHelper, CreateSignaalInput
 
-class CreateSignaalInput(graphene.InputObjectType):
-    alarmId = graphene.String()
-    banktransactieIds = graphene.List(graphene.Int) 
-    isActive = graphene.Boolean()
-    type = graphene.String()
-    actions = graphene.List(graphene.String, default_value=[])
-    context = graphene.String()
 
 class CreateSignaal(graphene.Mutation):
     class Arguments:
@@ -22,24 +11,10 @@ class CreateSignaal(graphene.Mutation):
     ok = graphene.Boolean()
     signaal = graphene.Field(lambda: Signaal)
 
-    def gebruikers_activiteit(self, _root, info, *_args, **_kwargs):
-        return dict(
-            action=info.field_name,
-            entities=gebruikers_activiteit_entities(
-                entity_type="signaal", result=self, key="signaal"
-            ),
-            after=dict(signaal=self.signaal),
-        )
 
     @staticmethod
-    @log_gebruikers_activiteit
     async def mutate(_root, _info, input: CreateSignaalInput):
         """ Mutatie voor het aanmaken van een nieuw Signaal """
+        response_signaal = await SignaalHelper.create(_root, _info, input)
 
-        create_signaal_response = requests.post(f"{settings.SIGNALENSERVICE_URL}/signals/", json=input, headers={"Content-type": "application/json"})
-        if create_signaal_response.status_code != 201:
-            raise GraphQLError(f"Aanmaken van het signaal is niet gelukt.")
-
-        response_signaal = create_signaal_response.json()["data"]
-
-        return CreateSignaal(signaal=response_signaal, ok=True)
+        return CreateSignaal(signaal=response_signaal.signaal, ok=True)
---
+++ backend/hhb_backend/graphql/mutations/signalen/signalen.py
@@ -0,0 +1,76 @@
+
+import graphene
+import requests
+from graphql import GraphQLError
+from flask import request
+
+from hhb_backend.graphql.utils.gebruikersactiviteiten import (log_gebruikers_activiteit, gebruikers_activiteit_entities)
+from hhb_backend.graphql import settings
+
+class CreateSignaalInput(graphene.InputObjectType):
+    alarmId = graphene.String()
+    banktransactieIds = graphene.List(graphene.Int) 
+    isActive = graphene.Boolean()
+    type = graphene.String()
+    actions = graphene.List(graphene.String, default_value=[])
+    context = graphene.String()
+
+class UpdateSignaalInput(graphene.InputObjectType):
+    alarmId = graphene.String()
+    banktransactieIds = graphene.List(graphene.Int)
+    isActive = graphene.Boolean()
+    type = graphene.String()
+    actions = graphene.List(graphene.String, default_value=[])
+    context = graphene.String()
+
+
+class SignaalHelper:
+    def __init__(self, signaal, previous, ok) -> None:
+        self.signaal = signaal
+        self.previous = previous
+        self.ok = ok
+
+    def gebruikers_activiteit(self, _root, info, *_args, **_kwargs):
+        return dict(
+            action=info.field_name,
+            entities=gebruikers_activiteit_entities(
+                entity_type="signaal", result=self, key="signaal"
+            ),
+            before=dict(signaal=self.previous),
+            after=dict(signaal=self.signaal),
+        )
+
+    @log_gebruikers_activiteit
+    async def create(_root, _info, input: CreateSignaalInput):
+        create_signaal_response = requests.post(f"{settings.SIGNALENSERVICE_URL}/signals/", json=input, headers={"Content-type": "application/json"})
+        if create_signaal_response.status_code != 201:
+            raise GraphQLError(f"Aanmaken van het signaal is niet gelukt.")
+
+        response_signaal = create_signaal_response.json()["data"]
+        return SignaalHelper(signaal=response_signaal, previous=dict(), ok=True)
+
+    @log_gebruikers_activiteit
+    async def delete(_root, _info, id):
+        previous = await request.dataloader.signalen_by_id.load(id)
+        if not previous:
+            raise GraphQLError(f"Signaal with id {id} not found")
+
+        response = requests.delete(f"{settings.SIGNALENSERVICE_URL}/signals/{id}")
+        if response.status_code != 204:
+            raise GraphQLError(f"Upstream API responded: {response.json()}")
+
+        return SignaalHelper(signaal=previous, previous=dict(), ok=True)  # for better logging of the gebruikersactiviteit, the previous alarm is stored in alarm and not previous.
+
+    @log_gebruikers_activiteit
+    async def update(_root, _info, id: str, input: UpdateSignaalInput):
+        previous_response = requests.get(f"{settings.SIGNALENSERVICE_URL}/signals/{id}", headers={"Content-type": "application/json"})
+        if previous_response.status_code != 200:
+            raise GraphQLError(f"Signaal bestaat niet.")
+        previous = previous_response.json()
+
+        response = requests.put(f"{settings.SIGNALENSERVICE_URL}/signals/{id}", json=input, headers={"Content-type": "application/json"})
+        if response.status_code != 200:
+            raise GraphQLError(f"Upstream API responded: {response.json()}")
+        response_signaal = response.json()['data']
+
+        return SignaalHelper(signaal=response_signaal, previous=previous, ok=True)
