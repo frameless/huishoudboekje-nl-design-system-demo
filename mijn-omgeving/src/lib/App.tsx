06d25e5e72097f6bffe5fa1ff48e182a3dc4bb9d
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-05-24 14:17:03 +0200

    Small Styling Changes to "mijn-omgeving"

--- mijn-omgeving/src/lib/App.tsx
+++ mijn-omgeving/src/lib/App.tsx
@@ -1,4 +1,5 @@
 import {ApolloProvider} from "@apollo/client";
+import {ChakraProvider} from "@chakra-ui/react";
 import React from "react";
 import {MemoryRouter as Router, Outlet, Route, Routes} from "react-router-dom";
 import BanktransactiesPage from "./components/Banktransacties";
@@ -11,23 +12,25 @@ import BanktransactieDetailPage from "./components/Banktransacties/Banktransacti
 import AfsprakenView from "./components/Afspraken/AfsprakenView";
 
 const App: React.FC<{ user: HuishoudboekjeUser, config: HuishoudboekjeConfig }> = ({user, config}) => {
-	const {apiUrl} = config;
-	return (
-		<ApolloProvider client={createApolloClient({apiUrl})}>
-			<Router>
-				<Routes>
-					<Route path={"/"} element={<Dashboard />} />
-					<Route path={"/banktransacties"} element={<Outlet />}>
-						<Route index element={<BanktransactiesPage bsn={user.bsn} />} />
-						<Route path={":id"} element={<BanktransactieDetailPage bsn={user.bsn} />} />
-					</Route>
-					<Route path={"/toekomst"} element={<Toekomst bsn={user.bsn} />} />
-					<Route path={"/gegevens"} element={<DetailBurgerView bsn={user.bsn} />} />
-					<Route path={"/afspraken"} element={<AfsprakenView bsn={user.bsn} />} />
-				</Routes>
-			</Router>
-		</ApolloProvider>
-	);
+    const {apiUrl} = config;
+    return (
+        <ChakraProvider>
+            <ApolloProvider client={createApolloClient({apiUrl})}>
+                <Router>
+                    <Routes>
+                        <Route path={"/"} element={<Dashboard />} />
+                        <Route path={"/banktransacties"} element={<Outlet />}>
+                            <Route index element={<BanktransactiesPage bsn={user.bsn} />} />
+                            <Route path={":id"} element={<BanktransactieDetailPage bsn={user.bsn} />} />
+                        </Route>
+                        <Route path={"/toekomst"} element={<Toekomst bsn={user.bsn} />} />
+                        <Route path={"/gegevens"} element={<DetailBurgerView bsn={user.bsn} />} />
+                        <Route path={"/afspraken"} element={<AfsprakenView bsn={user.bsn} />} />
+                    </Routes>
+                </Router>
+            </ApolloProvider>
+        </ChakraProvider>
+    );
 };
 
 export default App;
