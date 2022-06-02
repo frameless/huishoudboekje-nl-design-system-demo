64629faed6f2e96e6f3a7c68f7cf0a452b18f5f1
Koen Brouwer <contact@kbrouwer.nl>
2022-06-02 11:32:45 +0200

    Now using the correct way to mock implementations

--- backend-burgers/tests/dataloader.test.ts
+++ backend-burgers/tests/dataloader.test.ts
@@ -1,14 +1,17 @@
+import Huishoudboekjeservice from "../src/datasources/huishoudboekjeservice";
 import server from "../src/server";
 import burgers from "./fixtures/burgers.json";
 import {GetOneBurger} from "./queries";
 
-jest.setTimeout(60000);
+jest.mock("../src/datasources/huishoudboekjeservice");
 
 describe("BurgerLoader", () => {
 
 	describe("Given the burger exists", () => {
 
 		it("gets the entire burger", async () => {
+			jest.spyOn(Huishoudboekjeservice.prototype, "getBurgersByBsns").mockImplementation(async (bsns) => burgers.filter(b => bsns.includes(b.bsn)));
+
 			const result = await server.executeOperation({
 				query: GetOneBurger,
 				variables: {
