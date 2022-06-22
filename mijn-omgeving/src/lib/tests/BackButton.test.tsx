a2322e5c8a44c0d74edda09ac903cd2987142f04
Chantalle den Hertog <chantalle.den.hertog@topicus.nl>
2022-06-22 12:13:32 +0200

    BackButton.test.tsx coverage 100%

--- mijn-omgeving/src/lib/tests/BackButton.test.tsx
+++ mijn-omgeving/src/lib/tests/BackButton.test.tsx
@@ -1,11 +1,17 @@
-import {act} from "@testing-library/react";
+import {act, fireEvent, getByText} from "@testing-library/react";
 import React from "react";
 import {render, unmountComponentAtNode} from "react-dom";
 import BackButton from "../components/BackButton";
 
-jest.mock("react-router-dom", () => require("./utils/mock-hooks").reactRouterDomMock());
+// jest.mock("react-router-dom", () => require("./utils/mock-hooks").reactRouterDomMock());
 
 let container: HTMLDivElement | null = null;
+
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom') as any,
+    useNavigate: () => mockedUsedNavigate,
+}));
 
 beforeEach(() => {
     container = document.createElement("div");
@@ -36,12 +42,45 @@ describe("Back Button", () => {
         expect(container!.textContent).toBe("Huishoudboekje")
     });
 
-    // it('should fire an onClick event', function () {
-    //     const fn = jest.fn();
-    //     act(() => {
-    //         render(<BackButton to={"/huishoudboekje"} />, container);
-    //     });
-    //
-    //     expect()
-    // });
+    it('should trigger an onClick event and different navigation than default', function () {
+
+
+        act(() => {
+            render(<BackButton to={"/huishoudboekje"} />, container);
+        });
+
+        const clickEvent = new Event("click", {
+            bubbles: true,
+            cancelable: true,
+        });
+
+        const element = getByText(container!, "Terug");
+
+        fireEvent(element, clickEvent);
+        expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/huishoudboekje");
+
+
+    });
+
+    it('should trigger an onClick event and default navigation', function () {
+
+
+        act(() => {
+            render(<BackButton />, container);
+        });
+
+        const clickEvent = new Event("click", {
+            bubbles: true,
+            cancelable: true,
+        });
+
+        const element = getByText(container!, "Terug");
+
+        fireEvent(element, clickEvent);
+        expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/");
+
+
+    });
 })
