3ae802f261d1a8b4aaaa3b0400527269570b3a20
Koen Brouwer <contact@kbrouwer.nl>
2022-06-23 13:59:08 +0200

    WIP: refactoring Auth.

--- backend/hhb_backend/auth/__init__.py
+++ backend/hhb_backend/auth/__init__.py
@@ -1,25 +1,25 @@
+import itsdangerous
+import jwt
 import logging
 import re
 import secrets
 import uuid
 from datetime import timedelta
-from time import time
-
-import itsdangerous
-import jwt
-from isodate import parse_duration
 from flask import Flask, g, make_response, redirect, request, session
+from hhb_backend.auth.models import User
+from isodate import parse_duration
 from itsdangerous import TimedJSONWebSignatureSerializer
 from jwt import InvalidTokenError
-
-from hhb_backend.auth.models import User
+from time import time
 
 
 class Auth():
     def __init__(self, app: Flask, anonymous_rolename: str = 'anonymous', default_rolename='user'):
         self.logger = logger = logging.getLogger(__name__)
+        self.audience = app.config.get("AUTH_AUDIENCE", None) or "huishoudboekje-medewerkers"
+        self.secret = app.config.get("JWT_SECRET", None) or "test"
 
-        self.audience = app.config.get("AUTH_AUDIENCE", None) or "https://huishoudboekje.nlx.reviews"
+        self.logger.debug(f"JWT_SECRET {self.secret}, AUTH_AUDIENCE {self.audience}")
 
         app.before_request(self._init_auth)
 
@@ -51,32 +51,45 @@ class Auth():
     def _exp(self) -> dict:
         return {"exp": int(time()) + self.exp_offset} if self.exp_offset >= 0 else {}
 
-    def _user_loader(self):
-        if self.current_user:
-            return self.current_user
+    def _get_token_from_cookie(self):
+        return request.cookies.get('app-token', None)
 
-        # self.logger.debug(f"_user_loader: oidc_id_token={g.oidc_id_token}, authorization={request.headers.get('authorization', None)}")
+    def _get_token_from_header(self):
         if 'authorization' in request.headers:
             token_search = re.search('bearer (.*)', request.headers['authorization'], re.IGNORECASE)
-
             if token_search:
                 token = token_search.group(1)
-                
-        else: 
-            #iets met een cookie waarde van token zit in app-token 
-            self.logger.debug("Todo, find token in cookie.")
-            token = ""
+                self.logger.debug(f"_token_loader: Bearer found: {token}")
+                return token
 
-        if token:
+        return None
+
+    def _token_loader(self):
+        token_cookie = self._get_token_from_cookie()
+        token_header = self._get_token_from_header()
+
+        token = token_cookie or token_header or None
+        self.logger.debug(f"_token_loader: Token: {token}")
+        return token
+
+    def _user_loader(self):
+        token = self._token_loader()
+
+        unverifiedToken = jwt.decode(token, options={"verify_signature": False})
+        self.logger.debug(f"""_user_loader: Token: {token}, claims: {unverifiedToken}""")
+
+        if token is not None:
             try:
-                claims = jwt.decode(token, self.secret, algorithms="HS256", audience=self.audience)
-                if email := claims.get('email', None):
-                    if name := claims.get('name', None):
-                        user = User(email=email, name=name)
-                        self.logger.debug(f"_user_loader: token user: {user}")
-                        return user
+                # Try to decode and verify the token
+                claims = jwt.decode(token, self.secret, algorithms=['HS256'], audience=self.audience)
+                email = claim.get('email', None)
+                name = claim.get('name', None)
+                if email and name:
+                    user = User(email=email, name=name)
+                    self.logger.debug(f"_user_loader: token user: {user}")
+                    return user
             except InvalidTokenError as err:
-                self.logger.warning(f"""_user_loader: {err}; claims: {jwt.decode(token, algorithms="HS256", options={"verify_signature": False})}""")
+                self.logger.warning(f"""_user_loader: {err}; claims: {unverifiedToken}""")
 
         self.logger.debug(f"_user_loader: no user")
         return None
