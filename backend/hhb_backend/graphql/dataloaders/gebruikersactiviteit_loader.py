53bc11452ce95ad76e7daafed95db8be88f7630a
Koen Brouwer <contact@kbrouwer.nl>
2022-09-05 16:42:30 +0200

    Added types for paged results

--- backend/hhb_backend/graphql/dataloaders/gebruikersactiviteit_loader.py
+++ backend/hhb_backend/graphql/dataloaders/gebruikersactiviteit_loader.py
@@ -17,17 +17,17 @@ class GebruikersactiviteitLoader(DataLoader[Gebruikersactiviteit]):
     def by_burgers(self, burger_ids: List[int]) -> List[Gebruikersactiviteit]:
         return self.load(burger_ids, filter_item="filter_burgers")
 
-    def by_burgers_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]):
+    def by_burgers_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]) -> dict:
         return self.load_paged(keys, filter_item="filter_burgers", **kwargs)
 
     def by_afspraak(self, afspraak_id: int) -> List[Gebruikersactiviteit]:
         return self.load(afspraak_id, filter_item="filter_afspraken")
 
-    def by_afspraken_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]):
+    def by_afspraken_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]) -> dict:
         return self.load_paged(keys=keys, filter_item="filter_afspraken", **kwargs)
 
     def by_huishouden(self, huishouden_id: int) -> List[Gebruikersactiviteit]:
         return self.load(huishouden_id, filter_item="filter_huishouden")
 
-    def by_huishouden_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]):
+    def by_huishouden_paged(self, keys: List[int] = None, **kwargs: Unpack[DataLoaderOptions]) -> dict:
         return self.load_paged(keys=keys, filter_item="filter_huishouden", **kwargs)
