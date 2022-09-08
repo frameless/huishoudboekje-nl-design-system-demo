cb18b4e7c439ba55bd8bf91dc13b7f0312e58526
Koen Brouwer <contact@kbrouwer.nl>
2022-09-08 14:47:01 +0200

    Migrated from Flask-Script to Flask-Migrate

--- services/huishoudboekje_service/huishoudboekje_service/app.py
+++ services/huishoudboekje_service/huishoudboekje_service/app.py
@@ -2,6 +2,7 @@
 import logging
 import os
 from flask import Flask, Response
+from flask_migrate import Migrate
 
 from huishoudboekje_service.views import (
     BurgerView,
@@ -33,6 +34,7 @@ def create_app(config_name='huishoudboekje_service.config.Config'):
     logging.info(f"Starting {__name__} with {config_name}")
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     @app.route('/health')
     def health():
