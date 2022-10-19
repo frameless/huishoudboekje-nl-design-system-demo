48018ed8c5691ffc2b9c116f08e5d88513b254e6
Koen Brouwer <contact@kbrouwer.nl>
2022-10-19 11:06:04 +0200

    Fixed eslint warnings

--- frontend/app/src/tests/utils/mock-hooks.tsx
+++ frontend/app/src/tests/utils/mock-hooks.tsx
@@ -9,6 +9,7 @@ export const reactI18NextMock = () => ({
 
 export const reactRouterDomMock = () => ({
 	// this mock makes sure any components using the translate hook can use it without a warning being shown
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	useNavigate: (str) => {
 		// Don't do anything
 	},
