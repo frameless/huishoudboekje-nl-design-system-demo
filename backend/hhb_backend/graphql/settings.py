83ae88470c25d721d9491893d17a9aec20b83e4d
Koen Brouwer <contact@kbrouwer.nl>
2022-10-21 10:41:42 +0200

    Added Unleash class that will handle feature flags

--- backend/hhb_backend/graphql/settings.py
+++ backend/hhb_backend/graphql/settings.py
@@ -8,3 +8,4 @@ LOG_SERVICE_URL = os.getenv('LOG_SERVICE_URL', "http://localhost:8005")
 POSTADRESSEN_SERVICE_URL = os.getenv('POSTADRESSENSERVICE_URL', "http://localhost:8007") + '/v1'
 ALARMENSERVICE_URL = os.getenv('ALARMENSERVICE_URL', "http://localhost:8008") + '/v1'
 SIGNALENSERVICE_URL = os.getenv('SIGNALENSERVICE_URL', "http://localhost:8009") + '/v1'
+UNLEASHSERVICE_URL = os.getenv('UNLEASHSERVICE_URL', "http://localhost:8010")
