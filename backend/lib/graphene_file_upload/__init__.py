a437d782b463b640c7bc842f5792470e774922e2
Koen Brouwer <contact@kbrouwer.nl>
2022-11-03 16:08:48 +0100

    Moved things around

--- backend/lib/graphene_file_upload/__init__.py
+++ backend/lib/graphene_file_upload/__init__.py
@@ -1,9 +1,9 @@
 """Apply multipart request spec to flask"""
 from flask import request
-from graphql_server.flask import GraphQLView
 from graphql_server import load_json_body
+from graphql_server.flask import GraphQLView
 
-from utils import place_files_in_operations
+from .utils import place_files_in_operations
 
 
 class FileUploadGraphQLView(GraphQLView):
