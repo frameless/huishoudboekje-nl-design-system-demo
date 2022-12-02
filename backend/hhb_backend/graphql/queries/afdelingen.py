442ec61249ce8407e199d7711a83a29b463cc242
Koen Brouwer <contact@kbrouwer.nl>
2022-12-02 22:53:08 +0100

    Only create audit log when the request succeeded

--- backend/hhb_backend/graphql/queries/afdelingen.py
+++ backend/hhb_backend/graphql/queries/afdelingen.py
@@ -12,11 +12,12 @@ class AfdelingQuery:
 
     @classmethod
     def resolver(cls, _, info, id):
+        result = hhb_dataloader().afdelingen.load_one(id)
         AuditLogging.create(
             action=info.field_name,
             entities=gebruikers_activiteit_entities(entity_type="afdeling", result=id)
         )
-        return hhb_dataloader().afdelingen.load_one(id)
+        return result
 
 
 class AfdelingenQuery:
