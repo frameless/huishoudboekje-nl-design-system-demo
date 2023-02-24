aff6a434f065f9043b304fdf600d814a4284b805
Martijn Lankhof <martijn.lankhof@topicus.nl>
2023-02-24 16:09:17 +0100

    Added burger transacties endpoint hhb service and way to call it from rapportage service

--- services/rapportage_service/setup.py
+++ services/rapportage_service/setup.py
@@ -15,6 +15,7 @@ setup(
         "jsonschema>=4.15.0,<5",
         'gunicorn>=20.1.0,<21',
         'Werkzeug>=2.2.2,<3',
+        'requests>=2.28.1,<3',
         'MarkupSafe==2.1.1',
 		'future==0.18.2',
 		'WTForms==3.0.1'
