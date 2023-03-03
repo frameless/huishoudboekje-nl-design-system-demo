702209c031ab606243bb1858e78c2ff106405846
Justin Booij <Justin.Booij@topicus.nl>
2023-03-03 17:12:22 +0100

    Added query for transactie without suggesties & used that if transactie is afgeletterd
    - Transacties can now be gotten without suggesties to increase performance of the transactie view
    - transactie view is loaded based on being afgeletterd (no suggesties) or not (with suggesties).
    - No visual changes (this data was not used when afgeletterd items were being shown)
    
    TODO: try to get state working to cleanly refresh this component

--- frontend/app/src/components/Bankzaken/Transacties/TransactieDetailPage.tsx
+++ frontend/app/src/components/Bankzaken/Transacties/TransactieDetailPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useState, createContext} from "react";
 import {useTranslation} from "react-i18next";
-import {useParams} from "react-router-dom";
-import {Afspraak, BankTransaction, Rubriek, useGetTransactieQuery} from "../../../generated/graphql";
+import {useParams, useLocation} from "react-router-dom";
+import {Afspraak, BankTransaction, Rubriek, useGetTransactieQuery, useGetTransactieAfgeletterdQuery} from "../../../generated/graphql";
 import Queryable from "../../../utils/Queryable";
 import Page from "../../shared/Page";
 import PageNotFound from "../../shared/PageNotFound";
@@ -9,21 +9,35 @@ import TransactieItemView from "./TransactieItemView";
 
 const TransactieDetailPage = () => {
 	const {id = ""} = useParams<{id: string}>();
+	const location = useLocation();
 	const {t} = useTranslation();
 
-	const $transactie = useGetTransactieQuery({
-		fetchPolicy: "no-cache", // This is "no-cache" because the data changes
-		variables: {
-			id: parseInt(id),
-		},
-	});
+	const [afgeletterd, setAfgeletterd] = useState(location.state.afgeletterd)
+
+	const $transactie = () => {
+		if (afgeletterd == true) {
+			return useGetTransactieAfgeletterdQuery({
+				fetchPolicy: "no-cache", // This is "no-cache" because the data changes
+				variables: {
+					id: parseInt(id),
+				},
+			});
+
+		}
+		return useGetTransactieQuery({
+			fetchPolicy: "no-cache", // This is "no-cache" because the data changes
+			variables: {
+				id: parseInt(id),
+			},
+		});
+	}
 
 	return (
-		<Queryable query={$transactie} children={(data => {
+		<Queryable query={$transactie()} children={(data => {
 			const transactie: BankTransaction = data.bankTransaction;
 			const afspraken: Afspraak[] = []
-			transactie.suggesties?.forEach(suggestie =>{
-				const similar = suggestie.similarAfspraken? suggestie.similarAfspraken : []
+			transactie.suggesties?.forEach(suggestie => {
+				const similar = suggestie.similarAfspraken ? suggestie.similarAfspraken : []
 				afspraken.push(...similar)
 			})
 
