8c6c9194003e0624511844b00de7e602bf6b2910
Martijn Lankhof <martijn.lankhof@topicus.nl>
2023-03-17 16:41:16 +0100

    added organisatie rekeningen request in organisatie loader

--- backend/hhb_backend/graphql/dataloaders/organisatie_loader.py
+++ backend/hhb_backend/graphql/dataloaders/organisatie_loader.py
@@ -1,11 +1,26 @@
+from typing import List
 from hhb_backend.graphql.dataloaders.base_loader import DataLoader
 from hhb_backend.graphql.settings import ORGANISATIE_SERVICES_URL
 from hhb_backend.service.model.organisatie import Organisatie
+import requests
+from graphql import GraphQLError
+from hhb_backend.graphql.utils.upstream_error_handler import UpstreamError
 
 
 class OrganisatieLoader(DataLoader[Organisatie]):
     service = ORGANISATIE_SERVICES_URL
     model = "organisaties"
 
+    
+    def organisatie_rekeningen_by_rekening_id(self, rekening_id: int) -> List[int]:
+        try:
+            url = f"{self.service}/rekeningen/{rekening_id}/organisatie/rekeningen"
+            response = requests.get(url)
 
-    #rekening ids ophalen van organisatie met rekening _id
+        except requests.exceptions.ConnectionError:
+            raise GraphQLError(f"Failed to connect to service {self.service}")
+
+        if response.status_code != 200:
+            raise UpstreamError(response, f"Request to {url}  failed.")
+        
+        return response.json()["data"]
