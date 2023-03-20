f4d1ddd75da470251ab4d2441334dc6461d4de0c
Martijn Lankhof <martijn.lankhof@topicus.nl>
2023-03-20 17:01:23 +0100

    Added test to test logic that transactie can be booked on organisation level when it has no matching rekening directly

--- backend/tests/processen/test_transactie_suggesties.py
+++ backend/tests/processen/test_transactie_suggesties.py
@@ -46,19 +46,19 @@ def test_transactie_suggesties_matches(test_request_context):
                           'bedrag': 450000, 'omschrijving': 'Nog meer geld overmaken',
                           'credit': True, 'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 4,
                           'interval': 'P0Y1M0W0D', 'journaalposten': [], 'zoektermen': ['15814016000676480'],
-                          'organisatie_id': None,
+                          'afdeling_id': None,
                           'overschrijvingen': [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63], 'rubriek_id': None,
                           'valid_from': '2020-01-01', 'tegen_rekening_id': 2},
                          {'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Money', 'credit': True,
                           'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['1070123412341234'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['1070123412341234'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2020-01-01', 'tegen_rekening_id': 3},
                          {'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Money', 'credit': True,
                           'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 5, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15614016000323121'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15614016000323121'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2020-01-01', 'tegen_rekening_id': 5}
                          ]})
@@ -123,19 +123,19 @@ def test_transactie_suggesties_multiple_matches(test_request_context):
                           'bedrag': 450000, 'omschrijving': 'Nog meer geld overmaken',
                           'credit': True, 'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 4,
                           'interval': 'P0Y1M0W0D', 'journaalposten': [], 'zoektermen': ['15814016000676480'],
-                          'organisatie_id': None,
+                          'afdeling_id': None,
                           'overschrijvingen': [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 2},
                          {'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Zorg', 'credit': True,
                           'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['1070123412341234'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['1070123412341234'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 3},
                          {'aantal_betalingen': 12,
                           'bedrag': 54646, 'omschrijving': 'Zorg 2', 'credit': True,
                           'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 5, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['12341234'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['12341234'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 3}
                          ]})
@@ -192,13 +192,13 @@ def test_transactie_suggesties_matching_zoekterm_dates_overlap(test_request_cont
                 'data': [{'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Zorg', 'credit': True,
                           'valid_through': '2022-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2020-01-01', 'tegen_rekening_id': 2},
                          {'aantal_betalingen': 12,
                           'bedrag': 54646, 'omschrijving': 'Zorg 2', 'credit': True,
                           'valid_through': '2022-12-31', 'gebruiker_id': 1, 'id': 5, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2019-01-01', 'tegen_rekening_id': 2}
                          ]})       
@@ -252,7 +252,7 @@ def test_transactie_suggesties_startdate_after_transaction_date(test_request_con
                 'data': [{'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Zorg', 'credit': True,
                           'valid_through': '2022-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 2}
                          ]})
@@ -304,13 +304,13 @@ def test_transactie_suggesties_matching_zoekterm_enddate_passed(test_request_con
                 'data': [{'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Zorg', 'credit': True,
                           'valid_through': '2022-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2020-01-01', 'tegen_rekening_id': 2},
                          {'aantal_betalingen': 12,
                           'bedrag': 54646, 'omschrijving': 'Zorg 2', 'credit': True,
                           'valid_through': '2020-12-31', 'gebruiker_id': 1, 'id': 5, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': ['15814016000676480'], 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2019-01-01', 'tegen_rekening_id': 2}
                          ]})
@@ -362,13 +362,13 @@ def test_transactie_suggesties_no_matches(test_request_context):
                 'data': [{'aantal_betalingen': 12,
                           'bedrag': 450000, 'omschrijving': 'Nog meer geld overmaken',
                           'credit': True, 'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 4,
-                          'interval': 'P0Y1M0W0D', 'journaalposten': [], 'zoektermen': None, 'organisatie_id': None,
+                          'interval': 'P0Y1M0W0D', 'journaalposten': [], 'zoektermen': None, 'afdeling_id': None,
                           'overschrijvingen': [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 2},
                          {'aantal_betalingen': 12,
                           'bedrag': 4654654, 'omschrijving': 'Money', 'credit': True,
                           'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 3, 'interval': 'P0Y1M0W0D',
-                          'journaalposten': [], 'zoektermen': None, 'organisatie_id': None,
+                          'journaalposten': [], 'zoektermen': None, 'afdeling_id': None,
                           'overschrijvingen': [40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51], 'rubriek_id': None,
                           'valid_from': '2021-01-01', 'tegen_rekening_id': 3}]})
 
@@ -595,6 +595,63 @@ def test_transactie_suggesties_multiple_zoektermen_too_specific(test_request_con
         assert get_rekeningen.call_count == 1
         assert get_afspraken.call_count == 1
         assert get_organisaties.call_count == 1
+
+        # No leftover calls
+        assert not post_any.called
+        assert not get_any.called
+
+
+
+def test_transactie_suggesties_matching_afspraak_on_organisatie(test_request_context):
+    with requests_mock.Mocker() as mock:
+        get_any = mock.get(requests_mock.ANY, json={"data": []})
+        post_any = mock.post(requests_mock.ANY, json=post_echo)
+
+        get_transactions = mock.get(
+            f"{settings.TRANSACTIE_SERVICES_URL}/banktransactions/?filter_ids=7", json={'data': [
+                {'bedrag': None, 'customer_statement_message_id': 17, 'id': 7,
+                 'information_to_account_owner': '/EREF/15814016000676480//CNTP/NL32INGB0000012345/INGBNL2A/J.Janssen///REMI/STRD/CUR/9001123412351234/',
+                 'is_credit': None, 'is_geboekt': None, 'statement_line': '140220C32.00NTRF900112341235123408/',
+                 'tegen_rekening': 'NL04RABO5082680188', "transactie_datum": '2021-12-30'}
+                 ]})
+
+        get_rekeningen = mock.get(
+            f"{settings.HHB_SERVICES_URL}/rekeningen/?filter_ibans=NL04RABO5082680188", json={
+                'data': [{'afspraken': [6], 'gebruikers': [], 'iban': 'NL04RABO5082680188', 'id': 2, 'organisaties': [],
+                          'rekeninghouder': 'Hema'}
+                          ]})
+
+        get_afspraken = mock.get(
+            f"{settings.HHB_SERVICES_URL}/afspraken/?filter_rekening=2,4", json={
+                'data': [{'aantal_betalingen': 12,
+                          'bedrag': 450000, 'omschrijving': 'Nog meer geld overmaken',
+                          'credit': True, 'valid_through': '2021-12-31', 'gebruiker_id': 1, 'id': 4,
+                          'interval': 'P0Y1M0W0D', 'journaalposten': [], 'zoektermen': ['15814016000676480'],
+                          'afdeling_id': 20,
+                          'overschrijvingen': [52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63], 'rubriek_id': None,
+                          'valid_from': '2021-01-01', 'tegen_rekening_id': 4}
+                         ]})
+        
+        get_organisaties = mock.get(
+            f"{settings.ORGANISATIE_SERVICES_URL}/organisaties/rekeningen", json={
+                'data': [{"afdeling_ids": [20],"organisatie_id": 1,"rekening_ids": [2,4]}
+                ]})
+        
+        get_organisatie_rekeningen = mock.get(
+            f"{settings.HHB_SERVICES_URL}/rekeningen/?filter_ids=4", json={
+                'data': [{'afspraken': [4], 'gebruikers': [], 'iban': 'NL29ABNA5179215998', 'id': 4, 'organisaties': [],
+                          'rekeninghouder': 'Hema'}
+                ]})
+        
+        result = transactie_suggesties([7])
+
+        assert get_transactions.call_count == 1
+        assert get_rekeningen.call_count == 1
+        assert get_afspraken.call_count == 1
+        assert get_organisaties.call_count == 1
+        assert get_organisatie_rekeningen.call_count == 1
+        assert len(result[7]) == 1
+        assert result[7][0]["id"] == 4
 
         # No leftover calls
         assert not post_any.called
