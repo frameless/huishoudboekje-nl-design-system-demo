cc2c50845f2ac956f6104cda9dfe1adb4ff19124
Martijn Lankhof <martijn.lankhof@topicus.nl>
2023-05-11 15:02:57 +0200

    fixed wrong loading data because comma seperation

--- services/grootboek_service/migrations/versions/2900675361d5_add_grootboekrekening_model.py
+++ services/grootboek_service/migrations/versions/2900675361d5_add_grootboekrekening_model.py
@@ -28,39 +28,6 @@ def upgrade():
     sa.ForeignKeyConstraint(['parent_id'], ['grootboekrekeningen.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-    op.bulk_insert(gb_table, get_grootboekrekenignen())
 
 def downgrade():
     op.drop_table('grootboekrekeningen')
-
-def get_grootboekrekenignen():
-    raw_data = {}
-    with open("data/RGS-3.2-6-december-2019.csv", "r") as fh:
-        for line in fh.readlines()[1:]:
-            linedata = [l[1:-1] for l in line.strip().split(",")]
-            if linedata[3]:
-                reccord = {
-                    "id": linedata[0],
-                    "referentie": linedata[3],
-                    "naam": linedata[4],
-                    "omschijving_kort": linedata[4],
-                    "omschijving_lang": linedata[5],
-                    "debet": linedata[6] == "D"
-                }
-                raw_data[reccord["id"]] = reccord
-    
-    # Perform parent lookup in seperate loop so we have a populated lookup dict
-    insert_data = []
-    for reccord_id, reccord in raw_data.items():
-        reccord["parent_id"] = recursive_find_parent(reccord_id, raw_data)
-        insert_data.append(reccord)
-        
-    return insert_data
-
-def recursive_find_parent(name, parent_list):
-    name = name[:-1]
-    if len(name) == 0:
-        return None
-    if name in parent_list:
-        return parent_list[name]["id"]
-    return recursive_find_parent(name, parent_list)
