098edc0de04aa677a1a4e0b4d660f0b216a5674d
Justin-Booij <Justin.Booij@topicus.nl>
2023-06-12 13:04:35 +0200

    fixed tests
    Fixed failing tests and removed redundant test due to removal of logic for searching (is now done through sql)

--- backend/tests/graphql/queries/test_burgers.py
+++ backend/tests/graphql/queries/test_burgers.py
@@ -108,7 +108,8 @@ def get_afspraken(request, _context):
 
 def test_burgers_success(client):
     with requests_mock.Mocker() as rm:
-        adapter = rm.get(f"{settings.HHB_SERVICES_URL}/burgers/", json={"data": [{"id": 1, "email": "a@b.c"}]})
+        adapter = rm.get(f"{settings.HHB_SERVICES_URL}/burgers/",
+                         json={"data": [{"id": 1, "email": "a@b.c"}]})
         response = client.post(
             "/graphql",
             data='{"query": "{ burgers { email }}"}',
@@ -137,13 +138,14 @@ def test_burgers_paged_success(client):
         assert adapter.call_count == 1
         assert response.json == {
             'data': {'burgersPaged': {'burgers': [{'email': 'a@b.c'}, {'email': 'test@test.com'}],
-            'pageInfo': {'count': 12, 'limit': 2, 'start': 1}}}
+                                      'pageInfo': {'count': 12, 'limit': 2, 'start': 1}}}
         }
 
 
 def test_burger_success(client):
     with requests_mock.Mocker() as rm:
-        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/", json={'data': [{'id': 1, 'email': 'a@b.c'}]})
+        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/",
+               json={'data': [{'id': 1, 'email': 'a@b.c'}]})
         response = client.post(
             "/graphql",
             data='{"query": "{ burger(id:1) { email }}"}',
@@ -154,251 +156,72 @@ def test_burger_success(client):
 
 def test_burger_afspraken(client):
     with requests_mock.Mocker() as rm:
-        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/", json={'data': [{'id': 1, 'afspraken': [1]}]})
-        rm.get(f"{settings.HHB_SERVICES_URL}/afspraken/", json={'data': [{'id': 1, "burger_id": 1}]})
+        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/",
+               json={'data': [{'id': 1, 'afspraken': [1]}]})
+        rm.get(f"{settings.HHB_SERVICES_URL}/afspraken/",
+               json={'data': [{'id': 1, "burger_id": 1}]})
         response = client.post(
             "/graphql",
             data='{"query": "{ burger(id:1) { afspraken { id } } }"}',
             content_type='application/json'
         )
-        assert response.json == {'data': {'burger': {'afspraken': [{'id': 1}]}}}
+        assert response.json == {
+            'data': {'burger': {'afspraken': [{'id': 1}]}}}
 
 
 def test_burger_rekeningen(client):
     with requests_mock.Mocker() as rm:
-        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/", json={'data': [{'id': 1, 'rekeningen': [1]}]})
-        rm.get(f"{settings.HHB_SERVICES_URL}/rekeningen/", json={'data': [{'id': 1, "burgers": [1]}]})
+        rm.get(f"{settings.HHB_SERVICES_URL}/burgers/",
+               json={'data': [{'id': 1, 'rekeningen': [1]}]})
+        rm.get(f"{settings.HHB_SERVICES_URL}/rekeningen/",
+               json={'data': [{'id': 1, "burgers": [1]}]})
         response = client.post(
             "/graphql",
             data='{"query": "{ burger(id:1) { rekeningen { id } } }"}',
             content_type='application/json'
         )
-        assert response.json == {'data': {'burger': {'rekeningen': [{'id': 1}]}}}
+        assert response.json == {
+            'data': {'burger': {'rekeningen': [{'id': 1}]}}}
 
 
-def test_burgers_search_single_voornaam(client):
+def test_burgers_search(client):
     with requests_mock.Mocker() as rm:
         # arrange
-        request = '{"query": "{ burgers(search:piet){id,bsn,voornamen,achternaam} }"}'
-        expected = {'data': {'burgers': [
-            {'achternaam': 'pieterson',
-            'bsn': 285278939,
-            'id': 1,
-            'voornamen': 'piet pieter'}
-        ]}}
-
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
-        rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
-        )
-        rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
-
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
-
-        # assert
-        assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
-        assert rm4.call_count == 1
-        assert fallback.call_count == 0
-        assert response.json == expected
-
-
-def test_burgers_search_single_achternaam(client):
-    with requests_mock.Mocker() as rm:
-        # arrange
-        request = '{"query": "{ burgers(search:klaasen){id,bsn,voornamen,achternaam} }"}'
         expected = {'data': {'burgers': [
             {'achternaam': 'klaasen',
-            'bsn': 914304057,
-            'id': 2,
-            'voornamen': 'kees'}
+             'id': 2,
+             'voornamen': 'kees'}
         ]}}
 
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
+        fallback = rm.register_uri(
+            requests_mock.ANY, requests_mock.ANY, status_code=404)
         rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
+            re.compile(f"{settings.HHB_SERVICES_URL}/burgers",),
+            json={'data':
+                  [{'achternaam': 'klaasen',
+                    'id': 2,
+                    'voornamen': 'kees'}
+                   ]}
         )
         rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
 
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
+        response = client.post(
+            "/graphql",
+            json={
+                "query": """
+                    query test($search:String!) {
+                      burgers(search:$search) {
+                        id
+                        voornamen
+                        achternaam
+                      }
+                    }""",
+                "variables": {"search": "klaasen"},
+            },
+        )
 
         # assert
         assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
-        assert rm4.call_count == 1
-        assert fallback.call_count == 0
-        assert response.json == expected
-
-
-def test_burgers_search_single_bsn(client):
-    with requests_mock.Mocker() as rm:
-        # arrange
-        request = '{"query": "{ burgers(search:939){id,bsn,voornamen,achternaam} }"}'
-        expected = {'data': {'burgers': [
-            {'achternaam': 'pieterson',
-            'bsn': 285278939,
-            'id': 1,
-            'voornamen': 'piet pieter'}
-        ]}}
-
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
-        rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
-        )
-        rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
-
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
-
-        # assert
-        assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
-        assert rm4.call_count == 1
-        assert fallback.call_count == 0
-        assert response.json == expected
-
-
-def test_burgers_search_single_iban(client):
-    with requests_mock.Mocker() as rm:
-        # arrange
-        request = '{"query": "{ burgers(search:NL21ABNA3184752488){id,bsn,voornamen,achternaam} }"}'
-        expected = {'data': {'burgers': [
-             {'achternaam': 'hansen',
-            'bsn': 356948705,
-            'id': 3,
-            'voornamen': 'henk'}
-        ]}}
-
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
-        rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
-        )
-        rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
-
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
-
-        # assert
-        assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
-        assert rm4.call_count == 1
-        assert fallback.call_count == 0
-        assert response.json == expected
-
-
-def test_find_one_burger_after_zoekterm_search_single_afspraak(client):
-    with requests_mock.Mocker() as rm:
-        # arrange
-        request = '{"query": "{ burgers(search:zoekterm1){id,bsn,voornamen,achternaam} }"}'
-        expected = {'data': {'burgers': [
-            {'achternaam': 'pieterson',
-            'bsn': 285278939,
-            'id': 1,
-            'voornamen': 'piet pieter'}
-        ]}}
-
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
-        rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
-        )
-        rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
-
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
-
-        # assert
-        assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
-        assert rm4.call_count == 1
-        assert fallback.call_count == 0
-        assert response.json == expected
-
-
-def test_find_one_burger_after_zoekterm_search_with_one_invalid_afspraak(client):
-    with requests_mock.Mocker() as rm:
-        # arrange
-        request = '{"query": "{ burgers(search:zoekterm1){id,bsn,voornamen,achternaam} }"}'
-        expected = {'data': {'burgers': [
-            {'achternaam': 'pieterson',
-            'bsn': 285278939,
-            'id': 1,
-            'voornamen': 'piet pieter'}
-        ]}}
-
-        fallback = rm.register_uri(requests_mock.ANY, requests_mock.ANY, status_code=404)
-        rm1 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/burgers/.*"),
-            json=get_burgers
-        )
-        rm2 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/rekeningen/.*"),
-            json=get_rekeningen
-        )
-        rm3 = rm.get(
-            re.compile(f"{settings.HHB_SERVICES_URL}/afspraken/.*"),
-            json=get_afspraken
-        )
-        rm4 = rm.post(f"{settings.LOG_SERVICE_URL}/gebruikersactiviteiten/")
-
-        # act
-        response = client.post("/graphql", data=request, content_type='application/json')
-
-        # assert
-        assert rm1.call_count == 1
-        assert rm2.call_count == 1
-        assert rm3.call_count == 1
         assert rm4.call_count == 1
         assert fallback.call_count == 0
         assert response.json == expected
