83cfe850186f68df879004404a478e987c504127
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-07-03 11:58:54 +0200

    Fixed foutmelding eenmalig alarm

--- frontend/app/src/validators/useAlarmValidator.ts
+++ frontend/app/src/validators/useAlarmValidator.ts
@@ -13,7 +13,7 @@ const useAlarmValidator = () => {
 		datumMargin: zod.number().min(0).optional(),
 		byDay: zod.array(zod.nativeEnum(DayOfWeek)).min(1).optional(),
 		byMonth: zod.array(zod.number().min(1).max(12)).min(1).max(12).optional(),
-		byMonthDay: zod.number().min(1).max(28).optional(),
+		byMonthDay: zod.number().min(1).max(31).optional(),
 	}).superRefine((data, ctx) => {
 		if (data.isPeriodiek === Periodiek.Eenmalig) {
 			const parsed = eenmaligValidator.safeParse(data);
@@ -21,13 +21,23 @@ const useAlarmValidator = () => {
 				parsed.error.issues.map(ctx.addIssue);
 			}
 		}
+		if (data.isPeriodiek === Periodiek.Periodiek) {
+			const parsed = periodiekValidator.safeParse(data);
+			if (!parsed.success) {
+				parsed.error.issues.map(ctx.addIssue);
+			}
+		}
 	});
 };
+
+const periodiekValidator = zod.object({
+	byMonthDay: zod.number().min(1).max(28).optional(),
+});
 
 const eenmaligValidator = zod.object({
 	date: zod.date(), //.refine(val => d().endOf("day").isSameOrBefore(val)), // Must be in the future
 	datumMargin: zod.number().min(0),
-	byMonthDay: zod.number().min(1).max(28),
+	byMonthDay: zod.number().min(1).max(31),
 	byMonth: zod.array(zod.number().min(1).max(12)).min(1).max(12),
 });
 
