a16d01607a160e620a0767e58f27da18adbd7f0d
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-07-10 15:47:00 +0200

    Improved rapportage when no transactions

---
+++ .changeset/tall-monkeys-joke.md
@@ -0,0 +1,5 @@
+---
+"huishoudboekje": patch
+---
+
+Improved rapportage when no transactions
--- frontend/app/src/components/Rapportage/BalanceTable.tsx
+++ frontend/app/src/components/Rapportage/BalanceTable.tsx
@@ -8,6 +8,7 @@ import Section from "../shared/Section";
 import SectionContainer from "../shared/SectionContainer";
 import {createBalanceTableAggregation, createSaldos, Offsets, Transaction, Type} from "./Aggregator";
 import { Dayjs } from "dayjs";
+import { type } from "os";
 
 type BalanceTableProps = {transactions: BurgerRapportage[], startDate: Dayjs, endDate: Dayjs, startSaldo: number, offsets: Offsets};
 
@@ -20,7 +21,7 @@ const BalanceTable: React.FC<BalanceTableProps> = ({transactions, startDate, end
 		[Type.Inkomsten]: t("charts.inkomstenUitgaven.income"),
 		[Type.Uitgaven]: t("charts.inkomstenUitgaven.expenses"),
 	};
-
+	const dataFound = Object.keys(aggregationByOrganisatie).length !== 0
 	return (
 		<SectionContainer>
 			<Section title={t("balance")}> {/* Todo: Add helperText (07-03-2022) */}
@@ -32,62 +33,64 @@ const BalanceTable: React.FC<BalanceTableProps> = ({transactions, startDate, end
 								through: endDate && d(endDate, "L").endOf("day").format("L"),
 							}} />
 						</Text>
-
-						{Object.keys(aggregationByOrganisatie).map(category => {
-							const total = saldos[category]
-							return (
-								<Stack key={category} spacing={3}>
-									<Text fontWeight={"bold"}>{translatedCategory[category]}</Text>
-									{Object.keys(aggregationByOrganisatie[category]).map((rubriek, rubriekKey) => {
-										return (
-											<VStack alignItems={"left"} key={`${category}:${rubriekKey}`} spacing={0}>
-												<Box flex={1}>
-													<Text fontStyle={"italic"}>{rubriek === Type.Ongeboekt ? t("charts.inkomstenUitgaven.unbooked") : rubriek}</Text>
-												</Box>
-												{Object.keys(aggregationByOrganisatie[category][rubriek]).map((transaction, key) => {
-													return (
-														<Stack direction={"row"} key={`${rubriekKey}:${key}`}>
-															<Box flex={2} textAlign={"left"}>
-																<Text >{aggregationByOrganisatie[category][rubriek][transaction].rekeninghouder}</Text>
-															</Box>
-															<Box>
-																<Text>{d(aggregationByOrganisatie[category][rubriek][transaction].transactieDatum).format("DD-MM-YYYY")}</Text>
-															</Box>
-															<Box flex={2} textAlign={"right"}>
-																<Text paddingRight={"35%"}>{`€ ${currencyFormat2(false).format(Math.abs(aggregationByOrganisatie[category][rubriek][transaction].bedrag))}`}</Text>
-															</Box>
-														</Stack>);
-												})};
-											</VStack>
-										);
-									})}
-									<HStack w="15%" textAlign={"right"} position={"relative"} left={"75%"}>
-										<Divider borderColor={"black"} flex={1} pt={1} />
-										<Text fontWeight={"bold"} flex={0}>+</Text>
-									</HStack>
-									<Stack paddingBottom={5} direction={"row"}>
-										<Box flex={1}>
-											<Text className={"do-not-print"}>{t(`total ${category}`)}</Text>
-										</Box>
-										<Box flex={2} textAlign={"right"}>
-											<Text paddingRight={"6%"} fontWeight={"bold"}>{`€ ${currencyFormat2(false).format(Math.abs(total))}`}</Text>
-										</Box>
+						{ dataFound && (<>
+							{Object.keys(aggregationByOrganisatie).map(category => {
+								const total = saldos[category]
+								return (
+									<Stack key={category} spacing={3}>
+										<Text fontWeight={"bold"}>{translatedCategory[category]}</Text>
+										{Object.keys(aggregationByOrganisatie[category]).map((rubriek, rubriekKey) => {
+											return (
+												<VStack alignItems={"left"} key={`${category}:${rubriekKey}`} spacing={0}>
+													<Box flex={1}>
+														<Text fontStyle={"italic"}>{rubriek === Type.Ongeboekt ? t("charts.inkomstenUitgaven.unbooked") : rubriek}</Text>
+													</Box>
+													{Object.keys(aggregationByOrganisatie[category][rubriek]).map((transaction, key) => {
+														return (
+															<Stack direction={"row"} key={`${rubriekKey}:${key}`}>
+																<Box flex={2} textAlign={"left"}>
+																	<Text >{aggregationByOrganisatie[category][rubriek][transaction].rekeninghouder}</Text>
+																</Box>
+																<Box>
+																	<Text>{d(aggregationByOrganisatie[category][rubriek][transaction].transactieDatum).format("DD-MM-YYYY")}</Text>
+																</Box>
+																<Box flex={2} textAlign={"right"}>
+																	<Text paddingRight={"35%"}>{`€ ${currencyFormat2(false).format(Math.abs(aggregationByOrganisatie[category][rubriek][transaction].bedrag))}`}</Text>
+																</Box>
+															</Stack>);
+													})};
+												</VStack>
+											);
+										})}
+										<HStack w="15%" textAlign={"right"} position={"relative"} left={"75%"}>
+											<Divider borderColor={"black"} flex={1} pt={1} />
+											<Text fontWeight={"bold"} flex={0}>+</Text>
+										</HStack>
+										<Stack paddingBottom={5} direction={"row"}>
+											<Box flex={1}>
+												<Text className={"do-not-print"}>{t(`total ${category}`)}</Text>
+											</Box>
+											<Box flex={2} textAlign={"right"}>
+												<Text paddingRight={"6%"} fontWeight={"bold"}>{`€ ${currencyFormat2(false).format(Math.abs(total))}`}</Text>
+											</Box>
+										</Stack>
 									</Stack>
-								</Stack>
-							);
-						})}
-						<HStack w="15%" textAlign={"right"} position={"relative"} left={"85%"} alignItems={"center"}>
-							<Divider borderColor={"black"} flex={1} pt={1} />
-							<Text fontWeight={"bold"} flex={0}>-</Text>
-						</HStack>
-						<Stack direction={"row"}>
-							<Box flex={2}>
-								<Text>{t("total income expenses in period")}</Text>
-							</Box>
-							<Box flex={2} textAlign={"right"} paddingRight={"6%"} >
-								<Text fontWeight={"bold"}>{`€ ${currencyFormat2(false).format(saldos['Total'])}`}</Text>
-							</Box>
-						</Stack>
+								);
+							})}
+							<HStack w="15%" textAlign={"right"} position={"relative"} left={"85%"} alignItems={"center"}>
+								<Divider borderColor={"black"} flex={1} pt={1} />
+								<Text fontWeight={"bold"} flex={0}>-</Text>
+							</HStack>
+							<Stack direction={"row"}>
+								<Box flex={2}>
+									<Text>{t("total income expenses in period")}</Text>
+								</Box>
+								<Box flex={2} textAlign={"right"} paddingRight={"6%"} >
+									<Text fontWeight={"bold"}>{`€ ${currencyFormat2(false).format(saldos['Total'])}`}</Text>
+								</Box>
+							</Stack>
+						</>)}
+						{ !dataFound && (<Text paddingTop={"30px"} >{t("reports.noData")}</Text>)}
 						<HStack direction={"row"}>
 							<Box flex={1} paddingTop={"30px"}>
 								<Text>{t("end saldo at") + " " + saldoDate.format("L")}:
