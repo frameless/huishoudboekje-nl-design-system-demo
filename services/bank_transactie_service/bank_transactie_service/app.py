d0039b28063ac4cd3952980a88072e91967602e3
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-09-13 12:29:15 +0200

    Moved sqlalchemy metrics to core service in attempt to use in other services

--- services/bank_transactie_service/bank_transactie_service/app.py
+++ services/bank_transactie_service/bank_transactie_service/app.py
@@ -15,6 +15,7 @@ from bank_transactie_service.views.customer_statement_message import CustomerSta
 from core_service import database
 from bank_transactie_service.views.bank_transaction_range import BanktransactionRangeView
 from bank_transactie_service.views.transactions_filter_view import BanktransactionFilterView
+from core_service.sqlalchemy_statsd_metrics import add_sqlalchemy_statsd_metrics
 
 db = database.db
 
@@ -43,68 +44,7 @@ def create_app(config_name='bank_transactie_service.config.Config'):
     # This can also cause parameters that are normally hidden to be logged
     logging.getLogger('werkzeug').setLevel(app.config["LOG_LEVEL"])
 
-    if app.config["STATSD_HOSTPORT"] is not None and type(app.config["STATSD_HOSTPORT"]) is str:
-        statsd = None
-        try:
-            statsd_host = app.config["STATSD_HOSTPORT"].split(':')
-            statsd = StatsClient(host=statsd_host[0], port=int(statsd_host[1]), prefix=app.config["STATSD_PREFIX"])
-            logging.info(f"Connected to statsd host {app.config['STATSD_HOSTPORT']}")
-        except:
-            logging.warning("could not connect to statsd host")
-
-        if statsd:
-            statsd.set('sqlalchemy.pool.connections', 0)
-            statsd.set('sqlalchemy.used.connections', 0)
-            statsd.set('sqlalchemy.detached.connections', 0)
-
-            @event.listens_for(Engine, "before_cursor_execute")
-            def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
-                conn.info.setdefault("query_start_time", []).append(time.time())
-
-            @event.listens_for(Engine, "after_cursor_execute")
-            def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
-                total = int((time.time() - conn.info["query_start_time"].pop(-1)) * 1000) #time in miliseconds
-                statsd.timing("query.execution.duration", total)
-                logging.info(f"Exexcuted query {statement} in {total} miliseconds")
-
-            @event.listens_for(Pool, "connect")
-            def receive_connect(dbapi_connection, connection_record):
-                # Called at the moment a particular DBAPI connection is first created for a given Pool.
-                statsd.gauge('sqlalchemy.pool.connections', 1, delta=True)
-                logging.debug(f"Connect")
-
-            @event.listens_for(Pool, "checkin")
-            def receive_checkin(dbapi_connection, connection_record):
-                # Called when a connection returns to the pool.
-                statsd.gauge('sqlalchemy.pool.connections', 1, delta=True)
-                statsd.gauge('sqlalchemy.used.connections', -1, delta=True)
-                logging.debug(f"checkin")
-
-            @event.listens_for(Pool, "checkout")
-            def receive_checkout(dbapi_connection, connection_record, connection_proxy):
-                # Called when a connection is retrieved from the Pool.
-                statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
-                statsd.gauge('sqlalchemy.used.connections', 1, delta=True)
-                logging.debug(f"checkout")
-
-            @event.listens_for(Pool, "close")
-            def receive_close(dbapi_connection, connection_record, connection_proxy):
-                # Called when a DBAPI connection is closed.
-                statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
-                logging.debug(f"close")
-
-            @event.listens_for(Pool, "detach")
-            def receive_detach(dbapi_connection, connection_record, connection_proxy):
-                # Called when a DBAPI connection is “detached” from a pool.
-                statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
-                statsd.gauge('sqlalchemy.detached.connections', 1, delta=True)
-                logging.debug(f"chedetachckin")
-
-            @event.listens_for(Pool, "close_detached")
-            def receive_close_detached(dbapi_connection, connection_record, connection_proxy):
-                # Called when a detached DBAPI connection is closed.
-                statsd.gauge('sqlalchemy.detached.connections', -1, delta=True)
-                logging.debug(f"close_detached")
+    add_sqlalchemy_statsd_metrics(app)
 
 
     @app.route('/health')
