4edadf9a8e2669b76db77496ac4be894e744a435
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-09-13 10:33:45 +0200

    Added sqlalchemy metrics to banktransactio service

--- services/bank_transactie_service/bank_transactie_service/app.py
+++ services/bank_transactie_service/bank_transactie_service/app.py
@@ -4,6 +4,11 @@ import logging.config
 
 from flask import Flask, Response
 from flask_migrate import Migrate
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlalchemy.pool import Pool
+import time
+from statsd import StatsClient
 
 from bank_transactie_service.views.bank_transaction import BankTransactionView
 from bank_transactie_service.views.customer_statement_message import CustomerStatementMessageView
@@ -37,6 +42,57 @@ def create_app(config_name='bank_transactie_service.config.Config'):
     # Werkzeug has their own logger which outputs info level URL calls.
     # This can also cause parameters that are normally hidden to be logged
     logging.getLogger('werkzeug').setLevel(app.config["LOG_LEVEL"])
+
+    if app.config["STATSD_HOSTPORT"] is not None and type(app.config["STATSD_HOSTPORT"]) is str:
+        try:
+            statsd_host = app.config["STATSD_HOSTPORT"].split(':')
+            statsd = StatsClient(host=statsd_host[0], port=int(statsd_host[1]), prefix=app.config["STATSD_PREFIX"])
+        except:
+            logging.warning("could not connect to statsd host")
+
+        if statsd:
+            @event.listens_for(Engine, "before_cursor_execute")
+            def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+                conn.info.setdefault("query_start_time", []).append(time.time())
+
+            @event.listens_for(Engine, "after_cursor_execute")
+            def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+                total = (time.time() - conn.info["query_start_time"].pop(-1)) * 1000 #time in miliseconds
+                statsd.timing("query.execution.duration", total)
+
+            @event.listens_for(Pool, "connect")
+            def receive_connect(dbapi_connection, connection_record):
+                # Called at the moment a particular DBAPI connection is first created for a given Pool.
+                statsd.incr('sqlalchemy.pool.connections')
+
+            @event.listens_for(Pool, "checkin")
+            def receive_checkin(dbapi_connection, connection_record):
+                # Called when a connection returns to the pool.
+                statsd.incr('sqlalchemy.pool.connections')
+                statsd.decr('sqlalchemy.used.connections')
+
+            @event.listens_for(Pool, "checkout")
+            def receive_checkout(dbapi_connection, connection_record, connection_proxy):
+                # Called when a connection is retrieved from the Pool.
+                statsd.decr('sqlalchemy.pool.connections')
+                statsd.incr('sqlalchemy.used.connections')
+
+            @event.listens_for(Pool, "close")
+            def receive_close(dbapi_connection, connection_record, connection_proxy):
+                # Called when a DBAPI connection is closed.
+                statsd.decr('sqlalchemy.pool.connections')
+
+            @event.listens_for(Pool, "detach")
+            def receive_detach(dbapi_connection, connection_record, connection_proxy):
+                # Called when a DBAPI connection is “detached” from a pool.
+                statsd.decr('sqlalchemy.pool.connections')
+                statsd.incr('sqlalchemy.detached.connections')
+
+            @event.listens_for(Pool, "close_detached")
+            def receive_close_detached(dbapi_connection, connection_record, connection_proxy):
+                # Called when a detached DBAPI connection is closed.
+                statsd.decr('sqlalchemy.detached.connections')
+
 
     @app.route('/health')
     def health():
--- services/bank_transactie_service/bank_transactie_service/config.py
+++ services/bank_transactie_service/bank_transactie_service/config.py
@@ -5,6 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
+    STATSD_HOSTPORT = os.getenv("STATSD_HOST", None)
+    STATSD_PREFEIX = os.getenv("STATSD_PREFEIX", "hhb.banktransacties")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', None)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
