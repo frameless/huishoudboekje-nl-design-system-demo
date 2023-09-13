57d919f20b0b8ab8d405939476efdcd21bd76f3b
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-09-13 12:20:50 +0200

    Debugging

--- services/bank_transactie_service/bank_transactie_service/app.py
+++ services/bank_transactie_service/bank_transactie_service/app.py
@@ -64,47 +64,47 @@ def create_app(config_name='bank_transactie_service.config.Config'):
             @event.listens_for(Engine, "after_cursor_execute")
             def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
                 total = int((time.time() - conn.info["query_start_time"].pop(-1)) * 1000) #time in miliseconds
-                logging.info(f"Exexcuted query in {total} miliseconds")
                 statsd.timing("query.execution.duration", total)
+                logging.info(f"Exexcuted query {statement} in {total} miliseconds")
 
             @event.listens_for(Pool, "connect")
             def receive_connect(dbapi_connection, connection_record):
                 # Called at the moment a particular DBAPI connection is first created for a given Pool.
                 statsd.gauge('sqlalchemy.pool.connections', 1, delta=True)
-                logging.info(f"Connect")
+                logging.debug(f"Connect")
 
             @event.listens_for(Pool, "checkin")
             def receive_checkin(dbapi_connection, connection_record):
                 # Called when a connection returns to the pool.
                 statsd.gauge('sqlalchemy.pool.connections', 1, delta=True)
                 statsd.gauge('sqlalchemy.used.connections', -1, delta=True)
-                logging.info(f"checkin")
+                logging.debug(f"checkin")
 
             @event.listens_for(Pool, "checkout")
             def receive_checkout(dbapi_connection, connection_record, connection_proxy):
                 # Called when a connection is retrieved from the Pool.
                 statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
                 statsd.gauge('sqlalchemy.used.connections', 1, delta=True)
-                logging.info(f"checkout")
+                logging.debug(f"checkout")
 
             @event.listens_for(Pool, "close")
             def receive_close(dbapi_connection, connection_record, connection_proxy):
                 # Called when a DBAPI connection is closed.
                 statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
-                logging.info(f"close")
+                logging.debug(f"close")
 
             @event.listens_for(Pool, "detach")
             def receive_detach(dbapi_connection, connection_record, connection_proxy):
                 # Called when a DBAPI connection is “detached” from a pool.
                 statsd.gauge('sqlalchemy.pool.connections', -1, delta=True)
                 statsd.gauge('sqlalchemy.detached.connections', 1, delta=True)
-                logging.info(f"chedetachckin")
+                logging.debug(f"chedetachckin")
 
             @event.listens_for(Pool, "close_detached")
             def receive_close_detached(dbapi_connection, connection_record, connection_proxy):
                 # Called when a detached DBAPI connection is closed.
                 statsd.gauge('sqlalchemy.detached.connections', -1, delta=True)
-                logging.info(f"close_detached")
+                logging.debug(f"close_detached")
 
 
     @app.route('/health')
