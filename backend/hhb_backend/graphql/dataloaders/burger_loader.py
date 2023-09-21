51291ed6e244e020fb5bc3d850f0925691462ece
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-09-21 13:47:12 +0200

    Added tiemouts in backend requests

--- backend/hhb_backend/graphql/dataloaders/burger_loader.py
+++ backend/hhb_backend/graphql/dataloaders/burger_loader.py
@@ -6,6 +6,7 @@ from graphql import GraphQLError
 from hhb_backend.graphql.dataloaders.base_loader import DataLoader
 from hhb_backend.graphql.settings import HHB_SERVICES_URL
 from hhb_backend.service.model.burger import Burger
+from hhb_backend.graphql.settings import INTERNAL_CONNECTION_TIMEOUT, INTERNAL_READ_TIMEOUT
 
 
 class BurgerLoader(DataLoader[Burger]):
@@ -20,8 +21,12 @@ class BurgerLoader(DataLoader[Burger]):
             url = f"{self.service}/{self.model}"
             params = {"search": True}
             body = {"search": search_value}
-            response = requests.get(url, json=body, params=params)
+            response = requests.get(url, json=body, params=params, timeout=(INTERNAL_CONNECTION_TIMEOUT,INTERNAL_READ_TIMEOUT))
 
+        except requests.exceptions.ReadTimeout:
+            raise GraphQLError(f"Failed to read data from {self.service} in time ")
+        except requests.exceptions.ConnectTimeout:
+            raise GraphQLError(f"Failed to connect to {self.service} in time")
         except requests.exceptions.ConnectionError:
             raise GraphQLError(f"Failed to connect to service {self.service}")
 
