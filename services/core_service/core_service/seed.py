3b73f92c2fa77b8bf1aa1c5af57da8642a826958
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-09-27 00:07:11 +0200

    Added seed commands to insert test data

---
+++ services/core_service/core_service/seed.py
@@ -0,0 +1,29 @@
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+
+def seed_database_with_test_data(sql_file_name, db_url):
+    logging.info("seeding database with test data")
+
+    sql_file_path =  "test_data/" +  sql_file_name
+    engine = create_engine(db_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        # Read the SQL file
+        with open(sql_file_path, "r", encoding="utf-16") as sql_file:
+            sql_script = sql_file.read()
+
+        # Execute the SQL script using SQLAlchemy
+        session.execute(sql_script)
+        session.commit()
+
+        print("Seeded database with test data successfully.")
+    except SQLAlchemyError as error:
+        session.rollback()
+        print(f"Error seeding database with test data: {error}")
+    finally:
+        session.close()
---
+++ services/postadressenservice/test_data/seed_database.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize-typescript';
+import * as fs from 'fs/promises';
+
+const connection_url : string = process.env.DATABASE_URL as string
+const seed_testdata = process.env.SEED_TESTDATA
+const sequelize = new Sequelize(connection_url);
+
+async function seedDb() {
+  if(seed_testdata !== undefined){
+    try {
+      await sequelize.sync(); // Sync the models with the database
+      const sqlScript = await fs.readFile('./test_data/postadressen.sql', 'utf16le');;
+      await sequelize.query(sqlScript);
+      console.log('SQL script executed successfully');
+    } catch (error) {
+      console.error('Error executing SQL script:', error);
+    } finally {
+      await sequelize.close();
+    }
+  }else {
+    console.error("SEED_TESTDATA is not set, not seeding database with test data");
+  }
+}
+
+seedDb();
