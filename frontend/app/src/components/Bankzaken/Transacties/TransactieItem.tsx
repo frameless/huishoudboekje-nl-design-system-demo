b9b0be53ce511b3c5cdc1261332d50901ffa403a
jeijkholt <joost.eijkholt@alten.nl>
2023-10-23 14:59:22 +0200

    elements

--- frontend/app/src/components/Bankzaken/Transacties/TransactieItem.tsx
+++ frontend/app/src/components/Bankzaken/Transacties/TransactieItem.tsx
@@ -1,7 +1,6 @@
 import {
 	CheckIcon,
 	WarningIcon,
-	InfoIcon
 } from "@chakra-ui/icons";
 import {
 	Box,
@@ -11,23 +10,15 @@ import {
 	Tag,
 	Text,
 	useBreakpointValue,
-	Popover,
-	PopoverTrigger,
-	PopoverContent,
-	PopoverBody,
-	PopoverArrow,
-	useDisclosure,
 } from '@chakra-ui/react'
 import React from "react";
 import {useTranslation} from "react-i18next";
 import {useNavigate} from "react-router-dom";
 import {AppRoutes} from "../../../config/routes";
-import {
-	currencyFormat2,
-	formatIBAN
-} from "../../../utils/things";
+import { currencyFormat2 } from "../../../utils/things";
 import PrettyIban from "../../shared/PrettyIban";
 import { BankTransaction } from "../../../generated/graphql";
+import TransactiePopover from "./TransactiePopover"
 
 const hoverStyles = {
 	_hover: {
@@ -44,7 +35,6 @@ const TransactieItem: React.FC<TransactieItemProps> = ({transactie: bank_transac
 	const {t} = useTranslation();
 	const isMobile = useBreakpointValue([true, null, null, false]);
 	const navigate = useNavigate();
-	const { isOpen, onOpen, onClose } = useDisclosure();
 
 
 	return (
@@ -126,53 +116,7 @@ const TransactieItem: React.FC<TransactieItemProps> = ({transactie: bank_transac
 				<Box
 					flex={0}
 				>
-					<Popover
-						isOpen={isOpen}
-						placement='bottom-end'
-					>
-						<PopoverTrigger>
-							<Text>
-								<Icon
-									as={InfoIcon}
-									color="blue.400"
-									marginLeft="1.5"
-									onMouseEnter={onOpen}
-									onMouseLeave={onClose}
-								/>
-							</Text>
-						</PopoverTrigger>
-						<PopoverContent
-							color='white'
-							bg='blue.800'
-							borderColor='blue.800'
-							onMouseLeave={onClose}
-							maxWidth='400'
-						>
-							<PopoverArrow bg='blue.800' />
-							<PopoverBody>
-								<Box 
-									flex="2"
-									width="100%"
-								>
-									<Text marginBottom={3}>
-											<h4><strong>{t("transactions.description")}:</strong></h4>
-											<p>
-												{bank_transaction.statementLine}<br/><br/>
-												{bank_transaction.informationToAccountOwner?.replace(/\s+/g, ' ')}
-											</p>
-									</Text>
-									<Text marginBottom={2}>
-										<h4><strong>{t("transacties.tegenrekening")}:</strong></h4>
-										<p>{bank_transaction.tegenRekening?.rekeninghouder || t("unknown")}</p>
-									</Text>
-									<Text marginBottom={2}>
-										<h4><strong>{t("forms.rekeningen.fields.iban")}:</strong></h4>
-										<p>{formatIBAN(bank_transaction.tegenRekening?.iban) || t("unknown")}</p>
-									</Text>
-								</Box>
-							</PopoverBody>
-						</PopoverContent>
-					</Popover>
+					<TransactiePopover bank_transaction={bank_transaction} />
 				</Box>
 			</Stack>
 		</Box>
---
+++ frontend/app/src/components/Bankzaken/Transacties/TransactiePopover.tsx
@@ -0,0 +1,78 @@
+import { InfoIcon } from "@chakra-ui/icons";
+import {
+	Box,
+	BoxProps,
+	Icon,
+	Text,
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverBody,
+	PopoverArrow,
+	useDisclosure,
+} from '@chakra-ui/react'
+import React from "react";
+import {useTranslation} from "react-i18next";
+import { formatIBAN } from "../../../utils/things";
+import { BankTransaction } from "../../../generated/graphql";
+
+type TransactiePopoverProps = BoxProps & {
+	bank_transaction: BankTransaction
+};
+
+const TransactiePopover: React.FC<TransactiePopoverProps> = ({bank_transaction: bank_transaction, ...props}) => {
+	const {t} = useTranslation();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	return (
+		<Popover
+			isOpen={isOpen}
+			placement='bottom-end'
+		>
+			<PopoverTrigger>
+				<Text>
+					<Icon
+						as={InfoIcon}
+						color="blue.400"
+						marginLeft="1.5"
+						onMouseEnter={onOpen}
+						onMouseLeave={onClose}
+					/>
+				</Text>
+			</PopoverTrigger>
+			<PopoverContent
+				color='white'
+				bg='blue.800'
+				borderColor='blue.800'
+				onMouseLeave={onClose}
+				maxWidth='400'
+			>
+				<PopoverArrow bg='blue.800' />
+				<PopoverBody>
+					<Box 
+						flex="2"
+						width="100%"
+					>
+						<Text marginBottom={3}>
+								<h4><strong>{t("transactions.description")}:</strong></h4>
+								<p>
+									{bank_transaction.statementLine}<br/><br/>
+									{bank_transaction.informationToAccountOwner?.replace(/\s+/g, ' ')}
+								</p>
+						</Text>
+						<Text marginBottom={2}>
+							<h4><strong>{t("transacties.tegenrekening")}:</strong></h4>
+							<p>{bank_transaction.tegenRekening?.rekeninghouder || t("unknown")}</p>
+						</Text>
+						<Text marginBottom={2}>
+							<h4><strong>{t("forms.rekeningen.fields.iban")}:</strong></h4>
+							<p>{formatIBAN(bank_transaction.tegenRekening?.iban) || t("unknown")}</p>
+						</Text>
+					</Box>
+				</PopoverBody>
+			</PopoverContent>
+		</Popover>
+	);
+};
+
+export default TransactiePopover;
