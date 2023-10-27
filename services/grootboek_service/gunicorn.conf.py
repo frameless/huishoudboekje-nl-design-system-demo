3e01a7ab2efc71d038e52c87cc84f1f769e7a846
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-10-27 14:43:47 +0200

    Updated services Dockerfile, changed default values and made it more configurable using ENV variables

--- services/grootboek_service/gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-import os
-
-statsd_host = os.environ.get("STATSD_HOST", None)
-statsd_prefix = os.environ.get("STATSD_PREFIX", "hhb.grootboek")
---
+++ services/rapportage_service/gunicorn/config/gunicorn.conf.py
@@ -0,0 +1,21 @@
+import os
+import logging
+
+
+statsd_prefix = os.environ.get("STATSD_PREFIX", "hhb.rapportage")
+limit_request_line = int(os.environ.get("GUNICORN_LIMIT_REQUEST_LINE", "0"))
+
+statsd_host = os.environ.get("STATSD_HOST", None)
+bind = "0.0.0.0:" + os.environ.get("GUNICORN_PORT", "8000")
+workers = int(os.environ.get("GUNICORN_WORKERS", "2"))
+worker_class = os.environ.get("GUNICORN_WORKER_CLASS", "sync")
+threads = int(os.environ.get("GUNICORN_THREADS", "1"))
+loglevel = os.environ.get("GUNICORN_LOGLEVEL", "info")
+capture_output = int(os.environ.get("GUNICORN_CAPTURE_OUTPUT", "1")) > 0
+worker_tmp_dir = os.environ.get("GUNICORN_WORKER_TMP_DIR", "/dev/shm")
+
+if threads > 1:
+    logging.warning("[CONFIG WARNING] Number of hreads is greater then one, this can cause unexpected behaviour. This application is not thread-safe")
+
+if loglevel == "info" or loglevel == "debug":
+    logging.warning("[CONFIG WARNING] Loglevel is set above warning, do not use this in production environments")
