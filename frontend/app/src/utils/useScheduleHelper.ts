db1780360814f2692947ae8250c00cbb51f589d5
jeijkholt <joost.eijkholt@alten.nl>
2023-11-14 10:21:53 +0100

    Oeps

--- frontend/app/src/utils/useScheduleHelper.ts
+++ frontend/app/src/utils/useScheduleHelper.ts
@@ -143,13 +143,13 @@ const useScheduleHelper = (schedule?: Schedule | Betaalinstructie) => {
 			returnDate.setHours(0, 0, 0, 0);
 
 			if (byDay && byDay.length > 0) {
-				const returnDay = returnDate.getDay();
 				const bySortedDays = byDay.map(d => parseInt(DayNumberOfWeek[String(d)])).sort();
 				const futureDays = bySortedDays.filter(d => calculatingDate.getDay() <= d);
 				const upcomingDay = futureDays.length ? futureDays[0] : bySortedDays[0];
-				const addDays = returnDay <= upcomingDay
-					? upcomingDay - returnDay
-					: (upcomingDay + 7) - returnDay;
+				const calculatedDay = calculatingDate.getDay();
+				const addDays = calculatedDay <= upcomingDay
+					? upcomingDay - calculatedDay
+					: (upcomingDay + 7) - calculatedDay;
 				
 				calculatingDate.setDate(calculatingDate.getDate() + addDays);
 
