05ae39c1326288612e585648d850bd877525de8d
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-11-20 11:20:51 +0100

    Added GUNICORN_WORKER_TIMEOUT env var

--- services/bank_transactie_service/gunicorn/config/gunicorn.conf.py
+++ services/bank_transactie_service/gunicorn/config/gunicorn.conf.py
@@ -10,6 +10,7 @@ bind = "0.0.0.0:" + os.environ.get("GUNICORN_PORT", "8000")
 workers = int(os.environ.get("GUNICORN_WORKERS", "2"))
 worker_class = os.environ.get("GUNICORN_WORKER_CLASS", "sync")
 threads = int(os.environ.get("GUNICORN_THREADS", "1"))
+timeout = int(os.environ.get("GUNICORN_WORKER_TIMEOUT", "600"))
 loglevel = os.environ.get("GUNICORN_LOGLEVEL", "info")
 capture_output = int(os.environ.get("GUNICORN_CAPTURE_OUTPUT", "1")) > 0
 worker_tmp_dir = os.environ.get("GUNICORN_WORKER_TMP_DIR", "/dev/shm")
