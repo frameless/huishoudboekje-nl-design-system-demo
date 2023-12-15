ead2d54376c8cf0ec9bf5a4763fc307d91a7b166
Martijn Lankhof <martijn.lankhof@alten.nl>
2023-12-15 13:37:43 +0100

    Fixed disapearing pagination buttons

--- frontend/app/src/components/Bankzaken/Transacties/index.tsx
+++ frontend/app/src/components/Bankzaken/Transacties/index.tsx
@@ -523,20 +523,21 @@ const Transactions = () => {
 										</HStack>
 									</HStack>
 								</Stack>
-									{$transactions.loading ? <Spinner/> : <Stack>
-										{transacties.length > 0 ? (
-											<Stack>
-												<TransactiesList transacties={transacties} />
-											</Stack>
-										) : (
-											<Stack>
-												<Text>{t("messages.transactions.noResults")}</Text>
-											</Stack>
-										)}
-									<HStack justify={"center"}>
-										<PaginationButtons />
-									</HStack>
-								</Stack>}
+								{$transactions.loading ? <Spinner/> : <Stack>
+									{transacties.length > 0 ? (
+										<Stack>
+											<TransactiesList transacties={transacties} />
+										</Stack>
+									) : (
+										<Stack>
+											<Text>{t("messages.transactions.noResults")}</Text>
+										</Stack>
+									)}
+								</Stack>
+								}
+								<HStack justify={"center"}>
+									<PaginationButtons />
+								</HStack>
 							</Stack>
 						</Section>
 					);
