27ade9b7228bb43524c3053467ee849f1e913024
loekTestCoders <loek@testcoders.nl>
2024-02-13 15:24:10 +0100

    added test and removed old test
    added read-alarm-settings test and removed the old gherkin and test files

--- cypress/support/step_definitions/Alarms/read-alarm-settings.js
+++ cypress/support/step_definitions/Alarms/read-alarm-settings.js
@@ -70,44 +70,512 @@ beforeEach(() => {
 // Is already defined in other test
 
 Then('the alarm recurrency is displayed', () => {
+ 
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
 
-  // Skip test
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
 
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if recurrency is displayed
+  cy.contains('Periodiek')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
+  
 });
 
 Then('the alarm day of the month is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('16')
+    .should('have.value', '16')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if day of month is displayed
+  cy.contains('op de 16e')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
 Then('the alarm allowed deviation in days is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('23')
+      .should('have.value', '23')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if deviation is displayed
+  cy.contains('+23 dagen')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
 Then('the alarm next date is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+    cy.get('\#field\\-\\\:r15\\\:')
+    .type('{selectAll}01-01-2099{enter}')
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if next date is displayed
+  cy.contains('Eerstvolgende datum')
+  cy.contains('01-01-2099')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
 Then('the alarm expected amount is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+    cy.get('\#field\\-\\\:r1b\\\:')
+      .type('{selectAll}123.45')
+      .should('have.value', '123.45') 
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if expected amount is displayed
+  cy.contains('Bedrag')
+  cy.contains('123,45')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
 Then('the alarm allowed deviation of the expected amount is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('{selectAll}37')
+      .should('have.value', '37')
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check if allowed deviation of the expected amount is displayed
+  cy.contains('+/- € 37,00')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
 Then('the alarm status is displayed', () => {
 
-  // Skip test
+  // Click button element
+  cy.visit('/afspraken/1');
+  cy.waitForReact();
+  cy.url().should('eq', Cypress.config().baseUrl + '/afspraken/1')
+  cy.get('h2').contains('Alarm').should('be.visible')
+    .scrollIntoView() // Scrolls 'Alarm' into view
+  cy.get('button')
+    .contains('Toevoegen')
+    .click()
+  cy.wait(500) // Wait 0.5 seconds for modal opening
+
+  // Check whether modal is opened and visible
+  cy.contains('Alarm toevoegen')
+  cy.get('.chakra-modal__footer')
+    .scrollIntoView() // Scrolls modal footer into view
+    .should('be.visible')
+
+  // Fill in all required fields
+    // 'Startdatum'
+      // Is automatically filled in
+
+    // 'Dag in de maand'
+    cy.get('\#field\\-\\\:r17\\\:')
+    .type('1')
+    .should('have.value', '1')
+
+    // 'Toegestane afwijking'
+    cy.get('\#field\\-\\\:r19\\\:')
+      .type('1')
+      .should('have.value', '1')
+
+    // 'Bedrag verwachte betaling'
+      // Is automatically filled in
+
+    // 'Toegestane afwijking bedrag'
+    cy.get('\#field\\-\\\:r1d\\\:')
+      .type('1')
+      .should('have.value', '1') 
+
+  // Click 'Opslaan' button
+  cy.get('.css-r3jkky')
+    .click()
+
+  // Check whether modal is closed
+  cy.contains('Alarm toevoegen')
+    .should('not.exist')
+  cy.get('.chakra-modal__footer')
+    .should('not.exist')
+
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is opgeslagen')
+
+  // Check whether current status of alarm is displayed
+  cy.get('label[class^="chakra-switch"]')
+    .should('be.visible')
+
+  // Clean up alarm
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  cy.get('button[aria-label="Verwijderen"]')
+    .click()
+  
+  // Check success message
+  cy.get('[data-status="success"]')
+    .should('be.visible')
+  cy.contains('Het alarm is verwijderd')
 
 });
 
