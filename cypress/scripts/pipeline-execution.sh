4858c85dbdbb3dba63ee7a7a59ddeca4a30fc2b1
Martijn Lankhof <martijn.lankhof@alten.nl>
2024-02-19 13:28:11 +0100

    debugging

--- cypress/scripts/pipeline-execution.sh
+++ cypress/scripts/pipeline-execution.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Start port forwarding
-kubectl port-forward service/hhb-database 5432:5432 -v=8 --streaming-connection-idle-timeout=0 --namespace=$NAMESPACE &
+kubectl port-forward deployment/hhb-database 5432:5432 -v=8 --namespace=$NAMESPACE &
 forwarding_pid=$!
 
+
+sleep 10
+
+netstat -f
+
+
 #running cypress tests
-echo "Installing..."
-npm ci
-echo "Executing tests..."
-npx cypress run --config baseUrl=$APP_HOST
-cypress_exit_code=$?
+# echo "Installing..."
+# npm ci
+# echo "Executing tests..."
+# npx cypress run --config baseUrl=$APP_HOST
+# cypress_exit_code=$?
 
-# Stop port forwarding
-kill $forwarding_pid
-wait $forwarding_pid 2>/dev/null
-echo "Port forwarding stopped."
+# # Stop port forwarding
+# kill $forwarding_pid
+# wait $forwarding_pid 2>/dev/null
+# echo "Port forwarding stopped."
 
-# Exit with the exit code of Cypress tests
-exit $cypress_exit_code
+# # Exit with the exit code of Cypress tests
+# exit $cypress_exit_code
