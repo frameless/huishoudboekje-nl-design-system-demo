5d36477d0b2f7f8e2ddd2893e216764b43f7d441
Martijn Lankhof <martijn.lankhof@alten.nl>
2024-02-19 15:13:27 +0100

    debugging

--- cypress/scripts/pipeline-execution.sh
+++ cypress/scripts/pipeline-execution.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-apt update
-apt install python3
-apt install python3-pip
-
-pip install psycopg2
-
 # Start port forwarding
-kubectl port-forward deployment/hhb-database 5432:5432 --namespace=$NAMESPACE &
+kubectl port-forward deployment/hhb-database 5432:5432 --address 0.0.0.0 --namespace=$NAMESPACE &
 forwarding_pid=$!
 
-sleep 5
-
-python3 cypress/scripts/test.py
-
 #running cypress tests
-# echo "Executing tests..."
-# npx cypress run --config baseUrl=$APP_HOST
-# cypress_exit_code=$?
+echo "Installing..."
+echo "Executing tests..."
+npx cypress run --config baseUrl=$APP_HOST
+cypress_exit_code=$?
 
 # Stop port forwarding
 kill $forwarding_pid
