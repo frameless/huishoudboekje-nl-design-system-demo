6c73eea07e16016b21b0c5aa81dcfd99b756d914
Martijn Lankhof <martijn.lankhof@alten.nl>
2024-02-19 14:07:51 +0100

    debugging

--- cypress/scripts/pipeline-execution.sh
+++ cypress/scripts/pipeline-execution.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
 # Start port forwarding
-kubectl port-forward deployment/hhb-database 5432:5432 -v=8 --namespace=$NAMESPACE &
+kubectl port-forward deployment/hhb-database 5432:5432 --namespace=$NAMESPACE &
 forwarding_pid=$!
 
-apt update
-apt install telnet
-
-sleep 10
-
-telnet 0.0.0.0 5432
-
 sleep 5
-echo "Done..."
 
 #running cypress tests
-# echo "Installing..."
-# npm ci
-# echo "Executing tests..."
-# npx cypress run --config baseUrl=$APP_HOST
-# cypress_exit_code=$?
+echo "Executing tests..."
+npx cypress run --config baseUrl=$APP_HOST
+cypress_exit_code=$?
 
-# # Stop port forwarding
-# kill $forwarding_pid
-# wait $forwarding_pid 2>/dev/null
-# echo "Port forwarding stopped."
+# Stop port forwarding
+kill $forwarding_pid
+wait $forwarding_pid 2>/dev/null
+echo "Port forwarding stopped."
 
-# # Exit with the exit code of Cypress tests
-# exit $cypress_exit_code
+# Exit with the exit code of Cypress tests
+exit $cypress_exit_code
