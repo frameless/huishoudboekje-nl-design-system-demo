705ecc880308acbf824a92c7638720263a8b6625
Martijn Lankhof <martijn.lankhof@topicus.nl>
2024-04-15 12:01:03 +0000

    hotfix saldo 1794
    Merge branch '1794-saldo-wijkt-af-bij-startdatum-na-1e-van-maand' into 'develop' by squash
    
    hotfix 1794 in develop
    
    Closes #1794
    
    See merge request commonground/huishoudboekje/app-new!1198

--- backend/hhb_backend/graphql/queries/__init__.py
+++ backend/hhb_backend/graphql/queries/__init__.py
@@ -158,6 +158,9 @@ class RootQuery(graphene.ObjectType):
 
     def resolve_exports(root, info, **kwargs):
         return ExportsQuery.resolver(root, info, **kwargs)
+    
+    def resolve_exports_paged(root, info, **kwargs):
+        return ExportsPagedQuery.resolver(root, info, **kwargs)
 
     def resolve_exports_paged(root, info, **kwargs):
         return ExportsPagedQuery.resolver(root, info, **kwargs)
